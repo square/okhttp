570385f29ab96e1041e4bef51bd3c6c1e852cbbc
Jake Wharton <jw@squareup.com>
2013-05-07 10:26:37 -0700

    Remove javadoc zip from website directory.

--- deploy_website.sh
+++ deploy_website.sh
@@ -30,6 +30,7 @@ cp -R ../website/* .
 curl -L "http://repository.sonatype.org/service/local/artifact/maven/redirect?r=central-proxy&g=$GROUP_ID&a=$ARTIFACT_ID&v=LATEST&c=javadoc" > javadoc.zip
 mkdir javadoc
 unzip javadoc.zip -d javadoc
+rm javadoc.zip
 
 # Stage all files in git and create a commit
 git add .
