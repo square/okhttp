a948ffce1eb2b9ec46069722f6744f252d6dbfc7
jwilson <jwilson@squareup.com>
2013-08-24 11:56:39 -0400

    Drop Content-Length when redirected from POST to GET.
    https://github.com/square/okhttp/issues/296

--- okhttp/src/test/java/com/squareup/okhttp/internal/http/URLConnectionTest.java
+++ okhttp/src/test/java/com/squareup/okhttp/internal/http/URLConnectionTest.java
@@ -1722,6 +1722,32 @@ public final class URLConnectionTest {
     assertEquals("GET /page2 HTTP/1.1", page2.getRequestLine());
   }
 
+  @Test public void redirectedPostStripsRequestBodyHeaders() throws Exception {
+    server.enqueue(new MockResponse()
+        .setResponseCode(HttpURLConnection.HTTP_MOVED_TEMP)
+        .addHeader("Location: /page2"));
+    server.enqueue(new MockResponse().setBody("Page 2"));
+    server.play();
+
+    HttpURLConnection connection = client.open(server.getUrl("/page1"));
+    connection.setDoOutput(true);
+    connection.addRequestProperty("Content-Length", "4");
+    connection.addRequestProperty("Content-Type", "text/plain; charset=utf-8");
+    connection.addRequestProperty("Transfer-Encoding", "identity");
+    OutputStream outputStream = connection.getOutputStream();
+    outputStream.write("ABCD".getBytes("UTF-8"));
+    outputStream.close();
+    assertEquals("Page 2", readAscii(connection.getInputStream(), Integer.MAX_VALUE));
+
+    assertEquals("POST /page1 HTTP/1.1", server.takeRequest().getRequestLine());
+
+    RecordedRequest page2 = server.takeRequest();
+    assertEquals("GET /page2 HTTP/1.1", page2.getRequestLine());
+    assertContainsNoneMatching(page2.getHeaders(), "Content-Length");
+    assertContains(page2.getHeaders(), "Content-Type: text/plain; charset=utf-8");
+    assertContains(page2.getHeaders(), "Transfer-Encoding: identity");
+  }
+
   @Test public void response305UseProxy() throws Exception {
     server.play();
     server.enqueue(new MockResponse().setResponseCode(HttpURLConnection.HTTP_USE_PROXY)
