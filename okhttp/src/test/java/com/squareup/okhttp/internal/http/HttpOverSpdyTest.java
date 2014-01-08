3aa708978d83c272e2a24348dfdd7d6695c3d27b
lingming.yb <lingming.yb@alibaba-inc.com>
2014-01-08 11:46:32 +0800

    Testcase for spdy connection socket timeout.

--- okhttp/src/test/java/com/squareup/okhttp/internal/http/HttpOverSpdyTest.java
+++ okhttp/src/test/java/com/squareup/okhttp/internal/http/HttpOverSpdyTest.java
@@ -271,6 +271,21 @@ public abstract class HttpOverSpdyTest {
     assertContent("A", connection, Integer.MAX_VALUE);
   }
 
+  @Test public void spdyConnectionTimeout() throws Exception {
+    MockResponse response = new MockResponse().setBody("A");
+    response.setBodyDelayTimeMs(1000);
+    server.enqueue(response);
+    server.play();
+
+    HttpURLConnection connection1 = client.open(server.getUrl("/"));
+    connection1.setReadTimeout(2000);
+    HttpURLConnection connection2 = client.open(server.getUrl("/"));
+    connection2.setReadTimeout(200);
+    connection1.connect();
+    connection2.connect();
+    assertContent("A", connection1, Integer.MAX_VALUE);
+  }
+
   @Test public void responsesAreCached() throws IOException {
     client.setResponseCache(cache);
 
