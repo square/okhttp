square/okhttp

commit 022173a44406f1492c674dc9450423581a21d675

Don't suggest NPN will be used when it might be ALPN.

okhttp/src/main/java/com/squareup/okhttp/internal/Platform.java
@@ -55,11 +55,6 @@ import okio.Buffer;
  * <p>On platforms that support both extensions, OkHttp will use both,
  * preferring ALPN's result. Future versions of OkHttp will drop support for
  * NPN.
- *
- * <h3>Deflater Sync Flush</h3>
- * SPDY header compression requires a recent version of {@code
- * DeflaterOutputStream} that is public API in Java 7 and callable via
- * reflection in Android 4.1+.
  */
 public class Platform {
   private static final Platform PLATFORM = findPlatform();
@@ -102,15 +97,15 @@ public class Platform {
   }
 
   /** Returns the negotiated protocol, or null if no protocol was negotiated. */
-  public String getNpnSelectedProtocol(SSLSocket socket) {
+  public String getSelectedProtocol(SSLSocket socket) {
     return null;
   }
 
   /**
    * Sets client-supported protocols on a socket to send to a server. The
-   * protocols are only sent if the socket implementation supports NPN.
+   * protocols are only sent if the socket implementation supports ALPN or NPN.
    */
-  public void setNpnProtocols(SSLSocket socket, List<Protocol> npnProtocols) {
+  public void setProtocols(SSLSocket socket, List<Protocol> protocols) {
   }
 
   public void connectSocket(Socket socket, InetSocketAddress address,
@@ -141,7 +136,7 @@ public class Platform {
       Method getNpnSelectedProtocol = null;
       try {
         setNpnProtocols = openSslSocketClass.getMethod("setNpnProtocols", byte[].class);
-        getNpnSelectedProtocol = openSslSocketClass.getMethod("getNpnSelectedProtocol");
+        getNpnSelectedProtocol = openSslSocketClass.getMethod("getSelectedProtocol");
       } catch (NoSuchMethodException ignored) {
       }
 
@@ -224,11 +219,11 @@ public class Platform {
       }
     }
 
-    @Override public void setNpnProtocols(SSLSocket socket, List<Protocol> npnProtocols) {
+    @Override public void setProtocols(SSLSocket socket, List<Protocol> protocols) {
       if (setNpnProtocols == null) return;
       if (!openSslSocketClass.isInstance(socket)) return;
       try {
-        Object[] parameters = { concatLengthPrefixed(npnProtocols) };
+        Object[] parameters = { concatLengthPrefixed(protocols) };
         setNpnProtocols.invoke(socket, parameters);
       } catch (IllegalAccessException e) {
         throw new AssertionError(e);
@@ -237,7 +232,7 @@ public class Platform {
       }
     }
 
-    @Override public String getNpnSelectedProtocol(SSLSocket socket) {
+    @Override public String getSelectedProtocol(SSLSocket socket) {
       if (getNpnSelectedProtocol == null) return null;
       if (!openSslSocketClass.isInstance(socket)) return null;
       try {
@@ -267,11 +262,11 @@ public class Platform {
       this.serverProviderClass = serverProviderClass;
     }
 
-    @Override public void setNpnProtocols(SSLSocket socket, List<Protocol> npnProtocols) {
+    @Override public void setProtocols(SSLSocket socket, List<Protocol> protocols) {
       try {
-        List<String> names = new ArrayList<String>(npnProtocols.size());
-        for (int i = 0, size = npnProtocols.size(); i < size; i++) {
-          Protocol protocol = npnProtocols.get(i);
+        List<String> names = new ArrayList<String>(protocols.size());
+        for (int i = 0, size = protocols.size(); i < size; i++) {
+          Protocol protocol = protocols.get(i);
           if (protocol == Protocol.HTTP_1_0) continue; // No HTTP/1.0 for NPN.
           names.add(protocol.toString());
         }
@@ -285,7 +280,7 @@ public class Platform {
       }
     }
 
-    @Override public String getNpnSelectedProtocol(SSLSocket socket) {
+    @Override public String getSelectedProtocol(SSLSocket socket) {
       try {
         JettyNpnProvider provider =
             (JettyNpnProvider) Proxy.getInvocationHandler(getMethod.invoke(null, socket));

benchmarks/src/main/java/com/squareup/okhttp/benchmarks/Benchmark.java
@@ -165,8 +165,7 @@ public class Benchmark extends com.google.caliper.Benchmark {
     if (tls) {
       SSLContext sslContext = SslContextBuilder.localhost();
       server.useHttps(sslContext.getSocketFactory(), false);
-      server.setNpnEnabled(true);
-      server.setNpnProtocols(protocols);
+      server.setProtocols(protocols);
     }
 
     final MockResponse response = newResponse();

mockwebserver/src/main/java/com/squareup/okhttp/internal/spdy/SpdyServer.java
@@ -67,9 +67,9 @@ public final class SpdyServer implements IncomingStreamHandler {
         (SSLSocket) sslSocketFactory.createSocket(socket, socket.getInetAddress().getHostAddress(),
             socket.getPort(), true);
     sslSocket.setUseClientMode(false);
-    Platform.get().setNpnProtocols(sslSocket, spdyProtocols);
+    Platform.get().setProtocols(sslSocket, spdyProtocols);
     sslSocket.startHandshake();
-    String protocolString = Platform.get().getNpnSelectedProtocol(sslSocket);
+    String protocolString = Platform.get().getSelectedProtocol(sslSocket);
     protocol = protocolString != null ? Protocol.get(protocolString) : null;
     if (protocol == null || !spdyProtocols.contains(protocol)) {
       throw new IllegalStateException("Protocol " + protocol + " unsupported");

mockwebserver/src/main/java/com/squareup/okhttp/mockwebserver/MockResponse.java
@@ -237,8 +237,9 @@ public final class MockResponse implements Cloneable {
   }
 
   /**
-   * When {@link MockWebServer#setNpnProtocols(java.util.List) protocols}
-   * include a SPDY variant, this attaches a pushed stream to this response.
+   * When {@link MockWebServer#setProtocols(java.util.List) protocols}
+   * include {@linkplain com.squareup.okhttp.Protocol#HTTP_2}, this attaches a
+   * pushed stream to this response.
    */
   public MockResponse withPush(PushPromise promise) {
     this.promises.add(promise);

mockwebserver/src/main/java/com/squareup/okhttp/mockwebserver/MockWebServer.java
@@ -109,7 +109,7 @@ public final class MockWebServer {
   private Dispatcher dispatcher = new QueueDispatcher();
 
   private int port = -1;
-  private boolean npnEnabled = true;
+  private boolean protocolNegotiationEnabled = true;
   private List<Protocol> npnProtocols
       = Util.immutableList(Protocol.HTTP_2, Protocol.SPDY_3, Protocol.HTTP_1_1);
 
@@ -166,9 +166,19 @@ public final class MockWebServer {
    * Sets whether NPN is used on incoming HTTPS connections to negotiate a
    * protocol like HTTP/1.1 or SPDY/3. Call this method to disable NPN and
    * SPDY.
+   * @deprecated Use {@link #setProtocolNegotiationEnabled}.
    */
   public void setNpnEnabled(boolean npnEnabled) {
-    this.npnEnabled = npnEnabled;
+    this.protocolNegotiationEnabled = npnEnabled;
+  }
+
+  /**
+   * Sets whether ALPN or NPN is used on incoming HTTPS connections to
+   * negotiate a protocol like HTTP/1.1 or HTTP/2. Call this method to disable
+   * negotiation and restrict connections to HTTP/1.1.
+   */
+  public void setProtocolNegotiationEnabled(boolean protocolNegotiationEnabled) {
+    this.protocolNegotiationEnabled = protocolNegotiationEnabled;
   }
 
   /**
@@ -177,8 +187,21 @@ public final class MockWebServer {
    *
    * @param protocols the protocols to use, in order of preference. The list
    *     must contain "http/1.1". It must not contain null.
+   * @deprecated Use {@link #setProtocols(java.util.List)}.
    */
   public void setNpnProtocols(List<Protocol> protocols) {
+    setProtocols(protocols);
+  }
+
+  /**
+   * Indicates the protocols supported by NPN or ALPN on incoming HTTPS
+   * connections. This list is ignored when
+   * {@link #setProtocolNegotiationEnabled negotiation is disabled}.
+   *
+   * @param protocols the protocols to use, in order of preference. The list
+   *     must contain {@linkplain Protocol#HTTP_1_1}. It must not contain null.
+   */
+  public void setProtocols(List<Protocol> protocols) {
     protocols = Util.immutableList(protocols);
     if (!protocols.contains(Protocol.HTTP_1_1)) {
       throw new IllegalArgumentException("protocols doesn't contain http/1.1: " + protocols);
@@ -186,7 +209,7 @@ public final class MockWebServer {
     if (protocols.contains(null)) {
       throw new IllegalArgumentException("protocols must not contain null");
     }
-    this.npnProtocols = Util.immutableList(protocols);
+    this.npnProtocols = protocols;
   }
 
   /**
@@ -327,14 +350,14 @@ public final class MockWebServer {
           sslSocket.setUseClientMode(false);
           openClientSockets.put(socket, true);
 
-          if (npnEnabled) {
-            Platform.get().setNpnProtocols(sslSocket, npnProtocols);
+          if (protocolNegotiationEnabled) {
+            Platform.get().setProtocols(sslSocket, npnProtocols);
           }
 
           sslSocket.startHandshake();
 
-          if (npnEnabled) {
-            String protocolString = Platform.get().getNpnSelectedProtocol(sslSocket);
+          if (protocolNegotiationEnabled) {
+            String protocolString = Platform.get().getSelectedProtocol(sslSocket);
             protocol = protocolString != null
                 ? Protocol.get(protocolString)
                 : Protocol.HTTP_1_1;

okhttp-tests/src/test/java/com/squareup/okhttp/CallTest.java
@@ -1013,8 +1013,7 @@ public final class CallTest {
     client.setHostnameVerifier(new RecordingHostnameVerifier());
     client.setProtocols(Arrays.asList(protocol, Protocol.HTTP_1_1));
     server.useHttps(sslContext.getSocketFactory(), false);
-    server.setNpnEnabled(true);
-    server.setNpnProtocols(client.getProtocols());
+    server.setProtocols(client.getProtocols());
   }
 
   private void assertContains(Collection<String> collection, String element) {

okhttp-tests/src/test/java/com/squareup/okhttp/HttpResponseCacheTest.java
@@ -104,7 +104,7 @@ public final class HttpResponseCacheTest {
 
   @Before public void setUp() throws Exception {
     server = serverRule.get();
-    server.setNpnEnabled(false);
+    server.setProtocolNegotiationEnabled(false);
     server2 = server2Rule.get();
     cache = new HttpResponseCache(cacheRule.getRoot(), Integer.MAX_VALUE);
     ResponseCache.setDefault(cache);

okhttp-tests/src/test/java/com/squareup/okhttp/internal/http/URLConnectionTest.java
@@ -111,7 +111,7 @@ public final class URLConnectionTest {
 
   @Before public void setUp() throws Exception {
     hostName = server.getHostName();
-    server.setNpnEnabled(false);
+    server.setProtocolNegotiationEnabled(false);
   }
 
   @After public void tearDown() throws Exception {
@@ -1789,7 +1789,7 @@ public final class URLConnectionTest {
     if (https) {
       server.useHttps(sslContext.getSocketFactory(), false);
       server2.useHttps(sslContext.getSocketFactory(), false);
-      server2.setNpnEnabled(false);
+      server2.setProtocolNegotiationEnabled(false);
       client.setSslSocketFactory(sslContext.getSocketFactory());
       client.setHostnameVerifier(new RecordingHostnameVerifier());
     }
@@ -3120,7 +3120,7 @@ public final class URLConnectionTest {
     client.setHostnameVerifier(new RecordingHostnameVerifier());
     client.setProtocols(Arrays.asList(protocol, Protocol.HTTP_1_1));
     server.useHttps(sslContext.getSocketFactory(), false);
-    server.setNpnEnabled(true);
-    server.setNpnProtocols(client.getProtocols());
+    server.setProtocolNegotiationEnabled(true);
+    server.setProtocols(client.getProtocols());
   }
 }

commit efc791b84afa4d6a6083e0913dc550c8698d1f73

Correct accidental rename of reflected method.
Broken in 022173a44406f1492c674dc9450423581a21d675.

okhttp/src/main/java/com/squareup/okhttp/internal/Platform.java
@@ -148,7 +148,7 @@ public class Platform {
       Method getNpnSelectedProtocol = null;
       try {
         setNpnProtocols = openSslSocketClass.getMethod("setNpnProtocols", byte[].class);
-        getNpnSelectedProtocol = openSslSocketClass.getMethod("getSelectedProtocol");
+        getNpnSelectedProtocol = openSslSocketClass.getMethod("getNpnSelectedProtocol");
       } catch (NoSuchMethodException ignored) {
       }
 