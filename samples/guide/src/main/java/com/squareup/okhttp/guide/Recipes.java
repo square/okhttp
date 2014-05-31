1ba78fa29d1c0667bf7fb5efac2a9898d1d98b43
Jesse Wilson <jwilson@squareup.com>
2014-05-31 10:01:19 -0400

    Some POST samples.

--- samples/guide/src/main/java/com/squareup/okhttp/guide/Recipes.java
+++ samples/guide/src/main/java/com/squareup/okhttp/guide/Recipes.java
@@ -4,12 +4,15 @@ import com.google.gson.Gson;
 import com.squareup.okhttp.Cache;
 import com.squareup.okhttp.Callback;
 import com.squareup.okhttp.Headers;
+import com.squareup.okhttp.MediaType;
 import com.squareup.okhttp.OkHttpClient;
 import com.squareup.okhttp.Request;
+import com.squareup.okhttp.RequestBody;
 import com.squareup.okhttp.Response;
 import java.io.File;
 import java.io.IOException;
 import java.util.Map;
+import okio.BufferedSink;
 
 public final class Recipes {
   public void getResponseSynchronously() throws Exception {
@@ -19,9 +22,7 @@ public final class Recipes {
         .build();
 
     Response response = client.newCall(request).execute();
-    if (response.code() < 200 || response.code() >= 300) {
-      throw new IOException("Unexpected code " + response);
-    }
+    if (!response.isSuccessful()) throw new IOException("Unexpected code " + response);
 
     Headers responseHeaders = response.headers();
     for (int i = 0; i < responseHeaders.size(); i++) {
@@ -43,9 +44,7 @@ public final class Recipes {
       }
 
       @Override public void onResponse(Response response) throws IOException {
-        if (response.code() < 200 || response.code() >= 300) {
-          throw new IOException("Unexpected code " + response);
-        }
+        if (!response.isSuccessful()) throw new IOException("Unexpected code " + response);
 
         Headers responseHeaders = response.headers();
         for (int i = 0; i < responseHeaders.size(); i++) {
@@ -65,6 +64,8 @@ public final class Recipes {
         .url("https://api.github.com/gists/c2a7c39532239ff261be")
         .build();
     Response response = client.newCall(request).execute();
+    if (!response.isSuccessful()) throw new IOException("Unexpected code " + response);
+
     Gist gist = gson.fromJson(response.body().charStream(), Gist.class);
     for (Map.Entry<String, GistFile> entry : gist.files.entrySet()) {
       System.out.println(entry.getKey());
@@ -93,18 +94,16 @@ public final class Recipes {
         .build();
 
     Response response1 = client.newCall(request).execute();
-    if (response1.code() < 200 || response1.code() >= 300) {
-      throw new IOException("Unexpected code " + response1);
-    }
+    if (!response1.isSuccessful()) throw new IOException("Unexpected code " + response1);
+
     String response1Body = response1.body().string();
     System.out.println("Response 1 response:          " + response1);
     System.out.println("Response 1 cache response:    " + response1.cacheResponse());
     System.out.println("Response 1 network response:  " + response1.networkResponse());
 
     Response response2 = client.newCall(request).execute();
-    if (response2.code() < 200 || response2.code() >= 300) {
-      throw new IOException("Unexpected code " + response2);
-    }
+    if (!response2.isSuccessful()) throw new IOException("Unexpected code " + response2);
+
     String response2Body = response2.body().string();
     System.out.println("Response 2 response:          " + response2);
     System.out.println("Response 2 cache response:    " + response2.cacheResponse());
@@ -113,11 +112,89 @@ public final class Recipes {
     System.out.println("Response 2 equals Response 1? " + response1Body.equals(response2Body));
   }
 
+  public void postString() throws Exception {
+    MediaType mediaType = MediaType.parse("text/x-markdown; charset=utf-8");
+    String postBody = ""
+        + "Releases\n"
+        + "--------\n"
+        + "\n"
+        + " * _1.0_ May 6, 2013\n"
+        + " * _1.1_ June 15, 2013\n"
+        + " * _1.2_ August 11, 2013\n";
+
+    OkHttpClient client = new OkHttpClient();
+    Request request = new Request.Builder()
+        .url("https://api.github.com/markdown/raw")
+        .post(RequestBody.create(mediaType, postBody))
+        .build();
+
+    Response response = client.newCall(request).execute();
+    if (!response.isSuccessful()) throw new IOException("Unexpected code " + response);
+
+    System.out.println(response.body().string());
+  }
+
+  public void postFile() throws Exception {
+    MediaType mediaType = MediaType.parse("text/x-markdown; charset=utf-8");
+    File file = new File("README.md");
+
+    OkHttpClient client = new OkHttpClient();
+    Request request = new Request.Builder()
+        .url("https://api.github.com/markdown/raw")
+        .post(RequestBody.create(mediaType, file))
+        .build();
+
+    Response response = client.newCall(request).execute();
+    if (!response.isSuccessful()) throw new IOException("Unexpected code " + response);
+
+    System.out.println(response.body().string());
+  }
+
+  public void postStreaming() throws Exception {
+    RequestBody requestBody = new RequestBody() {
+      MediaType mediaType = MediaType.parse("text/x-markdown; charset=utf-8");
+
+      @Override public MediaType contentType() {
+        return mediaType;
+      }
+
+      @Override public void writeTo(BufferedSink sink) throws IOException {
+        sink.writeUtf8("Numbers\n");
+        sink.writeUtf8("-------\n");
+        for (int i = 2; i <= 997; i++) {
+          sink.writeUtf8(String.format(" * %s = %s\n", i, factor(i)));
+        }
+      }
+
+      private String factor(int n) {
+        for (int i = 2; i < n; i++) {
+          int x = n / i;
+          if (x * i == n) return factor(x) + " × " + i;
+        }
+        return Integer.toString(n);
+      }
+    };
+
+    OkHttpClient client = new OkHttpClient();
+    Request request = new Request.Builder()
+        .url("https://api.github.com/markdown/raw")
+        .post(requestBody)
+        .build();
+
+    Response response = client.newCall(request).execute();
+    if (!response.isSuccessful()) throw new IOException("Unexpected code " + response);
+
+    System.out.println(response.body().string());
+  }
+
   public static void main(String[] args) throws Exception {
     Recipes recipes = new Recipes();
     recipes.getResponseSynchronously();
     recipes.getResponseAsynchronously();
     recipes.parseResponseWithGson();
     recipes.cacheResponse();
+    recipes.postString();
+    recipes.postFile();
+    recipes.postStreaming();
   }
 }
