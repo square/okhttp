c4667b67233514ac2dba77ecd28200dbf1acece8
Daniel Lubarov <daniel@lubarov.com>
2014-07-29 15:46:13 -0700

    Use MockWebServerRule in ParallelConnectionStrategyTest

--- okhttp-tests/src/test/java/com/squareup/okhttp/ParallelConnectionStrategyTest.java
+++ okhttp-tests/src/test/java/com/squareup/okhttp/ParallelConnectionStrategyTest.java
@@ -2,7 +2,7 @@ package com.squareup.okhttp;
 
 import com.squareup.okhttp.internal.Util;
 import com.squareup.okhttp.internal.http.AuthenticatorAdapter;
-import com.squareup.okhttp.mockwebserver.MockWebServer;
+import com.squareup.okhttp.mockwebserver.rule.MockWebServerRule;
 import java.io.IOException;
 import java.net.InetAddress;
 import java.net.InetSocketAddress;
@@ -10,8 +10,6 @@ import java.net.Proxy;
 import java.net.Socket;
 import java.net.UnknownHostException;
 import javax.net.SocketFactory;
-import org.junit.After;
-import org.junit.Before;
 import org.junit.Rule;
 import org.junit.Test;
 import org.junit.rules.ExpectedException;
@@ -19,39 +17,27 @@ import org.junit.rules.ExpectedException;
 import static com.squareup.okhttp.internal.http.RouteSelector.TLS_V1;
 
 public class ParallelConnectionStrategyTest {
-  private MockWebServer httpServer;
-  private Address httpAddress;
-  private InetSocketAddress httpSocketAddress;
-
+  @Rule public MockWebServerRule serverRule = new MockWebServerRule();
   @Rule public ExpectedException expectedException = ExpectedException.none();
 
-  @Before public void setUp() throws Exception {
-    httpServer = new MockWebServer();
-    httpServer.play();
-
-    httpAddress = new Address(httpServer.getHostName(), httpServer.getPort(),
+  @Test public void testConnectionToLocalServer() throws Exception {
+    Address httpAddress = new Address(serverRule.get().getHostName(), serverRule.getPort(),
         SocketFactory.getDefault(), null, null, AuthenticatorAdapter.INSTANCE, null,
         Util.immutableList(Protocol.SPDY_3, Protocol.HTTP_1_1));
-    httpSocketAddress = new InetSocketAddress(InetAddress.getByName(httpServer.getHostName()),
-        httpServer.getPort());
-  }
-
-  @After public void tearDown() throws Exception {
-    httpServer.shutdown();
-  }
-
-  @Test public void testConnectionToLocalServer() throws Exception {
+    InetSocketAddress httpSocketAddress = new InetSocketAddress(
+        InetAddress.getByName(serverRule.get().getHostName()),
+        serverRule.getPort());
     Route route = new Route(httpAddress, Proxy.NO_PROXY, httpSocketAddress, TLS_V1);
     Socket socket = ParallelConnectionStrategy.DEFAULT.connect(route, 200);
     socket.close();
   }
 
   @Test public void testConnectionToGoodAndBadIp() throws Exception {
-    Address address = new Address("example.com", httpServer.getPort(), SocketFactory.getDefault(),
+    Address address = new Address("example.com", serverRule.getPort(), SocketFactory.getDefault(),
         null, null, AuthenticatorAdapter.INSTANCE, null,
         Util.immutableList(Protocol.SPDY_3, Protocol.HTTP_1_1));
     Route route = new Route(address, Proxy.NO_PROXY,
-        InetSocketAddress.createUnresolved("example.com", httpServer.getPort()), TLS_V1);
+        InetSocketAddress.createUnresolved("example.com", serverRule.getPort()), TLS_V1);
 
     HostResolver hostResolver = createExampleHostResolver(new InetAddress[] {
         InetAddress.getByName("192.0.2.0"), // This will fail - IP is reserved for documentation.
@@ -63,11 +49,11 @@ public class ParallelConnectionStrategyTest {
   }
 
   @Test public void testConnectionMultipleBadIps() throws Exception {
-    Address address = new Address("example.com", httpServer.getPort(), SocketFactory.getDefault(),
+    Address address = new Address("example.com", serverRule.getPort(), SocketFactory.getDefault(),
         null, null, AuthenticatorAdapter.INSTANCE, null,
         Util.immutableList(Protocol.SPDY_3, Protocol.HTTP_1_1));
     Route route = new Route(address, Proxy.NO_PROXY,
-        InetSocketAddress.createUnresolved("example.com", httpServer.getPort()), TLS_V1);
+        InetSocketAddress.createUnresolved("example.com", serverRule.getPort()), TLS_V1);
 
     HostResolver hostResolver = createExampleHostResolver(new InetAddress[] {
         // These will both fail - 192.0.2.0-192.0.2.255 are reserved for documentation.
