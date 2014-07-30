abafe60b39e9cf3d03d148ebe839707997c7051f
Daniel Lubarov <daniel@lubarov.com>
2014-07-30 14:19:19 -0700

    A few improvements to ParallelConnectionStrategy
    - Don't bother with the concurrency code if there's only one address
    - Limit the number of concurrent connection attempts for one hostname

--- okhttp/src/main/java/com/squareup/okhttp/ParallelConnectionStrategy.java
+++ okhttp/src/main/java/com/squareup/okhttp/ParallelConnectionStrategy.java
@@ -7,8 +7,10 @@ import java.net.InetAddress;
 import java.net.InetSocketAddress;
 import java.net.Socket;
 import java.net.UnknownHostException;
+import java.util.ArrayDeque;
 import java.util.ArrayList;
 import java.util.Collection;
+import java.util.Queue;
 import java.util.concurrent.Callable;
 import java.util.concurrent.CompletionService;
 import java.util.concurrent.ExecutionException;
@@ -21,6 +23,13 @@ import java.util.concurrent.atomic.AtomicReference;
  * See {@link ConnectionStrategy#PARALLEL}.
  */
 final class ParallelConnectionStrategy extends ConnectionStrategy {
+  /**
+   * The maximum number of concurrent TCP handshakes we will perform for a single hostname. If a
+   * hostname has many IPs behind it, we will initially connect to MAX_CONCURRENT_HANDSHAKES of
+   * them. Only if those connections fail will we will try the rest of the IPs.
+   */
+  private static final int MAX_CONCURRENT_HANDSHAKES = 3;
+
   // Use an unbounded thread pool to make sure we don't introduce an unnecessary bottleneck.
   private static final ExecutorService executor = Executors.newCachedThreadPool();
 
@@ -36,11 +45,21 @@ final class ParallelConnectionStrategy extends ConnectionStrategy {
 
   @Override public Socket connect(final Route route, final int connectTimeout) throws IOException {
     Collection<InetSocketAddress> addressesToTry = getAddressesToTry(route.inetSocketAddress);
+
+    // If there's only one address, we can avoid the overhead of parallelization.
+    if (addressesToTry.size() == 1) {
+      Socket socket = createSocket(route);
+      Platform.get().connectSocket(socket, addressesToTry.iterator().next(), connectTimeout);
+      return socket;
+    }
+
     CompletionService<Socket> completionService = new ExecutorCompletionService<>(executor);
     final AtomicReference<Socket> winner = new AtomicReference<>();
 
+    // Prepare a set of connection tasks, one for each address we want to try.
+    Queue<Callable<Socket>> connectionTaskQueue = new ArrayDeque<>();
     for (final InetSocketAddress address : addressesToTry) {
-      completionService.submit(new Callable<Socket>() {
+      connectionTaskQueue.add(new Callable<Socket>() {
         @Override public Socket call() throws IOException {
           Socket socket = createSocket(route);
           Platform.get().connectSocket(socket, address, connectTimeout);
@@ -52,23 +71,35 @@ final class ParallelConnectionStrategy extends ConnectionStrategy {
       });
     }
 
-    int numPendingTasks = addressesToTry.size();
+    // Trigger the initial set of (up to MAX_CONCURRENT_HANDSHAKES) connection tasks.
+    int handshakesInProgress = 0;
+    while (handshakesInProgress < MAX_CONCURRENT_HANDSHAKES && !connectionTaskQueue.isEmpty()) {
+      completionService.submit(connectionTaskQueue.remove());
+      handshakesInProgress++;
+    }
+
+    // Wait for connection tasks to complete until we either get a successful connection, or run out
+    // of addresses to try.
     Throwable latestFailure = null;
-    while (winner.get() == null && numPendingTasks > 0) {
+    while (handshakesInProgress > 0) {
       try {
         completionService.take().get(); // Block until a connection task completes.
+        return winner.get();
       } catch (InterruptedException e) {
         throw new InterruptedIOException();
       } catch (ExecutionException e) {
         latestFailure = e.getCause();
       }
-      numPendingTasks--;
+
+      // If there are more addresses remaining, start a task to try the next one.
+      if (!connectionTaskQueue.isEmpty()) {
+        completionService.submit(connectionTaskQueue.remove());
+      } else {
+        handshakesInProgress--;
+      }
     }
 
-    if (winner.get() == null) {
-      throw new IOException("Failed to connect to " + route.inetSocketAddress, latestFailure);
-    }
-    return winner.get();
+    throw new IOException("Failed to connect to " + route.inetSocketAddress, latestFailure);
   }
 
   /**
