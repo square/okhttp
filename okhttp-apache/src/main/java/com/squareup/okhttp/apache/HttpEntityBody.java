b16c9f29a8e21c77368f9970db862ad8f7af9f5e
Jake Wharton <jw@squareup.com>
2014-08-19 09:16:29 -0700

    Support overriding content type via explicit header.

--- okhttp-apache/src/main/java/com/squareup/okhttp/apache/HttpEntityBody.java
+++ okhttp-apache/src/main/java/com/squareup/okhttp/apache/HttpEntityBody.java
@@ -4,23 +4,27 @@ import com.squareup.okhttp.MediaType;
 import com.squareup.okhttp.RequestBody;
 import java.io.IOException;
 import okio.BufferedSink;
-import org.apache.http.Header;
 import org.apache.http.HttpEntity;
 
 /** Adapts an {@link HttpEntity} to OkHttp's {@link RequestBody}. */
 final class HttpEntityBody extends RequestBody {
-  private static final String DEFAULT_MEDIA_TYPE = "application/octet-stream";
+  private static final MediaType DEFAULT_MEDIA_TYPE = MediaType.parse("application/octet-stream");
 
   private final HttpEntity entity;
   private final MediaType mediaType;
 
-  HttpEntityBody(HttpEntity entity) {
+  HttpEntityBody(HttpEntity entity, String contentTypeHeader) {
     this.entity = entity;
 
-    // Apache is forgiving and lets you skip specifying a content type with an entity. OkHttp is
-    // not forgiving so we fall back to a generic type if it's missing.
-    Header contentType = entity.getContentType();
-    mediaType = MediaType.parse(contentType != null ? contentType.getValue() : DEFAULT_MEDIA_TYPE);
+    if (contentTypeHeader != null) {
+      mediaType = MediaType.parse(contentTypeHeader);
+    } else if (entity.getContentType() != null) {
+      mediaType = MediaType.parse(entity.getContentType().getValue());
+    } else {
+      // Apache is forgiving and lets you skip specifying a content type with an entity. OkHttp is
+      // not forgiving so we fall back to a generic type if it's missing.
+      mediaType = DEFAULT_MEDIA_TYPE;
+    }
   }
 
   @Override public long contentLength() {
