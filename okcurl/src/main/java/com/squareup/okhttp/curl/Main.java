60b1dfc169156f300045fd0e01e89b890661b5a7
Adrian Cole <adrian.f.cole@gmail.com>
2015-03-11 08:14:55 -0700

    Updates to http/2 final
    Http2 and Hpack have been [submitted]
    (https://www.rfc-editor.org/queue2.html#draft-ietf-httpbis-http2) to the
    IETF. This change removes the draft alpn identifier, and renames types
    accordingly.

--- okcurl/src/main/java/com/squareup/okhttp/curl/Main.java
+++ okcurl/src/main/java/com/squareup/okhttp/curl/Main.java
@@ -25,7 +25,7 @@ import com.squareup.okhttp.Request;
 import com.squareup.okhttp.RequestBody;
 import com.squareup.okhttp.Response;
 import com.squareup.okhttp.internal.http.StatusLine;
-import com.squareup.okhttp.internal.spdy.Http20Draft16;
+import com.squareup.okhttp.internal.spdy.Http2;
 
 import io.airlift.command.Arguments;
 import io.airlift.command.Command;
@@ -266,7 +266,7 @@ public class Main extends HelpOption implements Runnable {
   }
 
   private static void enableHttp2FrameLogging() {
-    Logger logger = Logger.getLogger(Http20Draft16.class.getName() + "$FrameLogger");
+    Logger logger = Logger.getLogger(Http2.class.getName() + "$FrameLogger");
     logger.setLevel(Level.FINE);
     ConsoleHandler handler = new ConsoleHandler();
     handler.setLevel(Level.FINE);
