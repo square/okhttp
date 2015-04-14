square/okhttp

commit f78f74f5a2cf1f73f974487816f998a6e37a9cf1

Use ExecutorService.execute, not ExecutorService.submit.
The latter returns a Future that we never call get() on, and
never get the exception from.

Fixes: https://github.com/square/okhttp/issues/1294

okhttp/src/main/java/com/squareup/okhttp/internal/spdy/SpdyConnection.java
@@ -352,7 +352,7 @@ public final class SpdyConnection implements Closeable {
   }
 
   void writeWindowUpdateLater(final int streamId, final long unacknowledgedBytesRead) {
-    executor.submit(new NamedRunnable("OkHttp Window Update %s stream %d", hostName, streamId) {
+    executor.execute(new NamedRunnable("OkHttp Window Update %s stream %d", hostName, streamId) {
       @Override public void execute() {
         try {
           frameWriter.windowUpdate(streamId, unacknowledgedBytesRead);
@@ -384,7 +384,7 @@ public final class SpdyConnection implements Closeable {
 
   private void writePingLater(
       final boolean reply, final int payload1, final int payload2, final Ping ping) {
-    executor.submit(new NamedRunnable("OkHttp %s ping %08x%08x",
+    executor.execute(new NamedRunnable("OkHttp %s ping %08x%08x",
         hostName, payload1, payload2) {
       @Override public void execute() {
         try {
@@ -636,7 +636,7 @@ public final class SpdyConnection implements Closeable {
               inFinished, headerBlock);
           lastGoodStreamId = streamId;
           streams.put(streamId, newStream);
-          executor.submit(new NamedRunnable("OkHttp %s stream %d", hostName, streamId) {
+          executor.execute(new NamedRunnable("OkHttp %s stream %d", hostName, streamId) {
             @Override public void execute() {
               try {
                 handler.receive(newStream);
@@ -704,7 +704,7 @@ public final class SpdyConnection implements Closeable {
     }
 
     private void ackSettingsLater(final Settings peerSettings) {
-      executor.submit(new NamedRunnable("OkHttp %s ACK Settings", hostName) {
+      executor.execute(new NamedRunnable("OkHttp %s ACK Settings", hostName) {
         @Override public void execute() {
           try {
             frameWriter.ackSettings(peerSettings);
@@ -798,7 +798,7 @@ public final class SpdyConnection implements Closeable {
       }
       currentPushRequests.add(streamId);
     }
-    pushExecutor.submit(new NamedRunnable("OkHttp %s Push Request[%s]", hostName, streamId) {
+    pushExecutor.execute(new NamedRunnable("OkHttp %s Push Request[%s]", hostName, streamId) {
       @Override public void execute() {
         boolean cancel = pushObserver.onRequest(streamId, requestHeaders);
         try {
@@ -816,7 +816,7 @@ public final class SpdyConnection implements Closeable {
 
   private void pushHeadersLater(final int streamId, final List<Header> requestHeaders,
       final boolean inFinished) {
-    pushExecutor.submit(new NamedRunnable("OkHttp %s Push Headers[%s]", hostName, streamId) {
+    pushExecutor.execute(new NamedRunnable("OkHttp %s Push Headers[%s]", hostName, streamId) {
       @Override public void execute() {
         boolean cancel = pushObserver.onHeaders(streamId, requestHeaders, inFinished);
         try {
@@ -842,7 +842,7 @@ public final class SpdyConnection implements Closeable {
     source.require(byteCount); // Eagerly read the frame before firing client thread.
     source.read(buffer, byteCount);
     if (buffer.size() != byteCount) throw new IOException(buffer.size() + " != " + byteCount);
-    pushExecutor.submit(new NamedRunnable("OkHttp %s Push Data[%s]", hostName, streamId) {
+    pushExecutor.execute(new NamedRunnable("OkHttp %s Push Data[%s]", hostName, streamId) {
       @Override public void execute() {
         try {
           boolean cancel = pushObserver.onData(streamId, buffer, byteCount, inFinished);
@@ -859,7 +859,7 @@ public final class SpdyConnection implements Closeable {
   }
 
   private void pushResetLater(final int streamId, final ErrorCode errorCode) {
-    pushExecutor.submit(new NamedRunnable("OkHttp %s Push Reset[%s]", hostName, streamId) {
+    pushExecutor.execute(new NamedRunnable("OkHttp %s Push Reset[%s]", hostName, streamId) {
       @Override public void execute() {
         pushObserver.onReset(streamId, errorCode);
         synchronized (SpdyConnection.this) {

commit f5db395127820a2dfc338563784304059a9d1702

Fix MockWebServer with SPDY on Android.
Fix for issue #1552
Related to commit f78f74f from issue #1294

SpdyConnection creates a non-daemon thread to handle the readerRunnable. If
that thread dies from a RuntimeException it triggers Android's default
UncaughtExceptionHandler, which kills the test process. Previously exceptions
were being swallowed, but commit f78f74f caused the exceptions to be
propagated.

Rather than fix the problem just in SpdyConnection the fix has been
made in Util.threadFactory() for consistency. A factory method Util.newThread()
has been created and all occurances (outside of tests) of new Thread() have
been moved over.

This does alter behavior on Android across OkHttp: Previously any unexpected
thread death due to an exception or error would have taken down the Android
process. Now it will be logged to System.err, but the process will continue.

mockwebserver/src/main/java/com/squareup/okhttp/mockwebserver/MockWebServer.java
@@ -667,7 +667,7 @@ public final class MockWebServer {
         .build();
 
     // The callback might act synchronously. Give it its own thread.
-    new Thread(new Runnable() {
+    Runnable webSocketWriterRunnable = new Runnable() {
       @Override public void run() {
         try {
           listener.onOpen(webSocket, fancyRequest, fancyResponse);
@@ -676,7 +676,9 @@ public final class MockWebServer {
           connectionClose.countDown();
         }
       }
-    }, "MockWebServer WebSocket Writer " + request.getPath()).start();
+    };
+    Util.newThread("MockWebServer WebSocket Writer " + request.getPath(), false /* daemon */,
+        webSocketWriterRunnable).start();
 
     // Use this thread to continuously read messages.
     while (webSocket.readMessage()) {

okhttp-ws/src/main/java/com/squareup/okhttp/ws/WebSocketCall.java
@@ -22,7 +22,6 @@ import com.squareup.okhttp.OkHttpClient;
 import com.squareup.okhttp.Request;
 import com.squareup.okhttp.Response;
 import com.squareup.okhttp.internal.Internal;
-import com.squareup.okhttp.internal.NamedRunnable;
 import com.squareup.okhttp.internal.Util;
 import com.squareup.okhttp.internal.ws.RealWebSocket;
 import com.squareup.okhttp.internal.ws.WebSocketProtocol;
@@ -182,12 +181,14 @@ public final class WebSocketCall {
         ConnectionWebSocket.create(response, connection, source, sink, random, listener);
 
     // Start a dedicated thread for reading the web socket.
-    new Thread(new NamedRunnable("OkHttp WebSocket reader %s", request.urlString()) {
-      @Override protected void execute() {
+    Runnable webSocketReaderRunnable = new Runnable() {
+      @Override public void run() {
         while (webSocket.readMessage()) {
         }
       }
-    }).start();
+    };
+    Util.newThread(String.format("OkHttp WebSocket reader %s", request.urlString()),
+        false /* daemon */, webSocketReaderRunnable).start();
 
     // TODO connection.setOwner(webSocket);
     Internal.instance.connectionSetOwner(connection, webSocket);

okhttp/src/main/java/com/squareup/okhttp/internal/Util.java
@@ -19,6 +19,8 @@ package com.squareup.okhttp.internal;
 import java.io.Closeable;
 import java.io.IOException;
 import java.io.InterruptedIOException;
+import java.io.PrintWriter;
+import java.io.StringWriter;
 import java.io.UnsupportedEncodingException;
 import java.lang.reflect.Array;
 import java.net.ServerSocket;
@@ -47,6 +49,22 @@ public final class Util {
 
   /** A cheap and type-safe constant for the UTF-8 Charset. */
   public static final Charset UTF_8 = Charset.forName("UTF-8");
+
+  /**
+   * An UncaughtExceptionHandler to use for OkHttp threads. Android's default handler kills the
+   * process.
+   */
+  private static final Thread.UncaughtExceptionHandler UNCAUGHT_EXCEPTION_HANDLER
+      = new Thread.UncaughtExceptionHandler() {
+    @Override public void uncaughtException(Thread thread, Throwable ex) {
+      StringWriter errorText = new StringWriter(256);
+      errorText.append("Uncaught exception in OkHttp thread \"");
+      errorText.append(thread.getName());
+      errorText.append("\"\n");
+      ex.printStackTrace(new PrintWriter(errorText));
+      System.err.println(errorText.toString());
+    }
+  };
 
   private Util() {
   }
@@ -239,11 +257,16 @@ public final class Util {
   public static ThreadFactory threadFactory(final String name, final boolean daemon) {
     return new ThreadFactory() {
       @Override public Thread newThread(Runnable runnable) {
-        Thread result = new Thread(runnable, name);
-        result.setDaemon(daemon);
-        return result;
+        return Util.newThread(name, daemon, runnable);
       }
     };
+  }
+
+  public static Thread newThread(String name, boolean daemon, Runnable runnable) {
+    Thread result = new Thread(runnable, name);
+    result.setDaemon(daemon);
+    result.setUncaughtExceptionHandler(UNCAUGHT_EXCEPTION_HANDLER);
+    return result;
   }
 
   /**

okhttp/src/main/java/com/squareup/okhttp/internal/spdy/SpdyConnection.java
@@ -169,7 +169,7 @@ public final class SpdyConnection implements Closeable {
     frameWriter = variant.newWriter(Okio.buffer(Okio.sink(builder.socket)), client);
 
     readerRunnable = new Reader();
-    new Thread(readerRunnable).start(); // Not a daemon thread.
+    Util.newThread("SpdyConnection Reader", false /* daemon */, readerRunnable).start();
   }
 
   /** The protocol as selected using ALPN. */