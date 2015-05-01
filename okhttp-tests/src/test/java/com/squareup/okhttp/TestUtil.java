28434ce17d1b0bfb7f398d047e825f0f76b6e403
Artem Zinnatullin <artem.zinnatullin@gmail.com>
2015-05-01 19:47:25 +0300

    Add wildcard hostnames support in CertificatePinner

--- okhttp-tests/src/test/java/com/squareup/okhttp/TestUtil.java
+++ okhttp-tests/src/test/java/com/squareup/okhttp/TestUtil.java
@@ -2,7 +2,11 @@ package com.squareup.okhttp;
 
 import com.squareup.okhttp.internal.spdy.Header;
 import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.Collection;
+import java.util.LinkedHashSet;
 import java.util.List;
+import java.util.Set;
 
 public final class TestUtil {
   private TestUtil() {
@@ -14,5 +18,13 @@ public final class TestUtil {
       result.add(new Header(elements[i], elements[i + 1]));
     }
     return result;
+  }
+
+  public static <T> Set<T> setOf(T... elements) {
+    return setOf(Arrays.asList(elements));
+  }
+
+  public static <T> Set<T> setOf(Collection<T> elements) {
+    return new LinkedHashSet<>(elements);
   }
 }
