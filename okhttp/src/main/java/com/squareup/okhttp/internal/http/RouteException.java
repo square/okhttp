71ead1911be28c1cae1eef765abf23724b776981
Neil Fuller <nfuller@google.com>
2015-05-01 14:10:41 +0100

    Keep track of failures during negotiation for debugging

--- okhttp/src/main/java/com/squareup/okhttp/internal/http/RouteException.java
+++ okhttp/src/main/java/com/squareup/okhttp/internal/http/RouteException.java
@@ -16,27 +16,46 @@
 package com.squareup.okhttp.internal.http;
 
 import java.io.IOException;
-import java.util.ArrayList;
-import java.util.List;
+import java.lang.reflect.InvocationTargetException;
+import java.lang.reflect.Method;
 
 /**
  * An exception thrown to indicate a problem connecting via a single Route. Multiple attempts may
  * have been made with alternative protocols, none of which were successful.
  */
 public final class RouteException extends Exception {
-
-  private final List<IOException> connectExceptions = new ArrayList<IOException>();
+  private static final Method addSuppressedExceptionMethod;
+  static {
+    Method m;
+    try {
+      m = Throwable.class.getDeclaredMethod("addSuppressed", Throwable.class);
+    } catch (Exception e) {
+      m = null;
+    }
+    addSuppressedExceptionMethod = m;
+  }
+  private IOException lastException;
 
   public RouteException(IOException cause) {
     super(cause);
-    connectExceptions.add(cause);
+    lastException = cause;
   }
 
   public IOException getLastConnectException() {
-    return connectExceptions.get(connectExceptions.size() - 1);
+    return lastException;
   }
 
   public void addConnectException(IOException e) {
-    connectExceptions.add(e);
+    addSuppressedIfPossible(e, lastException);
+    lastException = e;
+  }
+
+  private void addSuppressedIfPossible(IOException e, IOException suppressed) {
+    if (addSuppressedExceptionMethod != null) {
+      try {
+        addSuppressedExceptionMethod.invoke(e, suppressed);
+      } catch (InvocationTargetException | IllegalAccessException ignored) {
+      }
+    }
   }
 }
