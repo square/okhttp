33c52112cd015c1b1a8ba689e3144faf286066b7
jwilson <jwilson@squareup.com>
2015-06-20 20:07:34 -0400

    Fix FormEncodingBuilder to be consistent with browsers.
    Right now we're using the same encode set as the URL query; that's a mistake.

--- okhttp/src/main/java/com/squareup/okhttp/HttpUrl.java
+++ okhttp/src/main/java/com/squareup/okhttp/HttpUrl.java
@@ -256,10 +256,11 @@ public final class HttpUrl {
   private static final char[] HEX_DIGITS =
       { '0', '1', '2', '3', '4', '5', '6', '7', '8', '9', 'A', 'B', 'C', 'D', 'E', 'F' };
   static final String USERNAME_ENCODE_SET = " \"':;<=>@[]^`{}|/\\?#";
-  static final String PASSWORD_ENCODE_SET = " \"':;<=>@[]\\^`{}|/\\?#";
+  static final String PASSWORD_ENCODE_SET = " \"':;<=>@[]^`{}|/\\?#";
   static final String PATH_SEGMENT_ENCODE_SET = " \"<>^`{}|/\\?#";
   static final String QUERY_ENCODE_SET = " \"'<>#";
   static final String QUERY_COMPONENT_ENCODE_SET = " \"'<>#&=";
+  static final String FORM_ENCODE_SET = " \"':;<=>@[]^`{}|/\\?#&!$(),~";
   static final String FRAGMENT_ENCODE_SET = "";
 
   /** Either "http" or "https". */
