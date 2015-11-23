ee1dee1322da49e14672306846d477eefe5ae41d
Artem Zinnatullin <artem.zinnatullin@gmail.com>
2015-11-23 05:35:43 +0300

    Add null check to HttpLoggingInterceptor.setLevel()

--- okhttp-logging-interceptor/src/main/java/com/squareup/okhttp/logging/HttpLoggingInterceptor.java
+++ okhttp-logging-interceptor/src/main/java/com/squareup/okhttp/logging/HttpLoggingInterceptor.java
@@ -125,6 +125,7 @@ public final class HttpLoggingInterceptor implements Interceptor {
 
   /** Change the level at which this interceptor logs. */
   public HttpLoggingInterceptor setLevel(Level level) {
+    if (level == null) throw new NullPointerException("level == null. Use Level.NONE instead.");
     this.level = level;
     return this;
   }
