6c964d5753dbedcafff2638c678fbf90ccc9c8db
Yuri Schimke <yuri@schimke.ee>
2015-12-19 08:09:53 -0800

    handle POST requests

--- okhttp-twitter/src/main/java/okhttp/twitter/TwitterAuthInterceptor.java
+++ okhttp-twitter/src/main/java/okhttp/twitter/TwitterAuthInterceptor.java
@@ -4,7 +4,11 @@ import com.google.common.base.Joiner;
 import com.twitter.joauth.Normalizer;
 import com.twitter.joauth.OAuthParams;
 import com.twitter.joauth.Signer;
+import com.twitter.joauth.UrlCodec;
+import com.twitter.joauth.keyvalue.KeyValueHandler;
+import com.twitter.joauth.keyvalue.KeyValueParser;
 import java.io.IOException;
+import java.nio.charset.Charset;
 import java.security.InvalidKeyException;
 import java.security.NoSuchAlgorithmException;
 import java.security.SecureRandom;
@@ -16,9 +20,12 @@ import java.util.HashSet;
 import java.util.List;
 import java.util.Map;
 import java.util.Set;
+import okhttp3.FormBody;
 import okhttp3.Interceptor;
 import okhttp3.Request;
+import okhttp3.RequestBody;
 import okhttp3.Response;
+import okio.Buffer;
 
 public class TwitterAuthInterceptor implements Interceptor {
   private final SecureRandom secureRandom = new SecureRandom();
@@ -72,7 +79,7 @@ public class TwitterAuthInterceptor implements Interceptor {
   }
 
   private String generateAuthorization(Request request)
-      throws NoSuchAlgorithmException, InvalidKeyException {
+      throws NoSuchAlgorithmException, InvalidKeyException, IOException {
     long timestampSecs = generateTimestamp();
     String nonce = generateNonce();
 
@@ -85,6 +92,46 @@ public class TwitterAuthInterceptor implements Interceptor {
     );
 
     List<com.twitter.joauth.Request.Pair> javaParams = new ArrayList<>();
+
+    Set<String> queryParamNames = request.url().queryParameterNames();
+    for (String queryParam : queryParamNames) {
+      List<String> values = request.url().queryParameterValues(queryParam);
+
+      for (String value : values) {
+        javaParams.add(new com.twitter.joauth.Request.Pair(UrlCodec.encode(queryParam),
+            UrlCodec.encode(value)));
+      }
+    }
+
+    if (request.method().equals("POST")) {
+      System.out.println(request.header("Content-Type"));
+
+      RequestBody body = request.body();
+
+      if (body instanceof FormBody) {
+        FormBody formBody = (FormBody) body;
+
+        for (int i = 0; i < formBody.size(); i++) {
+          javaParams.add(new com.twitter.joauth.Request.Pair(formBody.encodedName(i),
+              formBody.encodedValue(i)));
+        }
+      } else if (isFormContentType(request)) {
+        Buffer buffer = new Buffer();
+        body.writeTo(buffer);
+        String encodedBody = buffer.readString(Charset.forName("UTF-8"));
+
+        KeyValueHandler.DuplicateKeyValueHandler handler =
+            new KeyValueHandler.DuplicateKeyValueHandler();
+
+        KeyValueParser.StandardKeyValueParser bodyParser =
+            new KeyValueParser.StandardKeyValueParser("&", "=");
+        bodyParser.parse(encodedBody, Arrays.<KeyValueHandler>asList(handler));
+
+        System.out.println(handler.toList());
+
+        javaParams.addAll(handler.toList());
+      }
+    }
 
     String normalized = normalizer.normalize(
         request.isHttps() ? "https" : "http", request.url().host(), request.url().port(),
@@ -103,5 +150,11 @@ public class TwitterAuthInterceptor implements Interceptor {
     oauthHeaders.put(OAuthParams.OAUTH_VERSION, quoted(OAuthParams.ONE_DOT_OH));
 
     return "OAuth " + Joiner.on(", ").withKeyValueSeparator("=").join(oauthHeaders);
+  }
+
+  private boolean isFormContentType(Request request) {
+    // TODO should we consider case? or parse properly?
+    String contentType = request.header("Content-Type");
+    return contentType != null && contentType.startsWith("application/x-www-form-urlencoded");
   }
 }
