1e2e8e1477794d8839c3e469738b898c5ae56c4c
jwilson <jwilson@squareup.com>
2016-03-14 21:42:19 -0400

    Fix a typo in the interceptor docs.

--- okhttp/src/main/java/okhttp3/Interceptor.java
+++ okhttp/src/main/java/okhttp3/Interceptor.java
@@ -19,8 +19,8 @@ import java.io.IOException;
 
 /**
  * Observes, modifies, and potentially short-circuits requests going out and the corresponding
- * requests coming back in. Typically interceptors will be used to add, remove, or transform headers
- * on the request or response.
+ * responses coming back in. Typically interceptors add, remove, or transform headers on the request
+ * or response.
  */
 public interface Interceptor {
   Response intercept(Chain chain) throws IOException;
