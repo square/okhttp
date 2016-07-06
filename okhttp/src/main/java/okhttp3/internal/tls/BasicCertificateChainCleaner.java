8688f539f17b5fe1be9ffc8bf167b88b1558084a
jwilson <jwilson@squareup.com>
2016-07-05 20:06:43 -0400

    More docs for HttpUrl.
    Closes: https://github.com/square/okhttp/issues/2577
    Closes: https://github.com/square/okhttp/issues/2263

--- okhttp/src/main/java/okhttp3/internal/tls/BasicCertificateChainCleaner.java
+++ okhttp/src/main/java/okhttp3/internal/tls/BasicCertificateChainCleaner.java
@@ -29,7 +29,7 @@ import javax.net.ssl.SSLPeerUnverifiedException;
  * A certificate chain cleaner that uses a set of trusted root certificates to build the trusted
  * chain. This class duplicates the clean chain building performed during the TLS handshake. We
  * prefer other mechanisms where they exist, such as with
- * {@link okhttp3.internal.AndroidPlatform.AndroidCertificateChainCleaner}.
+ * {@code okhttp3.internal.platform.AndroidPlatform.AndroidCertificateChainCleaner}.
  *
  * <p>This class includes code from <a href="https://conscrypt.org/">Conscrypt's</a> {@code
  * TrustManagerImpl} and {@code TrustedCertificateIndex}.
