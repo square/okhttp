d2539622e011409450b526e4eaa7011e29632567
jwilson <jwilson@squareup.com>
2016-07-10 11:34:02 -0400

    Fix HPACK tests to use the http2 package.

--- okhttp-hpacktests/src/test/java/okhttp3/internal/http2/hpackjson/Case.java
+++ okhttp-hpacktests/src/test/java/okhttp3/internal/http2/hpackjson/Case.java
@@ -19,7 +19,7 @@ import java.util.ArrayList;
 import java.util.LinkedHashMap;
 import java.util.List;
 import java.util.Map;
-import okhttp3.internal.framed.Header;
+import okhttp3.internal.http2.Header;
 import okio.ByteString;
 
 /**
