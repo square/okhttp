ce0aad2a5d66ed94b75f386d4783a5c60dc9c8e6
Jake Wharton <jakewharton@gmail.com>
2017-02-22 21:47:41 -0500

    Fix formatting

--- okhttp-urlconnection/src/main/java/okhttp3/JavaNetCookieJar.java
+++ okhttp-urlconnection/src/main/java/okhttp3/JavaNetCookieJar.java
@@ -22,7 +22,6 @@ import java.util.ArrayList;
 import java.util.Collections;
 import java.util.List;
 import java.util.Map;
-
 import okhttp3.internal.platform.Platform;
 
 import static okhttp3.internal.Util.delimiterOffset;
