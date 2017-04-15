c9496d350c0c3bb4b9d9571c3ab97181726561e2
jwilson <jwilson@squareup.com>
2017-04-15 13:08:46 -0400

    Invalidate the cache even if the response has no body.
    Closes: https://github.com/square/okhttp/issues/3203

--- okhttp/src/main/java/okhttp3/internal/http2/Settings.java
+++ okhttp/src/main/java/okhttp3/internal/http2/Settings.java
@@ -94,7 +94,6 @@ public final class Settings {
     return ((bit & set) != 0 ? values[ENABLE_PUSH] : defaultValue ? 1 : 0) == 1;
   }
 
-  // TODO: honor this setting.
   int getMaxConcurrentStreams(int defaultValue) {
     int bit = 1 << MAX_CONCURRENT_STREAMS;
     return (bit & set) != 0 ? values[MAX_CONCURRENT_STREAMS] : defaultValue;
