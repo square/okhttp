3819ed03996f1497512bffb445f81398a0bbaa17
Eric Gribkoff <ericgribkoff@google.com>
2017-05-25 15:24:49 -0700

    Avoid NPE with Http/2 settings id in the experimental range

--- okhttp/src/main/java/okhttp3/internal/http2/Settings.java
+++ okhttp/src/main/java/okhttp3/internal/http2/Settings.java
@@ -56,7 +56,7 @@ public final class Settings {
   }
 
   Settings set(int id, int value) {
-    if (id >= values.length) {
+    if (id < 0 || id >= values.length) {
       return this; // Discard unknown settings.
     }
 
