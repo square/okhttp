05abd64b9c2a2d74ac553074c0d2be32a9b60c96
ericaschulz <ericalynnschulz@gmail.com>
2017-06-27 22:21:58 +1000

    New method, withReadTimeout(), on Interceptor.Chain

--- okhttp/src/main/java/okhttp3/Interceptor.java
+++ okhttp/src/main/java/okhttp3/Interceptor.java
@@ -16,6 +16,7 @@
 package okhttp3;
 
 import java.io.IOException;
+import java.util.concurrent.TimeUnit;
 import javax.annotation.Nullable;
 
 /**
@@ -36,5 +37,9 @@ public interface Interceptor {
      * of network interceptors; for application interceptors this is always null.
      */
     @Nullable Connection connection();
+
+    int readTimeoutMillis();
+
+    Chain withReadTimeout(int timeout, TimeUnit unit);
   }
 }
