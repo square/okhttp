9ed8f9a37d6aced6b211d98c51a043488541fbb3
Yuri Schimke <yschimke@fb.com>
2017-07-17 06:40:34 -0700

    sample events code

--- okhttp/src/main/java/okhttp3/internal/http/CallServerInterceptor.java
+++ okhttp/src/main/java/okhttp3/internal/http/CallServerInterceptor.java
@@ -43,36 +43,62 @@ public final class CallServerInterceptor implements Interceptor {
     Request request = realChain.request();
 
     long sentRequestMillis = System.currentTimeMillis();
-    httpCodec.writeRequestHeaders(request);
+
+    realChain.eventListener().requestHeadersStart(realChain.call());
+    try {
+      httpCodec.writeRequestHeaders(request);
+      realChain.eventListener().requestHeadersEnd(realChain.call(), null);
+    } catch (IOException ioe) {
+      realChain.eventListener().requestHeadersEnd(realChain.call(), ioe);
+      throw ioe;
+    }
 
     Response.Builder responseBuilder = null;
+    realChain.eventListener().requestBodyStart(realChain.call());
+    try {
     if (HttpMethod.permitsRequestBody(request.method()) && request.body() != null) {
       // If there's a "Expect: 100-continue" header on the request, wait for a "HTTP/1.1 100
       // Continue" response before transmitting the request body. If we don't get that, return what
       // we did get (such as a 4xx response) without ever transmitting the request body.
       if ("100-continue".equalsIgnoreCase(request.header("Expect"))) {
         httpCodec.flushRequest();
+        // TODO event listener
         responseBuilder = httpCodec.readResponseHeaders(true);
       }
 
-      if (responseBuilder == null) {
-        // Write the request body if the "Expect: 100-continue" expectation was met.
-        Sink requestBodyOut = httpCodec.createRequestBody(request, request.body().contentLength());
-        BufferedSink bufferedRequestBody = Okio.buffer(requestBodyOut);
-        request.body().writeTo(bufferedRequestBody);
-        bufferedRequestBody.close();
-      } else if (!connection.isMultiplexed()) {
-        // If the "Expect: 100-continue" expectation wasn't met, prevent the HTTP/1 connection from
-        // being reused. Otherwise we're still obligated to transmit the request body to leave the
-        // connection in a consistent state.
-        streamAllocation.noNewStreams();
-      }
+        if (responseBuilder == null) {
+          // Write the request body if the "Expect: 100-continue" expectation was met.
+          Sink requestBodyOut =
+              httpCodec.createRequestBody(request, request.body().contentLength());
+          BufferedSink bufferedRequestBody = Okio.buffer(requestBodyOut);
+
+            request.body().writeTo(bufferedRequestBody);
+          bufferedRequestBody.close();
+        } else if (!connection.isMultiplexed()) {
+          // If the "Expect: 100-continue" expectation wasn't met, prevent the HTTP/1 connection
+          // from being reused. Otherwise we're still obligated to transmit the request body to
+          // leave the connection in a consistent state.
+          streamAllocation.noNewStreams();
+        }
+    }
+
+      realChain.eventListener().requestBodyEnd(realChain.call(), null);
+    } catch (IOException ioe) {
+      realChain.eventListener().requestBodyEnd(realChain.call(), ioe);
+      throw ioe;
     }
 
     httpCodec.finishRequest();
 
     if (responseBuilder == null) {
-      responseBuilder = httpCodec.readResponseHeaders(false);
+      realChain.eventListener().responseHeadersStart(realChain.call());
+      try {
+        responseBuilder = httpCodec.readResponseHeaders(false);
+        realChain.eventListener().responseHeadersEnd(realChain.call(), null);
+      } catch (IOException ioe) {
+        realChain.eventListener().responseHeadersEnd(realChain.call(), ioe);
+        throw ioe;
+      }
     }
 
     Response response = responseBuilder
@@ -82,26 +108,35 @@ public final class CallServerInterceptor implements Interceptor {
         .receivedResponseAtMillis(System.currentTimeMillis())
         .build();
 
-    int code = response.code();
-    if (forWebSocket && code == 101) {
-      // Connection is upgrading, but we need to ensure interceptors see a non-null response body.
-      response = response.newBuilder()
-          .body(Util.EMPTY_RESPONSE)
-          .build();
-    } else {
-      response = response.newBuilder()
-          .body(httpCodec.openResponseBody(response))
-          .build();
-    }
+    realChain.eventListener().responseBodyStart(realChain.call());
+    try {
+      int code = response.code();
+      if (forWebSocket && code == 101) {
+        // Connection is upgrading, but we need to ensure interceptors see a non-null response body.
+        response = response.newBuilder()
+            .body(Util.EMPTY_RESPONSE)
+            .build();
+      } else {
+          response = response.newBuilder()
+              .body(httpCodec.openResponseBody(response))
+              .build();
+      }
 
-    if ("close".equalsIgnoreCase(response.request().header("Connection"))
-        || "close".equalsIgnoreCase(response.header("Connection"))) {
-      streamAllocation.noNewStreams();
-    }
+      if ("close".equalsIgnoreCase(response.request().header("Connection"))
+          || "close".equalsIgnoreCase(response.header("Connection"))) {
+        streamAllocation.noNewStreams();
+      }
 
-    if ((code == 204 || code == 205) && response.body().contentLength() > 0) {
-      throw new ProtocolException(
-          "HTTP " + code + " had non-zero Content-Length: " + response.body().contentLength());
+      if ((code == 204 || code == 205) && response.body().contentLength() > 0) {
+        throw new ProtocolException(
+            "HTTP " + code + " had non-zero Content-Length: " + response.body().contentLength());
+      }
+
+      // TODO this is too early, response may not be read
+      realChain.eventListener().responseBodyEnd(realChain.call(), null);
+    } catch (IOException ioe) {
+      realChain.eventListener().responseBodyEnd(realChain.call(), ioe);
+      throw ioe;
     }
 
     return response;
