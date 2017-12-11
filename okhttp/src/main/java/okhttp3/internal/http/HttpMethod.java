727bc469304374857763d7a1dc053dc5b586675a
Mysterious <ehsan.mashhadi@gmail.com>
2017-12-11 11:06:19 +0330

    Return true on permitRequestBody

--- okhttp/src/main/java/okhttp3/internal/http/HttpMethod.java
+++ okhttp/src/main/java/okhttp3/internal/http/HttpMethod.java
@@ -16,40 +16,35 @@
 package okhttp3.internal.http;
 
 public final class HttpMethod {
-  public static boolean invalidatesCache(String method) {
-    return method.equals("POST")
-        || method.equals("PATCH")
-        || method.equals("PUT")
-        || method.equals("DELETE")
-        || method.equals("MOVE");     // WebDAV
-  }
+    public static boolean invalidatesCache(String method) {
+        return method.equals("POST")
+                || method.equals("PATCH")
+                || method.equals("PUT")
+                || method.equals("DELETE")
+                || method.equals("MOVE");     // WebDAV
+    }
 
-  public static boolean requiresRequestBody(String method) {
-    return method.equals("POST")
-        || method.equals("PUT")
-        || method.equals("PATCH")
-        || method.equals("PROPPATCH") // WebDAV
-        || method.equals("REPORT");   // CalDAV/CardDAV (defined in WebDAV Versioning)
-  }
+    public static boolean requiresRequestBody(String method) {
+        return method.equals("POST")
+                || method.equals("PUT")
+                || method.equals("PATCH")
+                || method.equals("PROPPATCH") // WebDAV
+                || method.equals("REPORT");   // CalDAV/CardDAV (defined in WebDAV Versioning)
+    }
 
-  public static boolean permitsRequestBody(String method) {
-    return requiresRequestBody(method)
-        || method.equals("OPTIONS")
-        || method.equals("DELETE")    // Permitted as spec is ambiguous.
-        || method.equals("PROPFIND")  // (WebDAV) without body: request <allprop/>
-        || method.equals("MKCOL")     // (WebDAV) may contain a body, but behaviour is unspecified
-        || method.equals("LOCK");     // (WebDAV) body: create lock, without body: refresh lock
-  }
+    public static boolean permitsRequestBody(String method) {
+        return true;
+    }
 
-  public static boolean redirectsWithBody(String method) {
-    return method.equals("PROPFIND"); // (WebDAV) redirects should also maintain the request body
-  }
+    public static boolean redirectsWithBody(String method) {
+        return method.equals("PROPFIND"); // (WebDAV) redirects should also maintain the request body
+    }
 
-  public static boolean redirectsToGet(String method) {
-    // All requests but PROPFIND should redirect to a GET request.
-    return !method.equals("PROPFIND");
-  }
+    public static boolean redirectsToGet(String method) {
+        // All requests but PROPFIND should redirect to a GET request.
+        return !method.equals("PROPFIND");
+    }
 
-  private HttpMethod() {
-  }
+    private HttpMethod() {
+    }
 }
