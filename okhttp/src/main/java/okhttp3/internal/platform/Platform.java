bdbe2dad8806a77bf018b87f4518df250fa67f89
Yuri Schimke <yuri@schimke.ee>
2018-06-30 14:32:10 +0100

    Android 4 and Java 1.7 prefer TLSv1.2 provider (#4089)
    * java 1.7 TLV v1.2 support
    
    * simplify TLSv1.2 vs TLS selection logic
    
    * tighten up version checks
    
    * cleanup

--- okhttp/src/main/java/okhttp3/internal/platform/Platform.java
+++ okhttp/src/main/java/okhttp3/internal/platform/Platform.java
@@ -124,8 +124,8 @@ public class Platform {
     return null;
   }
 
-  public void connectSocket(Socket socket, InetSocketAddress address,
-      int connectTimeout) throws IOException {
+  public void connectSocket(Socket socket, InetSocketAddress address, int connectTimeout)
+      throws IOException {
     socket.connect(address, connectTimeout);
   }
 
@@ -176,8 +176,10 @@ public class Platform {
     X509TrustManager trustManager = trustManager(sslSocketFactory);
 
     if (trustManager == null) {
-      throw new IllegalStateException("Unable to extract the trust manager on " + Platform.get()
-          + ", sslSocketFactory is " + sslSocketFactory.getClass());
+      throw new IllegalStateException("Unable to extract the trust manager on "
+          + Platform.get()
+          + ", sslSocketFactory is "
+          + sslSocketFactory.getClass());
     }
 
     return buildCertificateChainCleaner(trustManager);
@@ -265,6 +267,16 @@ public class Platform {
   }
 
   public SSLContext getSSLContext() {
+    String jvmVersion = System.getProperty("java.specification.version");
+    if ("1.7".equals(jvmVersion)) {
+      try {
+        // JDK 1.7 (public version) only support > TLSv1 with named protocols
+        return SSLContext.getInstance("TLSv1.2");
+      } catch (NoSuchAlgorithmException e) {
+        // fallback to TLS
+      }
+    }
+
     try {
       return SSLContext.getInstance("TLS");
     } catch (NoSuchAlgorithmException e) {
