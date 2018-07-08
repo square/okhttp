cf805ae0faba0430cf4437801147fced248eb1ea
Yuri Schimke <yuri@schimke.ee>
2018-07-08 12:53:54 +0100

    coroutines test

---
+++ okhttp-kotlin/src/test/kotlin/okhttp3/kotlin/CoroutinesTest.kt
@@ -0,0 +1,21 @@
+package okhttp3.kotlin
+
+import kotlinx.coroutines.experimental.runBlocking
+import org.junit.Test
+import java.util.concurrent.TimeUnit.MILLISECONDS
+import kotlin.test.assertEquals
+
+class CoroutinesTest {
+    @Test
+    fun `test suspend execute`() {
+        val request = request("https://google.com/robots.txt")
+        val client = client {
+            readTimeout(5000, MILLISECONDS)
+        }
+        val response = runBlocking {
+            client.execute(request)
+        }
+        assertEquals(200, response.code())
+        assertEquals("User-agent: *", response.body()!!.string().substring(0, 13))
+    }
+}
