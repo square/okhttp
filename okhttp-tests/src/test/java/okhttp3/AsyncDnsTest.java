7e4bdb2b57814c314e06bd3eefa7f5d014cfdd1e
Galder Zamarreño <galder@zamarreno.com>
2019-02-15 16:45:20 +0100

    Use mock map to test async dns cancel/timeout scenarios

--- okhttp-tests/src/test/java/okhttp3/AsyncDnsTest.java
+++ okhttp-tests/src/test/java/okhttp3/AsyncDnsTest.java
@@ -1,51 +1,83 @@
 package okhttp3;
 
-import java.lang.reflect.Constructor;
-import java.lang.reflect.Field;
-import java.lang.reflect.InvocationTargetException;
-import java.lang.reflect.Method;
 import java.net.InetAddress;
 import java.net.UnknownHostException;
-import java.util.*;
+import java.util.Arrays;
+import java.util.Comparator;
+import java.util.List;
+import java.util.concurrent.CompletableFuture;
 import java.util.concurrent.ExecutorService;
 import java.util.concurrent.Executors;
 import java.util.concurrent.TimeUnit;
-import java.util.stream.Collectors;
-
-import javafx.util.Duration;
+import java.util.concurrent.TimeoutException;
 import org.junit.After;
+import org.junit.AfterClass;
 import org.junit.Test;
-import sun.net.util.IPAddressUtil;
 
 import static java.util.Collections.singletonList;
 import static org.junit.Assert.assertEquals;
+import static org.junit.Assert.fail;
 
 public class AsyncDnsTest {
 
   private static final ExecutorService executor = Executors.newCachedThreadPool();
 
   @Test public void getOne() throws Exception {
-    //MockDnsCache.when("a.bc", "1.2.3.4");
-    MockDnsCache.when("a.bc", "1.2.3.4");
+    MockDnsCache.when("a.bc").thenReturn("1.2.3.4");
 
     final AsyncDns dns = new AsyncDns.Builder().executor(executor).build();
-    final List<InetAddress> result = dns.lookupAsync("a.bc").get(5, TimeUnit.SECONDS);
+    final List<InetAddress> result = dns.lookupAsync("a.bc").get(10, TimeUnit.SECONDS);
     assertEquals(singletonList(address("1.2.3.4")), result);
   }
 
   @Test public void getMulti() throws Exception {
-    //MockDnsCache.when("b.cd", "2.3.4.5", "3.4.5.6");
-    MockDnsCache.when("b.cd", "2.3.4.5", "3.4.5.6");
+    MockDnsCache.when("b.cd").thenReturn("2.3.4.5", "3.4.5.6");
 
     final AsyncDns dns = new AsyncDns.Builder().executor(executor).build();
-    final List<InetAddress> result = dns.lookupAsync("b.cd").get(5, TimeUnit.SECONDS);
+    final List<InetAddress> result = dns.lookupAsync("b.cd").get(10, TimeUnit.SECONDS);
     result.sort(new InetAdressComparator());
 
     assertEquals(Arrays.asList(address("2.3.4.5"), address("3.4.5.6")), result);
   }
+
+  @Test public void cancel() {
+    MockDnsCache.when("cancel.do").thenAnswer(() -> {
+      try {
+        Thread.sleep(2000);
+      } catch (InterruptedException e) {
+        Thread.currentThread().interrupt();
+      }
+      return null;
+    });
+
+    final AsyncDns dns = new AsyncDns.Builder().executor(executor).build();
+    final CompletableFuture<List<InetAddress>> future = dns.lookupAsync("cancel.do");
+    if (!future.isDone())
+      future.cancel(true);
+    else
+      fail("Should not be done");
+  }
+
+  @Test(expected = TimeoutException.class) public void timeout() throws Exception {
+    MockDnsCache.when("timeout.do").thenAnswer(() -> {
+      try {
+        Thread.sleep(2000);
+      } catch (InterruptedException e) {
+        Thread.currentThread().interrupt();
+      }
+      return null;
+    });
+
+    final AsyncDns dns = new AsyncDns.Builder().executor(executor).build();
+    dns.lookupAsync("b.cd").get(4, TimeUnit.SECONDS);
+  }
   
   @After public void clearDnsCache() {
     MockDnsCache.clear();
+  }
+
+  @AfterClass public static void shutdownExecutor() {
+    executor.shutdownNow();
   }
 
   private static InetAddress address(String host) {
--- okhttp-tests/src/test/java/okhttp3/MockDnsCache.java
+++ okhttp-tests/src/test/java/okhttp3/MockDnsCache.java
@@ -2,16 +2,13 @@ package okhttp3;
 
 import java.lang.reflect.Constructor;
 import java.lang.reflect.Field;
-import java.lang.reflect.InvocationTargetException;
 import java.lang.reflect.Method;
 import java.net.InetAddress;
 import java.net.UnknownHostException;
-import java.util.AbstractMap;
 import java.util.Arrays;
 import java.util.HashMap;
 import java.util.LinkedHashMap;
 import java.util.Map;
-import java.util.Set;
 import java.util.function.Supplier;
 import java.util.stream.Collectors;
 import javafx.util.Duration;
@@ -19,22 +16,11 @@ import sun.net.util.IPAddressUtil;
 
 final class MockDnsCache {
   // TODO is synchronization required for access/update?
-  //private static LinkedHashMap mockPositiveCache;
-  //private static Map<String, StubImpl> stubs = new HashMap<>();
-
-  private static Map<String, Object> positiveCache;
+  private static Map<String, ?> mockPositiveCache;
+  private static Map<String, StubImpl> stubs = new HashMap<>();
 
   static {
     try {
-      ////final LinkedHashMap test2 = new LinkedHashMap();
-      //final Field testField = MockDnsCache.class.getDeclaredField("test");
-      //System.out.println(testField.getDeclaringClass());
-      ////System.out.println(testField.getDeclaringClass());
-      ////System.out.println(test2.getClass());
-      ////final boolean assignableFrom = testField.getDeclaringClass().isAssignableFrom(test2.getClass());
-      ////
-      ////System.out.println(assignableFrom);
-
       final Field addressCacheField = InetAddress.class.getDeclaredField("addressCache");
       addressCacheField.setAccessible(true);
       final Object addressCache = addressCacheField.get(InetAddress.class);
@@ -45,30 +31,19 @@ final class MockDnsCache {
 
       final Field cacheField = addressCache.getClass().getDeclaredField("cache");
       cacheField.setAccessible(true);
-      positiveCache = (Map<String, Object>) cacheField.get(addressCache);
 
-      //when(positiveCache.get("a")).thenAnswer(x -> {
-      //  throw new RuntimeException("faked");
-      //});
-
-      //mockPositiveCache = new MockMap(positiveCache, stubs);
-      //mockPositiveCache = new LinkedHashMap();
-      //
-      //cacheField.set(mockPositiveCache, addressCache);
+      Map<String, ?> positiveCache = (Map<String, ?>) cacheField.get(addressCache);
+      mockPositiveCache = new MockLinkedHashMap(positiveCache, stubs);
+      cacheField.set(addressCache, mockPositiveCache);
     } catch (Exception e) {
       throw new AssertionError(e);
     }
   }
 
-  //static Stub when(String host) {
-  //  final StubImpl stub = new StubImpl(host);
-  //  stubs.put(host, stub);
-  //  return stub;
-  //}
-
-  static void when(String host, String... ips) {
-    final Object dnsEntry = createCacheEntry(host, ips);
-    positiveCache.put(host, dnsEntry);
+  static Stub when(String host) {
+    final StubImpl stub = new StubImpl(host);
+    stubs.put(host, stub);
+    return stub;
   }
 
   private static Object createCacheEntry(String host, String[] ips) {
@@ -77,7 +52,7 @@ final class MockDnsCache {
       final Constructor<?> ctor = clazz.getDeclaredConstructors()[0];
       ctor.setAccessible(true);
 
-      final InetAddress[] addresses = Arrays.asList(ips).stream()
+      final InetAddress[] addresses = Arrays.stream(ips)
           .map(IPAddressUtil::textToNumericFormatV4)
           .map(bytes -> getByAddress(host, bytes))
           .collect(Collectors.toList())
@@ -92,8 +67,7 @@ final class MockDnsCache {
   }
 
   static void clear() {
-    //mockPositiveCache.clear();
-    positiveCache.clear();
+    mockPositiveCache.clear();
   }
 
   private static InetAddress getByAddress(String host, byte[] bytes) {
@@ -126,11 +100,11 @@ final class MockDnsCache {
     }
   }
 
-    public static final class MockMap extends LinkedHashMap {
-      private final Map<String, Object> delegate;
+    public static final class MockLinkedHashMap extends LinkedHashMap<String, Object> {
+      private final Map<String, ?> delegate;
       private final Map<String, StubImpl> stubs;
 
-      private MockMap(Map<String, Object> delegate, Map<String, StubImpl> stubs) {
+      private MockLinkedHashMap(Map<String, ?> delegate, Map<String, StubImpl> stubs) {
         this.delegate = delegate;
         this.stubs = stubs;
       }
@@ -143,13 +117,5 @@ final class MockDnsCache {
         return delegate.get(key);
       }
     }
-
-  //static class ValueStub implements Stub {
-  //  final Object value;
-  //
-  //  ValueStub(Object value) {
-  //    this.value = value;
-  //  }
-  //}
 
 }
