a754116b5a1e5dee8f3def9a653ccbce1f1947fa
Yuri Schimke <yuri@schimke.ee>
2019-03-22 19:24:22 +0000

    Review comments

--- okhttp/src/main/java/okhttp3/internal/connection/RouteException.kt
+++ okhttp/src/main/java/okhttp3/internal/connection/RouteException.kt
@@ -23,12 +23,7 @@ import java.io.IOException;
  */
 class RouteException internal constructor(val firstConnectException: IOException) :
     RuntimeException(firstConnectException) {
-  var lastConnectException: IOException? = null
-    private set
-
-  init {
-    lastConnectException = firstConnectException
-  }
+  var lastConnectException: IOException = firstConnectException
 
   fun addConnectException(e: IOException) {
     firstConnectException.addSuppressed(e)
