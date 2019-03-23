1fea22632d00e0f5e14bf677103ea7eea1603a78
Jesse Wilson <jwilson@squareup.com>
2019-03-23 14:41:49 -0500

    Convert MediaType to Kotlin

--- okhttp/src/main/java/okhttp3/MediaType.java
+++ okhttp/src/main/java/okhttp3/MediaType.java
@@ -13,99 +13,32 @@
  * See the License for the specific language governing permissions and
  * limitations under the License.
  */
-package okhttp3;
+package okhttp3
 
-import java.nio.charset.Charset;
-import java.util.Locale;
-import java.util.regex.Matcher;
-import java.util.regex.Pattern;
-import javax.annotation.Nullable;
+import java.nio.charset.Charset
+import java.util.Locale
+import java.util.regex.Pattern
 
 /**
- * An <a href="http://tools.ietf.org/html/rfc2045">RFC 2045</a> Media Type, appropriate to describe
- * the content type of an HTTP request or response body.
+ * An [RFC 2045](http://tools.ietf.org/html/rfc2045) Media Type, appropriate to describe the
+ * content type of an HTTP request or response body.
  */
-public final class MediaType {
-  private static final String TOKEN = "([a-zA-Z0-9-!#$%&'*+.^_`{|}~]+)";
-  private static final String QUOTED = "\"([^\"]*)\"";
-  private static final Pattern TYPE_SUBTYPE = Pattern.compile(TOKEN + "/" + TOKEN);
-  private static final Pattern PARAMETER = Pattern.compile(
-      ";\\s*(?:" + TOKEN + "=(?:" + TOKEN + "|" + QUOTED + "))?");
-
-  private final String mediaType;
-  private final String type;
-  private final String subtype;
-  private final @Nullable String charset;
-
-  private MediaType(String mediaType, String type, String subtype, @Nullable String charset) {
-    this.mediaType = mediaType;
-    this.type = type;
-    this.subtype = subtype;
-    this.charset = charset;
-  }
-
+class MediaType private constructor(
+  private val mediaType: String,
+  val type: String,
+  val subtype: String,
+  private val charset: String?
+) {
   /**
-   * Returns a media type for {@code string}.
-   *
-   * @throws IllegalArgumentException if {@code string} is not a well-formed media type.
+   * Returns the charset of this media type, or `defaultValue` if either this media type
+   * doesn't specify a charset, of it its charset is unsupported by the current runtime.
    */
-  public static MediaType get(String string) {
-    Matcher typeSubtype = TYPE_SUBTYPE.matcher(string);
-    if (!typeSubtype.lookingAt()) {
-      throw new IllegalArgumentException("No subtype found for: \"" + string + '"');
-    }
-    String type = typeSubtype.group(1).toLowerCase(Locale.US);
-    String subtype = typeSubtype.group(2).toLowerCase(Locale.US);
-
-    String charset = null;
-    Matcher parameter = PARAMETER.matcher(string);
-    for (int s = typeSubtype.end(); s < string.length(); s = parameter.end()) {
-      parameter.region(s, string.length());
-      if (!parameter.lookingAt()) {
-        throw new IllegalArgumentException("Parameter is not formatted correctly: \""
-            + string.substring(s)
-            + "\" for: \""
-            + string
-            + '"');
-      }
-
-      String name = parameter.group(1);
-      if (name == null || !name.equalsIgnoreCase("charset")) continue;
-      String charsetParameter;
-      String token = parameter.group(2);
-      if (token != null) {
-        // If the token is 'single-quoted' it's invalid! But we're lenient and strip the quotes.
-        charsetParameter = (token.startsWith("'") && token.endsWith("'") && token.length() > 2)
-            ? token.substring(1, token.length() - 1)
-            : token;
-      } else {
-        // Value is "double-quoted". That's valid and our regex group already strips the quotes.
-        charsetParameter = parameter.group(3);
-      }
-      if (charset != null && !charsetParameter.equalsIgnoreCase(charset)) {
-        throw new IllegalArgumentException("Multiple charsets defined: \""
-            + charset
-            + "\" and: \""
-            + charsetParameter
-            + "\" for: \""
-            + string
-            + '"');
-      }
-      charset = charsetParameter;
-    }
-
-    return new MediaType(string, type, subtype, charset);
-  }
-
-  /**
-   * Returns a media type for {@code string}, or null if {@code string} is not a well-formed media
-   * type.
-   */
-  public static @Nullable MediaType parse(String string) {
+  @JvmOverloads
+  fun charset(defaultValue: Charset? = null): Charset? {
     try {
-      return get(string);
-    } catch (IllegalArgumentException ignored) {
-      return null;
+      return if (charset != null) Charset.forName(charset) else defaultValue
+    } catch (e: IllegalArgumentException) {
+      return defaultValue // This charset is invalid or unsupported. Give up.
     }
   }
 
@@ -113,49 +46,87 @@ public final class MediaType {
    * Returns the high-level media type, such as "text", "image", "audio", "video", or
    * "application".
    */
-  public String type() {
-    return type;
-  }
+  fun type() = type
 
   /**
    * Returns a specific media subtype, such as "plain" or "png", "mpeg", "mp4" or "xml".
    */
-  public String subtype() {
-    return subtype;
-  }
-
-  /**
-   * Returns the charset of this media type, or null if this media type doesn't specify a charset.
-   */
-  public @Nullable Charset charset() {
-    return charset(null);
-  }
-
-  /**
-   * Returns the charset of this media type, or {@code defaultValue} if either this media type
-   * doesn't specify a charset, of it its charset is unsupported by the current runtime.
-   */
-  public @Nullable Charset charset(@Nullable Charset defaultValue) {
-    try {
-      return charset != null ? Charset.forName(charset) : defaultValue;
-    } catch (IllegalArgumentException e) {
-      return defaultValue; // This charset is invalid or unsupported. Give up.
-    }
-  }
+  fun subtype() = subtype
 
   /**
    * Returns the encoded media type, like "text/plain; charset=utf-8", appropriate for use in a
    * Content-Type header.
    */
-  @Override public String toString() {
-    return mediaType;
-  }
+  override fun toString(): String = mediaType
 
-  @Override public boolean equals(@Nullable Object other) {
-    return other instanceof MediaType && ((MediaType) other).mediaType.equals(mediaType);
-  }
+  override fun equals(other: Any?): Boolean = other is MediaType && other.mediaType == mediaType
 
-  @Override public int hashCode() {
-    return mediaType.hashCode();
+  override fun hashCode(): Int = mediaType.hashCode()
+
+  companion object {
+    private const val TOKEN = "([a-zA-Z0-9-!#$%&'*+.^_`{|}~]+)"
+    private const val QUOTED = "\"([^\"]*)\""
+    private val TYPE_SUBTYPE = Pattern.compile("$TOKEN/$TOKEN")
+    private val PARAMETER = Pattern.compile(";\\s*(?:$TOKEN=(?:$TOKEN|$QUOTED))?")
+
+    /**
+     * Returns a media type for `string`.
+     *
+     * @throws IllegalArgumentException if `string` is not a well-formed media type.
+     */
+    @JvmStatic
+    fun get(string: String): MediaType {
+      val typeSubtype = TYPE_SUBTYPE.matcher(string)
+      require(typeSubtype.lookingAt()) { "No subtype found for: \"$string\"" }
+      val type = typeSubtype.group(1).toLowerCase(Locale.US)
+      val subtype = typeSubtype.group(2).toLowerCase(Locale.US)
+
+      var charset: String? = null
+      val parameter = PARAMETER.matcher(string)
+      var s = typeSubtype.end()
+      while (s < string.length) {
+        parameter.region(s, string.length)
+        require(parameter.lookingAt()) {
+          "Parameter is not formatted correctly: \"${string.substring(s)}\" for: \"$string\""
+        }
+
+        val name = parameter.group(1)
+        if (name == null || !name.equals("charset", ignoreCase = true)) {
+          s = parameter.end()
+          continue
+        }
+        val charsetParameter: String
+        val token = parameter.group(2)
+        charsetParameter = when {
+          token == null -> {
+            // Value is "double-quoted". That's valid and our regex group already strips the quotes.
+            parameter.group(3)
+          }
+          token.startsWith("'") && token.endsWith("'") && token.length > 2 -> {
+            // If the token is 'single-quoted' it's invalid! But we're lenient and strip the quotes.
+            token.substring(1, token.length - 1)
+          }
+          else -> token
+        }
+        require(charset == null || charsetParameter.equals(charset, ignoreCase = true)) {
+          "Multiple charsets defined: \"$charset\" and: \"$charsetParameter\" for: \"$string\""
+        }
+        charset = charsetParameter
+        s = parameter.end()
+        continue
+      }
+
+      return MediaType(string, type, subtype, charset)
+    }
+
+    /** Returns a media type for `string`, or null if `string` is not a well-formed media type. */
+    @JvmStatic
+    fun parse(string: String): MediaType? {
+      try {
+        return get(string)
+      } catch (ignored: IllegalArgumentException) {
+        return null
+      }
+    }
   }
 }
