9a926f4af6b5f6443f0bec0e6d4cb9f799024365
Egor Andreevici <egor@squareup.com>
2019-03-25 21:15:28 -0400

    Convert Route.java to Kotlin

--- okhttp/src/main/java/okhttp3/Route.java
+++ okhttp/src/main/java/okhttp3/Route.java
@@ -13,89 +13,64 @@
  * See the License for the specific language governing permissions and
  * limitations under the License.
  */
-package okhttp3;
+package okhttp3
 
-import java.net.InetSocketAddress;
-import java.net.Proxy;
-import javax.annotation.Nullable;
+import java.net.InetSocketAddress
+import java.net.Proxy
 
 /**
  * The concrete route used by a connection to reach an abstract origin server. When creating a
  * connection the client has many options:
  *
- * <ul>
- *     <li><strong>HTTP proxy:</strong> a proxy server may be explicitly configured for the client.
- *         Otherwise the {@linkplain java.net.ProxySelector proxy selector} is used. It may return
- *         multiple proxies to attempt.
- *     <li><strong>IP address:</strong> whether connecting directly to an origin server or a proxy,
- *         opening a socket requires an IP address. The DNS server may return multiple IP addresses
- *         to attempt.
- * </ul>
+ *  * **HTTP proxy:** a proxy server may be explicitly configured for the client.
+ * Otherwise the [proxy selector][java.net.ProxySelector] is used. It may return
+ * multiple proxies to attempt.
+ *  * **IP address:** whether connecting directly to an origin server or a proxy,
+ * opening a socket requires an IP address. The DNS server may return multiple IP addresses
+ * to attempt.
  *
- * <p>Each route is a specific selection of these options.
+ * Each route is a specific selection of these options.
  */
-public final class Route {
-  final Address address;
-  final Proxy proxy;
-  final InetSocketAddress inetSocketAddress;
+class Route(
+  internal val address: Address,
+  internal val proxy: Proxy,
+  internal val inetSocketAddress: InetSocketAddress
+) {
 
-  public Route(Address address, Proxy proxy, InetSocketAddress inetSocketAddress) {
-    if (address == null) {
-      throw new NullPointerException("address == null");
-    }
-    if (proxy == null) {
-      throw new NullPointerException("proxy == null");
-    }
-    if (inetSocketAddress == null) {
-      throw new NullPointerException("inetSocketAddress == null");
-    }
-    this.address = address;
-    this.proxy = proxy;
-    this.inetSocketAddress = inetSocketAddress;
-  }
-
-  public Address address() {
-    return address;
-  }
+  fun address(): Address = address
 
   /**
-   * Returns the {@link Proxy} of this route.
+   * Returns the [Proxy] of this route.
    *
-   * <strong>Warning:</strong> This may disagree with {@link Address#proxy} when it is null. When
+   * **Warning:** This may disagree with [Address.proxy] when it is null. When
    * the address's proxy is null, the proxy selector is used.
    */
-  public Proxy proxy() {
-    return proxy;
-  }
+  fun proxy(): Proxy = proxy
 
-  public InetSocketAddress socketAddress() {
-    return inetSocketAddress;
-  }
+  fun socketAddress(): InetSocketAddress = inetSocketAddress
 
   /**
-   * Returns true if this route tunnels HTTPS through an HTTP proxy. See <a
-   * href="http://www.ietf.org/rfc/rfc2817.txt">RFC 2817, Section 5.2</a>.
+   * Returns true if this route tunnels HTTPS through an HTTP proxy.
+   * See [RFC 2817, Section 5.2](http://www.ietf.org/rfc/rfc2817.txt).
    */
-  public boolean requiresTunnel() {
-    return address.sslSocketFactory != null && proxy.type() == Proxy.Type.HTTP;
+  fun requiresTunnel(): Boolean {
+    return address.sslSocketFactory != null && proxy.type() == Proxy.Type.HTTP
   }
 
-  @Override public boolean equals(@Nullable Object other) {
-    return other instanceof Route
-        && ((Route) other).address.equals(address)
-        && ((Route) other).proxy.equals(proxy)
-        && ((Route) other).inetSocketAddress.equals(inetSocketAddress);
+  override fun equals(other: Any?): Boolean {
+    return (other is Route
+        && other.address == address
+        && other.proxy == proxy
+        && other.inetSocketAddress == inetSocketAddress)
   }
 
-  @Override public int hashCode() {
-    int result = 17;
-    result = 31 * result + address.hashCode();
-    result = 31 * result + proxy.hashCode();
-    result = 31 * result + inetSocketAddress.hashCode();
-    return result;
+  override fun hashCode(): Int {
+    var result = 17
+    result = 31 * result + address.hashCode()
+    result = 31 * result + proxy.hashCode()
+    result = 31 * result + inetSocketAddress.hashCode()
+    return result
   }
 
-  @Override public String toString() {
-    return "Route{" + inetSocketAddress + "}";
-  }
+  override fun toString(): String = "Route{$inetSocketAddress}"
 }
