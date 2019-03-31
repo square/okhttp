02f4d4c22d82413c5c2d78288a37806bb498d247
Zac Sweers <pandanomic@gmail.com>
2019-03-30 17:11:54 -0700

    Convert Request to Kotlin

--- okhttp/src/main/java/okhttp3/Request.kt
+++ okhttp/src/main/java/okhttp3/Request.kt
@@ -13,284 +13,222 @@
  * See the License for the specific language governing permissions and
  * limitations under the License.
  */
-package okhttp3;
+package okhttp3
 
-import java.net.URL;
-import java.util.Collections;
-import java.util.LinkedHashMap;
-import java.util.List;
-import java.util.Map;
-import javax.annotation.Nullable;
-import okhttp3.internal.Util;
-import okhttp3.internal.http.HttpMethod;
+import okhttp3.internal.Util
+import okhttp3.internal.http.HttpMethod
+import java.net.URL
 
 /**
- * An HTTP request. Instances of this class are immutable if their {@link #body} is null or itself
+ * An HTTP request. Instances of this class are immutable if their [body] is `null` or itself
  * immutable.
  */
-public final class Request {
-  final HttpUrl url;
-  final String method;
-  final Headers headers;
-  final @Nullable RequestBody body;
-  final Map<Class<?>, Object> tags;
+class Request internal constructor(
+  internal val url: HttpUrl,
+  builder: Builder
+) {
+  internal val method: String = builder.method
+  internal val headers: Headers = builder.headers.build()
+  internal val body: RequestBody? = builder.body
+  internal val tags: Map<Class<*>, Any> = Util.immutableMap(builder.tags)
 
-  private volatile @Nullable CacheControl cacheControl; // Lazily initialized.
+  @Volatile
+  private var cacheControl: CacheControl? = null // Lazily initialized
 
-  Request(Builder builder) {
-    this.url = builder.url;
-    this.method = builder.method;
-    this.headers = builder.headers.build();
-    this.body = builder.body;
-    this.tags = Util.immutableMap(builder.tags);
-  }
+  val isHttps: Boolean
+    get() = url.isHttps
 
-  public HttpUrl url() {
-    return url;
-  }
+  fun url(): HttpUrl = url
 
-  public String method() {
-    return method;
-  }
+  fun method(): String = method
 
-  public Headers headers() {
-    return headers;
-  }
+  fun headers(): Headers = headers
 
-  public @Nullable String header(String name) {
-    return headers.get(name);
-  }
+  fun header(name: String): String? = headers[name]
 
-  public List<String> headers(String name) {
-    return headers.values(name);
-  }
+  fun headers(name: String): List<String> = headers.values(name)
 
-  public @Nullable RequestBody body() {
-    return body;
-  }
+  fun body(): RequestBody? = body
 
   /**
-   * Returns the tag attached with {@code Object.class} as a key, or null if no tag is attached with
+   * Returns the tag attached with `Object.class` as a key, or `null` if no tag is attached with
    * that key.
    *
-   * <p>Prior to OkHttp 3.11, this method never returned null if no tag was attached. Instead it
-   * returned either this request, or the request upon which this request was derived with {@link
-   * #newBuilder()}.
+   * Prior to OkHttp 3.11, this method never returned `null` if no tag was attached. Instead it
+   * returned either this request, or the request upon which this request was derived with
+   * [newBuilder].
    */
-  public @Nullable Object tag() {
-    return tag(Object.class);
-  }
+  fun tag(): Any? = tag(Any::class.java)
 
   /**
-   * Returns the tag attached with {@code type} as a key, or null if no tag is attached with that
+   * Returns the tag attached with [type] as a key, or `null` if no tag is attached with that
    * key.
    */
-  public @Nullable <T> T tag(Class<? extends T> type) {
-    return type.cast(tags.get(type));
-  }
+  fun <T> tag(type: Class<out T>): T? = type.cast(tags[type])
 
-  public Builder newBuilder() {
-    return new Builder(this);
-  }
+  fun newBuilder(): Builder = Builder(this)
 
   /**
-   * Returns the cache control directives for this response. This is never null, even if this
-   * response contains no {@code Cache-Control} header.
+   * Returns the cache control directives for this response. This is never `null`, even if this
+   * response contains no `Cache-Control` header.
    */
-  public CacheControl cacheControl() {
-    CacheControl result = cacheControl;
-    return result != null ? result : (cacheControl = CacheControl.parse(headers));
+  fun cacheControl(): CacheControl {
+    return cacheControl ?: CacheControl.parse(headers).also {
+      this.cacheControl = it
+    }
   }
 
-  public boolean isHttps() {
-    return url.isHttps();
-  }
+  override fun toString(): String = "Request{method=$method, url=$url, tags=$tags}"
 
-  @Override public String toString() {
-    return "Request{method="
-        + method
-        + ", url="
-        + url
-        + ", tags="
-        + tags
-        + '}';
-  }
-
-  public static class Builder {
-    @Nullable HttpUrl url;
-    String method;
-    Headers.Builder headers;
-    @Nullable RequestBody body;
+  class Builder {
+    internal var url: HttpUrl? = null
+    internal var method: String
+    internal var headers: Headers.Builder
+    internal var body: RequestBody? = null
 
     /** A mutable map of tags, or an immutable empty map if we don't have any. */
-    Map<Class<?>, Object> tags = Collections.emptyMap();
+    internal var tags: MutableMap<Class<*>, Any> = mutableMapOf()
 
-    public Builder() {
-      this.method = "GET";
-      this.headers = new Headers.Builder();
+    constructor() {
+      this.method = "GET"
+      this.headers = Headers.Builder()
     }
 
-    Builder(Request request) {
-      this.url = request.url;
-      this.method = request.method;
-      this.body = request.body;
-      this.tags = request.tags.isEmpty()
-          ? Collections.emptyMap()
-          : new LinkedHashMap<>(request.tags);
-      this.headers = request.headers.newBuilder();
+    internal constructor(request: Request) {
+      this.url = request.url
+      this.method = request.method
+      this.body = request.body
+      this.tags = if (request.tags.isEmpty())
+        mutableMapOf()
+      else
+        request.tags.toMutableMap()
+      this.headers = request.headers.newBuilder()
     }
 
-    public Builder url(HttpUrl url) {
-      if (url == null) throw new NullPointerException("url == null");
-      this.url = url;
-      return this;
+    fun url(url: HttpUrl): Builder = apply {
+      this.url = url
     }
 
     /**
      * Sets the URL target of this request.
      *
-     * @throws IllegalArgumentException if {@code url} is not a valid HTTP or HTTPS URL. Avoid this
-     * exception by calling {@link HttpUrl#parse}; it returns null for invalid URLs.
+     * @throws IllegalArgumentException if [url] is not a valid HTTP or HTTPS URL. Avoid this
+     * exception by calling [HttpUrl.parse]; it returns `null` for invalid URLs.
      */
-    public Builder url(String url) {
-      if (url == null) throw new NullPointerException("url == null");
-
+    fun url(url: String): Builder {
       // Silently replace web socket URLs with HTTP URLs.
-      if (url.regionMatches(true, 0, "ws:", 0, 3)) {
-        url = "http:" + url.substring(3);
-      } else if (url.regionMatches(true, 0, "wss:", 0, 4)) {
-        url = "https:" + url.substring(4);
+      val finalUrl: String = when {
+        url.regionMatches(0, "ws:", 0, 3, ignoreCase = true) -> {
+          "http:${url.substring(3)}"
+        }
+        url.regionMatches(0, "wss:", 0, 4, ignoreCase = true) -> {
+          "https:${url.substring(4)}"
+        }
+        else -> url
       }
 
-      return url(HttpUrl.get(url));
+      return url(HttpUrl[finalUrl])
     }
 
     /**
      * Sets the URL target of this request.
      *
-     * @throws IllegalArgumentException if the scheme of {@code url} is not {@code http} or {@code
-     * https}.
+     * @throws IllegalArgumentException if the scheme of [url] is not `http` or `https`.
      */
-    public Builder url(URL url) {
-      if (url == null) throw new NullPointerException("url == null");
-      return url(HttpUrl.get(url.toString()));
-    }
+    // Code review TODO - this never checked the scheme before like the doc says. Should it?
+    fun url(url: URL) = url(HttpUrl[url.toString()])
 
     /**
-     * Sets the header named {@code name} to {@code value}. If this request already has any headers
+     * Sets the header named [name] to [value]. If this request already has any headers
      * with that name, they are all replaced.
      */
-    public Builder header(String name, String value) {
-      headers.set(name, value);
-      return this;
+    fun header(name: String, value: String) = apply {
+      headers[name] = value
     }
 
     /**
-     * Adds a header with {@code name} and {@code value}. Prefer this method for multiply-valued
+     * Adds a header with [name] and [value]. Prefer this method for multiply-valued
      * headers like "Cookie".
      *
-     * <p>Note that for some headers including {@code Content-Length} and {@code Content-Encoding},
-     * OkHttp may replace {@code value} with a header derived from the request body.
+     * Note that for some headers including `Content-Length` and `Content-Encoding`,
+     * OkHttp may replace [value] with a header derived from the request body.
      */
-    public Builder addHeader(String name, String value) {
-      headers.add(name, value);
-      return this;
+    fun addHeader(name: String, value: String) = apply {
+      headers.add(name, value)
     }
 
-    /** Removes all headers named {@code name} on this builder. */
-    public Builder removeHeader(String name) {
-      headers.removeAll(name);
-      return this;
+    /** Removes all headers named [name] on this builder. */
+    fun removeHeader(name: String) = apply {
+      headers.removeAll(name)
     }
 
-    /** Removes all headers on this builder and adds {@code headers}. */
-    public Builder headers(Headers headers) {
-      this.headers = headers.newBuilder();
-      return this;
+    /** Removes all headers on this builder and adds [headers]. */
+    fun headers(headers: Headers) = apply {
+      this.headers = headers.newBuilder()
     }
 
     /**
-     * Sets this request's {@code Cache-Control} header, replacing any cache control headers already
-     * present. If {@code cacheControl} doesn't define any directives, this clears this request's
+     * Sets this request's `Cache-Control` header, replacing any cache control headers already
+     * present. If [cacheControl] doesn't define any directives, this clears this request's
      * cache-control headers.
      */
-    public Builder cacheControl(CacheControl cacheControl) {
-      String value = cacheControl.toString();
-      if (value.isEmpty()) return removeHeader("Cache-Control");
-      return header("Cache-Control", value);
+    fun cacheControl(cacheControl: CacheControl): Builder {
+      val value = cacheControl.toString()
+      return when {
+        value.isEmpty() -> removeHeader("Cache-Control")
+        else -> header("Cache-Control", value)
+      }
     }
 
-    public Builder get() {
-      return method("GET", null);
-    }
+    fun get() = method("GET", null)
 
-    public Builder head() {
-      return method("HEAD", null);
-    }
+    fun head() = method("HEAD", null)
 
-    public Builder post(RequestBody body) {
-      return method("POST", body);
-    }
+    fun post(body: RequestBody) = method("POST", body)
 
-    public Builder delete(@Nullable RequestBody body) {
-      return method("DELETE", body);
-    }
+    @JvmOverloads
+    fun delete(body: RequestBody? = Util.EMPTY_REQUEST) = method("DELETE", body)
 
-    public Builder delete() {
-      return delete(Util.EMPTY_REQUEST);
-    }
+    fun put(body: RequestBody) = method("PUT", body)
 
-    public Builder put(RequestBody body) {
-      return method("PUT", body);
-    }
+    fun patch(body: RequestBody) = method("PATCH", body)
 
-    public Builder patch(RequestBody body) {
-      return method("PATCH", body);
-    }
-
-    public Builder method(String method, @Nullable RequestBody body) {
-      if (method == null) throw new NullPointerException("method == null");
-      if (method.length() == 0) throw new IllegalArgumentException("method.length() == 0");
+    fun method(method: String, body: RequestBody?): Builder = apply {
+      if (method.isEmpty()) throw IllegalArgumentException("method.isEmpty() == true")
       if (body != null && !HttpMethod.permitsRequestBody(method)) {
-        throw new IllegalArgumentException("method " + method + " must not have a request body.");
+        throw IllegalArgumentException("method $method must not have a request body.")
       }
       if (body == null && HttpMethod.requiresRequestBody(method)) {
-        throw new IllegalArgumentException("method " + method + " must have a request body.");
+        throw IllegalArgumentException("method $method must have a request body.")
       }
-      this.method = method;
-      this.body = body;
-      return this;
+      this.method = method
+      this.body = body
     }
 
-    /** Attaches {@code tag} to the request using {@code Object.class} as a key. */
-    public Builder tag(@Nullable Object tag) {
-      return tag(Object.class, tag);
-    }
+    /** Attaches [tag] to the request using `Object.class` as a key. */
+    fun tag(tag: Any?): Builder = tag(Any::class.java, tag)
 
     /**
-     * Attaches {@code tag} to the request using {@code type} as a key. Tags can be read from a
-     * request using {@link Request#tag}. Use null to remove any existing tag assigned for {@code
-     * type}.
+     * Attaches [tag] to the request using [type] as a key. Tags can be read from a
+     * request using [Request.tag]. Use `null` to remove any existing tag assigned for [type].
      *
-     * <p>Use this API to attach timing, debugging, or other application data to a request so that
+     * Use this API to attach timing, debugging, or other application data to a request so that
      * you may read it in interceptors, event listeners, or callbacks.
      */
-    public <T> Builder tag(Class<? super T> type, @Nullable T tag) {
-      if (type == null) throw new NullPointerException("type == null");
-
+    fun <T> tag(type: Class<in T>, tag: T?) = apply {
       if (tag == null) {
-        tags.remove(type);
+        tags.remove(type)
       } else {
-        if (tags.isEmpty()) tags = new LinkedHashMap<>();
-        tags.put(type, type.cast(tag));
+        if (tags.isEmpty()) {
+          tags = mutableMapOf()
+        }
+        tags[type] = type.cast(tag)!! // Force-unwrap due to lack of contracts on Class#cast()
       }
-
-      return this;
     }
 
-    public Request build() {
-      if (url == null) throw new IllegalStateException("url == null");
-      return new Request(this);
-    }
+    fun build(): Request =  Request(
+        checkNotNull(url) { "url == null" },
+        this
+    )
   }
 }
