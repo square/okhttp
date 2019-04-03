5af46e6496aa1cf69b7938fa652d5e0a0f5bd243
Shaishav Gandhi <shaishav@uber.com>
2019-04-02 18:27:33 -0700

    Move CertificateChainCleaner to Kotlin

--- okhttp/src/main/java/okhttp3/internal/tls/CertificateChainCleaner.kt
+++ okhttp/src/main/java/okhttp3/internal/tls/CertificateChainCleaner.kt
@@ -14,34 +14,38 @@
  *  See the License for the specific language governing permissions and
  *  limitations under the License.
  */
-package okhttp3.internal.tls;
+package okhttp3.internal.tls
 
-import java.security.cert.Certificate;
-import java.security.cert.X509Certificate;
-import java.util.List;
-import javax.net.ssl.SSLPeerUnverifiedException;
-import javax.net.ssl.X509TrustManager;
-import okhttp3.internal.platform.Platform;
+import okhttp3.internal.platform.Platform
+import java.security.cert.Certificate
+import java.security.cert.X509Certificate
+import javax.net.ssl.SSLPeerUnverifiedException
+import javax.net.ssl.X509TrustManager
 
 /**
  * Computes the effective certificate chain from the raw array returned by Java's built in TLS APIs.
- * Cleaning a chain returns a list of certificates where the first element is {@code chain[0]}, each
+ * Cleaning a chain returns a list of certificates where the first element is `chain[0]`, each
  * certificate is signed by the certificate that follows, and the last certificate is a trusted CA
  * certificate.
  *
- * <p>Use of the chain cleaner is necessary to omit unexpected certificates that aren't relevant to
+ * Use of the chain cleaner is necessary to omit unexpected certificates that aren't relevant to
  * the TLS handshake and to extract the trusted CA certificate for the benefit of certificate
  * pinning.
  */
-public abstract class CertificateChainCleaner {
-  public abstract List<Certificate> clean(List<Certificate> chain, String hostname)
-      throws SSLPeerUnverifiedException;
+abstract class CertificateChainCleaner {
 
-  public static CertificateChainCleaner get(X509TrustManager trustManager) {
-    return Platform.get().buildCertificateChainCleaner(trustManager);
-  }
+  @Throws(SSLPeerUnverifiedException::class)
+  abstract fun clean(chain: List<Certificate>, hostname: String): List<Certificate>
 
-  public static CertificateChainCleaner get(X509Certificate... caCerts) {
-    return new BasicCertificateChainCleaner(new BasicTrustRootIndex(caCerts));
+  companion object {
+    @JvmStatic
+    fun get(trustManager: X509TrustManager): CertificateChainCleaner {
+      return Platform.get().buildCertificateChainCleaner(trustManager)
+    }
+
+    @JvmStatic
+    fun get(vararg caCerts: X509Certificate): CertificateChainCleaner {
+      return BasicCertificateChainCleaner(BasicTrustRootIndex(*caCerts))
+    }
   }
 }
