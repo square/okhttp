49b11a81e77d08ee5399ca735818084d3883f452
Shaishav Gandhi <shaishav@uber.com>
2019-04-07 12:10:10 -0700

    Convert internal.io & internal.proxy to Kotlin

--- okhttp/src/main/java/okhttp3/internal/proxy/NullProxySelector.kt
+++ okhttp/src/main/java/okhttp3/internal/proxy/NullProxySelector.kt
@@ -13,27 +13,25 @@
  * See the License for the specific language governing permissions and
  * limitations under the License.
  */
-package okhttp3.internal.proxy;
+package okhttp3.internal.proxy
 
-import java.io.IOException;
-import java.net.Proxy;
-import java.net.ProxySelector;
-import java.net.SocketAddress;
-import java.net.URI;
-import java.util.Collections;
-import java.util.List;
+import java.io.IOException
+import java.net.Proxy
+import java.net.ProxySelector
+import java.net.SocketAddress
+import java.net.URI
 
 /**
- * A proxy selector that always returns the {@link Proxy#NO_PROXY}.
+ * A proxy selector that always returns the [Proxy.NO_PROXY].
  */
-public class NullProxySelector extends ProxySelector {
-  @Override public List<Proxy> select(URI uri) {
+open class NullProxySelector: ProxySelector() {
+  override fun select(uri: URI?): List<Proxy> {
     if (uri == null) {
-      throw new IllegalArgumentException("uri must not be null");
+      throw IllegalArgumentException("uri must not be null")
     }
-    return Collections.singletonList(Proxy.NO_PROXY);
+    return listOf(Proxy.NO_PROXY)
   }
 
-  @Override public void connectFailed(URI uri, SocketAddress sa, IOException ioe) {
+  override fun connectFailed(uri: URI?, sa: SocketAddress?, ioe: IOException?) {
   }
 }
