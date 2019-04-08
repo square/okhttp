1a9c68512634d826b9457631d8ce6e2f0e64be6d
Jesse Wilson <jwilson@squareup.com>
2019-04-08 12:18:36 -0700

    Convert RealConnectionPool to Kotlin

--- okhttp/src/main/java/okhttp3/internal/connection/RealConnectionPool.kt
+++ okhttp/src/main/java/okhttp3/internal/connection/RealConnectionPool.kt
@@ -14,144 +14,124 @@
  *  See the License for the specific language governing permissions and
  *  limitations under the License.
  */
-package okhttp3.internal.connection;
+package okhttp3.internal.connection
 
-import java.io.IOException;
-import java.lang.ref.Reference;
-import java.net.Proxy;
-import java.util.ArrayDeque;
-import java.util.ArrayList;
-import java.util.Deque;
-import java.util.Iterator;
-import java.util.List;
-import java.util.concurrent.Executor;
-import java.util.concurrent.SynchronousQueue;
-import java.util.concurrent.ThreadPoolExecutor;
-import java.util.concurrent.TimeUnit;
-import javax.annotation.Nullable;
-import okhttp3.Address;
-import okhttp3.Route;
-import okhttp3.internal.Util;
-import okhttp3.internal.connection.Transmitter.TransmitterReference;
-import okhttp3.internal.platform.Platform;
+import okhttp3.Address
+import okhttp3.Route
+import okhttp3.internal.Util
+import okhttp3.internal.Util.closeQuietly
+import okhttp3.internal.connection.Transmitter.TransmitterReference
+import okhttp3.internal.platform.Platform
+import java.io.IOException
+import java.net.Proxy
+import java.util.ArrayDeque
+import java.util.concurrent.SynchronousQueue
+import java.util.concurrent.ThreadPoolExecutor
+import java.util.concurrent.TimeUnit
 
-import static okhttp3.internal.Util.closeQuietly;
+class RealConnectionPool(
+  /** The maximum number of idle connections for each address.  */
+  private val maxIdleConnections: Int,
+  keepAliveDuration: Long,
+  timeUnit: TimeUnit
+) {
+  private val keepAliveDurationNs: Long = timeUnit.toNanos(keepAliveDuration)
 
-public final class RealConnectionPool {
-  /**
-   * Background threads are used to cleanup expired connections. There will be at most a single
-   * thread running per connection pool. The thread pool executor permits the pool itself to be
-   * garbage collected.
-   */
-  private static final Executor executor = new ThreadPoolExecutor(0 /* corePoolSize */,
-      Integer.MAX_VALUE /* maximumPoolSize */, 60L /* keepAliveTime */, TimeUnit.SECONDS,
-      new SynchronousQueue<>(), Util.threadFactory("OkHttp ConnectionPool", true));
-
-  /** The maximum number of idle connections for each address. */
-  private final int maxIdleConnections;
-  private final long keepAliveDurationNs;
-  private final Runnable cleanupRunnable = () -> {
-    while (true) {
-      long waitNanos = cleanup(System.nanoTime());
-      if (waitNanos == -1) return;
-      if (waitNanos > 0) {
-        long waitMillis = waitNanos / 1000000L;
-        waitNanos -= (waitMillis * 1000000L);
-        synchronized (RealConnectionPool.this) {
-          try {
-            RealConnectionPool.this.wait(waitMillis, (int) waitNanos);
-          } catch (InterruptedException ignored) {
-          }
+  private val cleanupRunnable = object : Runnable {
+    override fun run() {
+      while (true) {
+        val waitNanos = cleanup(System.nanoTime())
+        if (waitNanos == -1L) return
+        try {
+          this@RealConnectionPool.waitNanos(waitNanos)
+        } catch (_: InterruptedException) {
         }
       }
     }
-  };
+  }
 
-  private final Deque<RealConnection> connections = new ArrayDeque<>();
-  final RouteDatabase routeDatabase = new RouteDatabase();
-  boolean cleanupRunning;
+  private val connections = ArrayDeque<RealConnection>()
+  val routeDatabase = RouteDatabase()
+  var cleanupRunning: Boolean = false
 
-  public RealConnectionPool(int maxIdleConnections, long keepAliveDuration, TimeUnit timeUnit) {
-    this.maxIdleConnections = maxIdleConnections;
-    this.keepAliveDurationNs = timeUnit.toNanos(keepAliveDuration);
-
+  init {
     // Put a floor on the keep alive duration, otherwise cleanup will spin loop.
-    if (keepAliveDuration <= 0) {
-      throw new IllegalArgumentException("keepAliveDuration <= 0: " + keepAliveDuration);
-    }
+    require(keepAliveDuration > 0) { "keepAliveDuration <= 0: $keepAliveDuration" }
   }
 
-  public synchronized int idleConnectionCount() {
-    int total = 0;
-    for (RealConnection connection : connections) {
-      if (connection.transmitters.isEmpty()) total++;
-    }
-    return total;
+  @Synchronized fun idleConnectionCount(): Int {
+    return connections.count { it.transmitters.isEmpty() }
   }
 
-  public synchronized int connectionCount() {
-    return connections.size();
+  @Synchronized fun connectionCount(): Int {
+    return connections.size
   }
 
   /**
-   * Attempts to acquire a recycled connection to {@code address} for {@code transmitter}. Returns
-   * true if a connection was acquired.
+   * Attempts to acquire a recycled connection to `address` for `transmitter`. Returns true if a
+   * connection was acquired.
    *
-   * <p>If {@code routes} is non-null these are the resolved routes (ie. IP addresses) for the
-   * connection. This is used to coalesce related domains to the same HTTP/2 connection, such as
-   * {@code square.com} and {@code square.ca}.
+   * If `routes` is non-null these are the resolved routes (ie. IP addresses) for the connection.
+   * This is used to coalesce related domains to the same HTTP/2 connection, such as `square.com`
+   * and `square.ca`.
    */
-  boolean transmitterAcquirePooledConnection(Address address, Transmitter transmitter,
-      @Nullable List<Route> routes, boolean requireMultiplexed) {
-    assert (Thread.holdsLock(this));
-    for (RealConnection connection : connections) {
-      if (requireMultiplexed && !connection.isMultiplexed()) continue;
-      if (!connection.isEligible(address, routes)) continue;
-      transmitter.acquireConnectionNoEvents(connection);
-      return true;
+  fun transmitterAcquirePooledConnection(
+    address: Address,
+    transmitter: Transmitter,
+    routes: List<Route>?,
+    requireMultiplexed: Boolean
+  ): Boolean {
+    assert(Thread.holdsLock(this))
+    for (connection in connections) {
+      if (requireMultiplexed && !connection.isMultiplexed) continue
+      if (!connection.isEligible(address, routes)) continue
+      transmitter.acquireConnectionNoEvents(connection)
+      return true
     }
-    return false;
+    return false
   }
 
-  void put(RealConnection connection) {
-    assert (Thread.holdsLock(this));
+  fun put(connection: RealConnection) {
+    assert(Thread.holdsLock(this))
     if (!cleanupRunning) {
-      cleanupRunning = true;
-      executor.execute(cleanupRunnable);
+      cleanupRunning = true
+      executor.execute(cleanupRunnable)
     }
-    connections.add(connection);
+    connections.add(connection)
   }
 
   /**
-   * Notify this pool that {@code connection} has become idle. Returns true if the connection has
+   * Notify this pool that `connection` has become idle. Returns true if the connection has
    * been removed from the pool and should be closed.
    */
-  boolean connectionBecameIdle(RealConnection connection) {
-    assert (Thread.holdsLock(this));
+  fun connectionBecameIdle(connection: RealConnection): Boolean {
+    assert(Thread.holdsLock(this))
     if (connection.noNewExchanges || maxIdleConnections == 0) {
-      connections.remove(connection);
-      return true;
+      connections.remove(connection)
+      return true
     } else {
-      notifyAll(); // Awake the cleanup thread: we may have exceeded the idle connection limit.
-      return false;
+      // Awake the cleanup thread: we may have exceeded the idle connection limit.
+      (this as java.lang.Object).notifyAll()
+      return false
     }
   }
 
-  public void evictAll() {
-    List<RealConnection> evictedConnections = new ArrayList<>();
-    synchronized (this) {
-      for (Iterator<RealConnection> i = connections.iterator(); i.hasNext(); ) {
-        RealConnection connection = i.next();
+  fun evictAll() {
+    val evictedConnections = mutableListOf<RealConnection>()
+    synchronized(this) {
+      val i = connections.iterator()
+      while (i.hasNext()) {
+        val connection = i.next()
         if (connection.transmitters.isEmpty()) {
-          connection.noNewExchanges = true;
-          evictedConnections.add(connection);
-          i.remove();
+          connection.noNewExchanges = true
+          evictedConnections.add(connection)
+          i.remove()
         }
       }
     }
 
-    for (RealConnection connection : evictedConnections) {
-      closeQuietly(connection.socket());
+    for (connection in evictedConnections) {
+      closeQuietly(connection.socket())
     }
   }
 
@@ -159,104 +139,137 @@ public final class RealConnectionPool {
    * Performs maintenance on this pool, evicting the connection that has been idle the longest if
    * either it has exceeded the keep alive limit or the idle connections limit.
    *
-   * <p>Returns the duration in nanos to sleep until the next scheduled call to this method. Returns
+   * Returns the duration in nanos to sleep until the next scheduled call to this method. Returns
    * -1 if no further cleanups are required.
    */
-  long cleanup(long now) {
-    int inUseConnectionCount = 0;
-    int idleConnectionCount = 0;
-    RealConnection longestIdleConnection = null;
-    long longestIdleDurationNs = Long.MIN_VALUE;
+  fun cleanup(now: Long): Long {
+    var inUseConnectionCount = 0
+    var idleConnectionCount = 0
+    var longestIdleConnection: RealConnection? = null
+    var longestIdleDurationNs = Long.MIN_VALUE
 
     // Find either a connection to evict, or the time that the next eviction is due.
-    synchronized (this) {
-      for (Iterator<RealConnection> i = connections.iterator(); i.hasNext(); ) {
-        RealConnection connection = i.next();
-
+    synchronized(this) {
+      for (connection in connections) {
         // If the connection is in use, keep searching.
         if (pruneAndGetAllocationCount(connection, now) > 0) {
-          inUseConnectionCount++;
-          continue;
+          inUseConnectionCount++
+          continue
         }
 
-        idleConnectionCount++;
+        idleConnectionCount++
 
         // If the connection is ready to be evicted, we're done.
-        long idleDurationNs = now - connection.idleAtNanos;
+        val idleDurationNs = now - connection.idleAtNanos
         if (idleDurationNs > longestIdleDurationNs) {
-          longestIdleDurationNs = idleDurationNs;
-          longestIdleConnection = connection;
+          longestIdleDurationNs = idleDurationNs
+          longestIdleConnection = connection
         }
       }
 
-      if (longestIdleDurationNs >= this.keepAliveDurationNs
-          || idleConnectionCount > this.maxIdleConnections) {
-        // We've found a connection to evict. Remove it from the list, then close it below (outside
-        // of the synchronized block).
-        connections.remove(longestIdleConnection);
-      } else if (idleConnectionCount > 0) {
-        // A connection will be ready to evict soon.
-        return keepAliveDurationNs - longestIdleDurationNs;
-      } else if (inUseConnectionCount > 0) {
-        // All connections are in use. It'll be at least the keep alive duration 'til we run again.
-        return keepAliveDurationNs;
-      } else {
-        // No connections, idle or in use.
-        cleanupRunning = false;
-        return -1;
+      when {
+        longestIdleDurationNs >= this.keepAliveDurationNs
+            || idleConnectionCount > this.maxIdleConnections -> {
+          // We've found a connection to evict. Remove it from the list, then close it below
+          // (outside of the synchronized block).
+          connections.remove(longestIdleConnection)
+        }
+        idleConnectionCount > 0 -> {
+          // A connection will be ready to evict soon.
+          return keepAliveDurationNs - longestIdleDurationNs
+        }
+        inUseConnectionCount > 0 -> {
+          // All connections are in use. It'll be at least the keep alive duration 'til we run
+          // again.
+          return keepAliveDurationNs
+        }
+        else -> {
+          // No connections, idle or in use.
+          cleanupRunning = false
+          return -1
+        }
       }
     }
 
-    closeQuietly(longestIdleConnection.socket());
+    closeQuietly(longestIdleConnection!!.socket())
 
     // Cleanup again immediately.
-    return 0;
+    return 0
   }
 
   /**
    * Prunes any leaked transmitters and then returns the number of remaining live transmitters on
-   * {@code connection}. Transmitters are leaked if the connection is tracking them but the
-   * application code has abandoned them. Leak detection is imprecise and relies on garbage
-   * collection.
+   * `connection`. Transmitters are leaked if the connection is tracking them but the application
+   * code has abandoned them. Leak detection is imprecise and relies on garbage collection.
    */
-  private int pruneAndGetAllocationCount(RealConnection connection, long now) {
-    List<Reference<Transmitter>> references = connection.transmitters;
-    for (int i = 0; i < references.size(); ) {
-      Reference<Transmitter> reference = references.get(i);
+  private fun pruneAndGetAllocationCount(connection: RealConnection, now: Long): Int {
+    val references = connection.transmitters
+    var i = 0
+    while (i < references.size) {
+      val reference = references[i]
 
       if (reference.get() != null) {
-        i++;
-        continue;
+        i++
+        continue
       }
 
       // We've discovered a leaked transmitter. This is an application bug.
-      TransmitterReference transmitterRef = (TransmitterReference) reference;
-      String message = "A connection to " + connection.route().address().url()
-          + " was leaked. Did you forget to close a response body?";
-      Platform.get().logCloseableLeak(message, transmitterRef.callStackTrace);
+      val transmitterRef = reference as TransmitterReference
+      val message = "A connection to ${connection.route().address().url()} was leaked. " +
+          "Did you forget to close a response body?"
+      Platform.get().logCloseableLeak(message, transmitterRef.callStackTrace)
 
-      references.remove(i);
-      connection.noNewExchanges = true;
+      references.removeAt(i)
+      connection.noNewExchanges = true
 
       // If this was the last allocation, the connection is eligible for immediate eviction.
       if (references.isEmpty()) {
-        connection.idleAtNanos = now - keepAliveDurationNs;
-        return 0;
+        connection.idleAtNanos = now - keepAliveDurationNs
+        return 0
       }
     }
 
-    return references.size();
+    return references.size
   }
 
-  /** Track a bad route in the route database. Other routes will be attempted first. */
-  public void connectFailed(Route failedRoute, IOException failure) {
+  /** Track a bad route in the route database. Other routes will be attempted first.  */
+  fun connectFailed(failedRoute: Route, failure: IOException) {
     // Tell the proxy selector when we fail to connect on a fresh connection.
     if (failedRoute.proxy().type() != Proxy.Type.DIRECT) {
-      Address address = failedRoute.address();
+      val address = failedRoute.address()
       address.proxySelector().connectFailed(
-          address.url().uri(), failedRoute.proxy().address(), failure);
+          address.url().uri(), failedRoute.proxy().address(), failure)
     }
 
-    routeDatabase.failed(failedRoute);
+    routeDatabase.failed(failedRoute)
+  }
+
+  companion object {
+    /**
+     * Background threads are used to cleanup expired connections. There will be at most a single
+     * thread running per connection pool. The thread pool executor permits the pool itself to be
+     * garbage collected.
+     */
+    private val executor = ThreadPoolExecutor(
+        0, // corePoolSize.
+        Int.MAX_VALUE, // maximumPoolSize.
+        60L, TimeUnit.SECONDS,  // keepAliveTime.
+        SynchronousQueue(),
+        Util.threadFactory("OkHttp ConnectionPool", true)
+    )
+  }
+}
+
+/**
+ * Lock and wait a duration in nanoseconds. Unlike [java.lang.Object.wait] this interprets 0 as
+ * "don't wait" instead of "wait forever".
+ */
+@Throws(InterruptedException::class)
+private fun Any.waitNanos(nanos: Long) {
+  if (nanos == 0L) return
+  val ms = nanos / 1_000_000L
+  val ns = nanos - (ms * 1_000_000L)
+  synchronized(this) {
+    (this as Object).wait(ms, ns.toInt())
   }
 }
