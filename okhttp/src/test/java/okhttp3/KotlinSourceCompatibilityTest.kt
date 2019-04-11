742de8688976c2a56b991d7c4e29c504de161de8
Yuri Schimke <yuri@schimke.ee>
2019-04-11 07:36:40 +0100

    Fix existing code

--- okhttp/src/test/java/okhttp3/KotlinSourceCompatibilityTest.kt
+++ okhttp/src/test/java/okhttp3/KotlinSourceCompatibilityTest.kt
@@ -891,8 +891,7 @@ class KotlinSourceCompatibilityTest {
     builder = builder.eventListenerFactory(object : EventListener.Factory {
       override fun create(call: Call): EventListener = TODO()
     })
-    // Broken
-//    builder = builder.eventListenerFactory(LoggingEventListener.Factory { s -> TODO() })
+    builder = builder.eventListenerFactory(LoggingEventListener.Factory { s -> TODO() })
     builder = builder.eventListenerFactory { it: Call -> TODO() }
     val client: OkHttpClient = builder.build()
   }
