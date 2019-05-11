aa0c2a367462d61a27b133e6896b485df9ecc1a3
Yuri Schimke <yuri@schimke.ee>
2019-05-11 09:50:08 +0100

    Convert Main to Kotlin

--- okcurl/src/main/java/okhttp3/curl/Main.kt
+++ okcurl/src/main/java/okhttp3/curl/Main.kt
@@ -13,291 +13,275 @@
  * See the License for the specific language governing permissions and
  * limitations under the License.
  */
-package okhttp3.curl;
+package okhttp3.curl
 
-import com.google.common.base.Joiner;
-import io.airlift.airline.Arguments;
-import io.airlift.airline.Command;
-import io.airlift.airline.HelpOption;
-import io.airlift.airline.Option;
-import io.airlift.airline.SingleCommand;
-import java.io.IOException;
-import java.io.InputStream;
-import java.security.cert.X509Certificate;
-import java.util.List;
-import java.util.Properties;
-import java.util.logging.ConsoleHandler;
-import java.util.logging.Level;
-import java.util.logging.LogRecord;
-import java.util.logging.Logger;
-import java.util.logging.SimpleFormatter;
-import javax.net.ssl.HostnameVerifier;
-import javax.net.ssl.SSLContext;
-import javax.net.ssl.SSLSocketFactory;
-import javax.net.ssl.TrustManager;
-import javax.net.ssl.X509TrustManager;
-import okhttp3.Headers;
-import okhttp3.MediaType;
-import okhttp3.OkHttpClient;
-import okhttp3.Protocol;
-import okhttp3.Request;
-import okhttp3.RequestBody;
-import okhttp3.Response;
-import okhttp3.internal.Util;
-import okhttp3.internal.http.StatusLine;
-import okhttp3.internal.http2.Http2;
-import okhttp3.internal.platform.Platform;
-import okhttp3.logging.HttpLoggingInterceptor;
-import okhttp3.logging.LoggingEventListener;
-import okio.BufferedSource;
-import okio.Okio;
-import okio.Sink;
+import com.github.rvesse.airline.HelpOption
+import com.github.rvesse.airline.SingleCommand
+import com.github.rvesse.airline.annotations.Arguments
+import com.github.rvesse.airline.annotations.Command
+import com.github.rvesse.airline.annotations.Option
+import okhttp3.MediaType
+import okhttp3.OkHttpClient
+import okhttp3.Protocol
+import okhttp3.Request
+import okhttp3.RequestBody
+import okhttp3.curl.Main.Companion.NAME
+import okhttp3.internal.Util
+import okhttp3.internal.http.StatusLine
+import okhttp3.internal.http2.Http2
+import okhttp3.internal.platform.Platform
+import okhttp3.logging.HttpLoggingInterceptor
+import okhttp3.logging.LoggingEventListener
+import okio.sink
+import java.io.IOException
+import java.security.cert.X509Certificate
+import java.util.Properties
+import java.util.concurrent.TimeUnit.SECONDS
+import java.util.logging.ConsoleHandler
+import java.util.logging.Level
+import java.util.logging.LogRecord
+import java.util.logging.Logger
+import java.util.logging.SimpleFormatter
+import javax.inject.Inject
+import javax.net.ssl.HostnameVerifier
+import javax.net.ssl.SSLSocketFactory
+import javax.net.ssl.TrustManager
+import javax.net.ssl.X509TrustManager
 
-import static java.util.concurrent.TimeUnit.SECONDS;
+@Command(name = NAME, description = "A curl for the next-generation web.")
+class Main : Runnable {
+  @Inject
+  var help: HelpOption<Main>? = null
 
-@Command(name = Main.NAME, description = "A curl for the next-generation web.")
-public class Main extends HelpOption implements Runnable {
-  static final String NAME = "okcurl";
-  static final int DEFAULT_TIMEOUT = -1;
-  private static Logger frameLogger;
+  @Option(name = ["-X", "--request"], description = "Specify request command to use")
+  var method: String? = null
 
-  static Main fromArgs(String... args) {
-    return SingleCommand.singleCommand(Main.class).parse(args);
-  }
+  @Option(name = ["-d", "--data"], description = "HTTP POST data")
+  var data: String? = null
 
-  public static void main(String... args) {
-    fromArgs(args).run();
-  }
+  @Option(name = ["-H", "--header"], description = "Custom header to pass to server")
+  var headers: MutableList<String>? = null
 
-  private static String versionString() {
-    try {
-      Properties prop = new Properties();
-      InputStream in = Main.class.getResourceAsStream("/okcurl-version.properties");
-      prop.load(in);
-      in.close();
-      return prop.getProperty("version");
-    } catch (IOException e) {
-      throw new AssertionError("Could not load okcurl-version.properties.");
-    }
-  }
+  @Option(name = ["-A", "--user-agent"], description = "User-Agent to send to server")
+  var userAgent = NAME + "/" + versionString()
 
-  private static String protocols() {
-    return Joiner.on(", ").join(Protocol.values());
-  }
+  @Option(name = ["--connect-timeout"],
+      description = "Maximum time allowed for connection (seconds)")
+  var connectTimeout = DEFAULT_TIMEOUT
 
-  @Option(name = {"-X", "--request"}, description = "Specify request command to use")
-  public String method;
+  @Option(name = ["--read-timeout"],
+      description = "Maximum time allowed for reading data (seconds)")
+  var readTimeout = DEFAULT_TIMEOUT
 
-  @Option(name = {"-d", "--data"}, description = "HTTP POST data")
-  public String data;
-
-  @Option(name = {"-H", "--header"}, description = "Custom header to pass to server")
-  public List<String> headers;
-
-  @Option(name = {"-A", "--user-agent"}, description = "User-Agent to send to server")
-  public String userAgent = NAME + "/" + versionString();
-
-  @Option(name = "--connect-timeout", description = "Maximum time allowed for connection (seconds)")
-  public int connectTimeout = DEFAULT_TIMEOUT;
-
-  @Option(name = "--read-timeout", description = "Maximum time allowed for reading data (seconds)")
-  public int readTimeout = DEFAULT_TIMEOUT;
-
-  @Option(
-      name = "--call-timeout",
+  @Option(name = ["--call-timeout"],
       description = "Maximum time allowed for the entire call (seconds)")
-  public int callTimeout = DEFAULT_TIMEOUT;
+  var callTimeout = DEFAULT_TIMEOUT
 
-  @Option(name = {"-L", "--location"}, description = "Follow redirects")
-  public boolean followRedirects;
+  @Option(name = ["-L", "--location"], description = "Follow redirects")
+  var followRedirects: Boolean = false
 
-  @Option(name = {"-k", "--insecure"},
-      description = "Allow connections to SSL sites without certs")
-  public boolean allowInsecure;
+  @Option(name = ["-k", "--insecure"], description = "Allow connections to SSL sites without certs")
+  var allowInsecure: Boolean = false
 
-  @Option(name = {"-i", "--include"}, description = "Include protocol headers in the output")
-  public boolean showHeaders;
+  @Option(name = ["-i", "--include"], description = "Include protocol headers in the output")
+  var showHeaders: Boolean = false
 
-  @Option(name = "--frames", description = "Log HTTP/2 frames to STDERR")
-  public boolean showHttp2Frames;
+  @Option(name = ["--frames"], description = "Log HTTP/2 frames to STDERR")
+  var showHttp2Frames: Boolean = false
 
-  @Option(name = {"-e", "--referer"}, description = "Referer URL")
-  public String referer;
+  @Option(name = ["-e", "--referer"], description = "Referer URL")
+  var referer: String? = null
 
-  @Option(name = {"-V", "--version"}, description = "Show version number and quit")
-  public boolean version;
+  @Option(name = ["-V", "--version"], description = "Show version number and quit")
+  var version: Boolean = false
 
-  @Option(
-      name = {"-v", "--verbose"},
-      description = "Makes " + NAME + " verbose during the operation")
-  public boolean verbose;
+  @Option(name = ["-v", "--verbose"], description = "Makes $NAME verbose during the operation")
+  var verbose: Boolean = false
 
-  @Arguments(title = "url", description = "Remote resource URL")
-  public String url;
+  @Arguments(title = ["url"], description = "Remote resource URL")
+  var url: String? = null
 
-  private OkHttpClient client;
+  private lateinit var client: OkHttpClient
 
-  @Override public void run() {
-    if (showHelpIfRequested()) {
-      return;
+  override fun run() {
+    if (help?.showHelpIfRequested() == true) {
+      return
     }
+
     if (version) {
-      System.out.println(NAME + " " + versionString());
-      System.out.println("Protocols: " + protocols());
-      return;
+      println(NAME + " " + versionString())
+      println("Protocols: " + protocols())
+      return
     }
 
     if (showHttp2Frames) {
-      enableHttp2FrameLogging();
+      enableHttp2FrameLogging()
     }
 
-    client = createClient();
-    Request request = createRequest();
+    client = createClient()
+    val request = createRequest()
+
+    if (request == null) {
+      help?.showHelp()
+      return
+    }
+
     try {
-      Response response = client.newCall(request).execute();
+      val response = client.newCall(request).execute()
       if (showHeaders) {
-        System.out.println(StatusLine.get(response));
-        Headers headers = response.headers();
-        for (int i = 0, size = headers.size(); i < size; i++) {
-          System.out.println(headers.name(i) + ": " + headers.value(i));
+        println(StatusLine.get(response))
+        val headers = response.headers()
+        for (i in 0 until headers.size()) {
+          println(headers.name(i) + ": " + headers.value(i))
         }
-        System.out.println();
+        println()
       }
 
       // Stream the response to the System.out as it is returned from the server.
-      Sink out = Okio.sink(System.out);
-      BufferedSource source = response.body().source();
+      val out = System.out.sink()
+      val source = response.body()!!.source()
       while (!source.exhausted()) {
-        out.write(source.getBuffer(), source.getBuffer().size());
-        out.flush();
+        out.write(source.buffer, source.buffer.size)
+        out.flush()
       }
 
-      response.body().close();
-    } catch (IOException e) {
-      e.printStackTrace();
+      response.body()!!.close()
+    } catch (e: IOException) {
+      e.printStackTrace()
     } finally {
-      close();
+      close()
     }
   }
 
-  private OkHttpClient createClient() {
-    OkHttpClient.Builder builder = new OkHttpClient.Builder();
-    builder.followSslRedirects(followRedirects);
+  private fun createClient(): OkHttpClient {
+    val builder = OkHttpClient.Builder()
+    builder.followSslRedirects(followRedirects)
     if (connectTimeout != DEFAULT_TIMEOUT) {
-      builder.connectTimeout(connectTimeout, SECONDS);
+      builder.connectTimeout(connectTimeout.toLong(), SECONDS)
     }
     if (readTimeout != DEFAULT_TIMEOUT) {
-      builder.readTimeout(readTimeout, SECONDS);
+      builder.readTimeout(readTimeout.toLong(), SECONDS)
     }
     if (callTimeout != DEFAULT_TIMEOUT) {
-      builder.callTimeout(callTimeout, SECONDS);
+      builder.callTimeout(callTimeout.toLong(), SECONDS)
     }
     if (allowInsecure) {
-      X509TrustManager trustManager = createInsecureTrustManager();
-      SSLSocketFactory sslSocketFactory = createInsecureSslSocketFactory(trustManager);
-      builder.sslSocketFactory(sslSocketFactory, trustManager);
-      builder.hostnameVerifier(createInsecureHostnameVerifier());
+      val trustManager = createInsecureTrustManager()
+      val sslSocketFactory = createInsecureSslSocketFactory(trustManager)
+      builder.sslSocketFactory(sslSocketFactory, trustManager)
+      builder.hostnameVerifier(createInsecureHostnameVerifier())
     }
     if (verbose) {
-      HttpLoggingInterceptor.Logger logger = System.out::println;
-      builder.eventListenerFactory(new LoggingEventListener.Factory(logger));
+      val logger = HttpLoggingInterceptor.Logger { println(it) }
+      builder.eventListenerFactory(LoggingEventListener.Factory(logger))
     }
-    return builder.build();
+    return builder.build()
   }
 
-  private String getRequestMethod() {
-    if (method != null) {
-      return method;
+  fun createRequest(): Request? {
+    val request = Request.Builder()
+
+    val requestUrl = url ?: return null
+    val requestMethod = method ?: if (data != null) "POST" else "GET"
+
+    request.url(requestUrl)
+
+    data?.let {
+      request.method(requestMethod, RequestBody.create(mediaType(), it))
     }
-    if (data != null) {
-      return "POST";
+
+    for (header in headers.orEmpty()) {
+      val parts = header.split(":".toRegex(), 2).toTypedArray()
+      request.header(parts[0], parts[1])
     }
-    return "GET";
+    referer?.let {
+      request.header("Referer", it)
+    }
+    request.header("User-Agent", userAgent)
+
+    return request.build()
   }
 
-  private RequestBody getRequestBody() {
-    if (data == null) {
-      return null;
-    }
-    String bodyData = data;
-
-    String mimeType = "application/x-www-form-urlencoded";
-    if (headers != null) {
-      for (String header : headers) {
-        String[] parts = header.split(":", -1);
-        if ("Content-Type".equalsIgnoreCase(parts[0])) {
-          mimeType = parts[1].trim();
-          headers.remove(header);
-          break;
+  private fun mediaType(): MediaType? {
+    val mimeType = headers?.let {
+      for (header in it) {
+        val parts = header.split(":".toRegex()).toTypedArray()
+        if ("Content-Type".equals(parts[0], ignoreCase = true)) {
+          it.remove(header)
+          return@let parts[1].trim { c -> c <= ' ' }
         }
       }
+      return@let null
+    } ?: "application/x-www-form-urlencoded"
+
+    return MediaType.parse(mimeType)
+  }
+
+  private fun close() {
+    client.connectionPool().evictAll() // Close any persistent connections.
+  }
+
+  companion object {
+    internal const val NAME = "okcurl"
+    internal const val DEFAULT_TIMEOUT = -1
+    private var frameLogger: Logger? = null
+
+    @JvmStatic
+    fun fromArgs(vararg args: String): Main =
+        SingleCommand.singleCommand(Main::class.java).parse(*args)
+
+    @JvmStatic
+    fun main(args: Array<String>) {
+      Main.fromArgs(*args).run()
     }
 
-    return RequestBody.create(MediaType.parse(mimeType), bodyData);
-  }
-
-  Request createRequest() {
-    Request.Builder request = new Request.Builder();
-
-    request.url(url);
-    request.method(getRequestMethod(), getRequestBody());
-
-    if (headers != null) {
-      for (String header : headers) {
-        String[] parts = header.split(":", 2);
-        request.header(parts[0], parts[1]);
+    private fun versionString(): String {
+      try {
+        val prop = Properties()
+        Main::class.java.getResourceAsStream("/okcurl-version.properties").use {
+          prop.load(it)
+        }
+        return prop.getProperty("version", "dev")
+      } catch (e: IOException) {
+        throw AssertionError("Could not load okcurl-version.properties.")
       }
     }
-    if (referer != null) {
-      request.header("Referer", referer);
+
+    private fun protocols() = Protocol.values().joinToString(", ")
+
+    private fun createInsecureTrustManager(): X509TrustManager = object : X509TrustManager {
+      override fun checkClientTrusted(chain: Array<X509Certificate>, authType: String) {}
+
+      override fun checkServerTrusted(chain: Array<X509Certificate>, authType: String) {}
+
+      override fun getAcceptedIssuers(): Array<X509Certificate> = arrayOf()
     }
-    request.header("User-Agent", userAgent);
 
-    return request.build();
-  }
-
-  private void close() {
-    client.connectionPool().evictAll(); // Close any persistent connections.
-  }
-
-  private static X509TrustManager createInsecureTrustManager() {
-    return new X509TrustManager() {
-      @Override public void checkClientTrusted(X509Certificate[] chain, String authType) {
+    private fun createInsecureSslSocketFactory(trustManager: TrustManager): SSLSocketFactory {
+      try {
+        val context = Platform.get().newSSLContext()
+        context.init(null, arrayOf(trustManager), null)
+        return context.socketFactory
+      } catch (e: Exception) {
+        throw AssertionError(e)
       }
-
-      @Override public void checkServerTrusted(X509Certificate[] chain, String authType) {
-      }
-
-      @Override public X509Certificate[] getAcceptedIssuers() {
-        return new X509Certificate[0];
-      }
-    };
-  }
-
-  private static SSLSocketFactory createInsecureSslSocketFactory(TrustManager trustManager) {
-    try {
-      SSLContext context = Platform.get().newSSLContext();
-      context.init(null, new TrustManager[] {trustManager}, null);
-      return context.getSocketFactory();
-    } catch (Exception e) {
-      throw new AssertionError(e);
     }
-  }
 
-  private static HostnameVerifier createInsecureHostnameVerifier() {
-    return (name, session) -> true;
-  }
+    private fun createInsecureHostnameVerifier(): HostnameVerifier =
+        HostnameVerifier { _, _ -> true }
 
-  private static void enableHttp2FrameLogging() {
-    frameLogger = Logger.getLogger(Http2.class.getName());
-    frameLogger.setLevel(Level.FINE);
-    ConsoleHandler handler = new ConsoleHandler();
-    handler.setLevel(Level.FINE);
-    handler.setFormatter(new SimpleFormatter() {
-      @Override public String format(LogRecord record) {
-        return Util.format("%s%n", record.getMessage());
+    private fun enableHttp2FrameLogging() {
+      frameLogger = Logger.getLogger(Http2::class.java.name).apply {
+        level = Level.FINE
+        addHandler(ConsoleHandler().apply {
+          level = Level.FINE
+          formatter = object : SimpleFormatter() {
+            override fun format(record: LogRecord): String {
+              return Util.format("%s%n", record.message)
+            }
+          }
+        })
       }
-    });
-    frameLogger.addHandler(handler);
+    }
   }
 }
