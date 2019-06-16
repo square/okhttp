2227a6dd91f0c2c92f2506cea82260223584cab5
Masaru Nomura <massa.nomura@gmail.com>
2019-06-16 16:07:01 -0400

    feedback: define a class starting with -

--- okhttp-logging-interceptor/src/main/java/okhttp3/logging/-UTF8.kt
+++ okhttp-logging-interceptor/src/main/java/okhttp3/logging/-UTF8.kt
@@ -13,7 +13,7 @@
  * See the License for the specific language governing permissions and
  * limitations under the License.
  */
-@file:JvmName("UTF8")
+@file:JvmName("-UTF8")
 
 package okhttp3.logging
 
