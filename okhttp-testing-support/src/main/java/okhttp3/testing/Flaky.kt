28cf4819363b881937d7c3f79ac48e4ac5404004
Yuri Schimke <yuri@schimke.ee>
2019-07-06 12:18:46 +0100

    Collect information of Flaky conscrypt test

--- okhttp-testing-support/src/main/java/okhttp3/testing/Flaky.kt
+++ okhttp-testing-support/src/main/java/okhttp3/testing/Flaky.kt
@@ -2,4 +2,8 @@ package okhttp3.testing
 
 @Target(AnnotationTarget.CLASS, AnnotationTarget.FUNCTION)
 @Retention(AnnotationRetention.RUNTIME)
+/**
+ * Annotation marking a test as flaky, and requires extra logging and linking against
+ * a known github issue.  This does not ignore the failure.
+ */
 annotation class Flaky(val issues: Array<String> = [])
