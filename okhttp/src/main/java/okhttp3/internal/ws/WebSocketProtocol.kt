a70f33635baa79ba5a94c4c7a271e41fc6587e97
Jake Wharton <jakew@google.com>
2019-07-08 13:47:17 -0400

    Support new IANA WS close codes
    https://www.iana.org/assignments/websocket/websocket.txt

--- okhttp/src/main/java/okhttp3/internal/ws/WebSocketProtocol.kt
+++ okhttp/src/main/java/okhttp3/internal/ws/WebSocketProtocol.kt
@@ -122,7 +122,7 @@ object WebSocketProtocol {
   fun closeCodeExceptionMessage(code: Int): String? {
     return if (code < 1000 || code >= 5000) {
       "Code must be in range [1000,5000): $code"
-    } else if (code in 1004..1006 || code in 1012..2999) {
+    } else if (code in 1004..1006 || code in 1015..2999) {
       "Code $code is reserved and may not be used."
     } else {
       null
