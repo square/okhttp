a744c84244935824de8e7a06d94140ae343bb367
Yuri Schimke <yuri@schimke.ee>
2019-07-16 23:10:11 +0100

    Review comments

--- android-test/src/androidTest/java/okhttp/android/test/OkHttpTest.kt
+++ android-test/src/androidTest/java/okhttp/android/test/OkHttpTest.kt
@@ -1,3 +1,18 @@
+/*
+ * Copyright (C) 2019 Square, Inc.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
 package okhttp.android.test
 
 import android.os.Build
@@ -12,9 +27,12 @@ import okhttp3.TlsVersion
 import org.junit.After
 import org.junit.Assert.assertEquals
 import org.junit.Assert.fail
+import org.junit.Assume
 import org.junit.Before
 import org.junit.Test
 import org.junit.runner.RunWith
+import java.net.InetAddress
+import java.net.UnknownHostException
 
 /**
  * Run with "./gradlew :android-test:connectedCheck" and make sure ANDROID_SDK_ROOT is set.
@@ -36,6 +54,8 @@ class OkHttpTest {
 
   @Test
   fun testRequest() {
+    assumeNetwork()
+
     val request = Request.Builder().url("https://api.twitter.com/robots.txt").build()
 
     val response = client.newCall(request).execute()
@@ -47,6 +67,8 @@ class OkHttpTest {
 
   @Test
   fun testRequestUsesAndroidConscrypt() {
+    assumeNetwork()
+
     val request = Request.Builder().url("https://facebook.com/robots.txt").build()
 
     var socketClass: String? = null
@@ -75,12 +97,22 @@ class OkHttpTest {
 
   @Test
   fun testHttpRequestBlocked() {
+    Assume.assumeTrue(Build.VERSION.SDK_INT >= 23)
+
     val request = Request.Builder().url("http://api.twitter.com/robots.txt").build()
 
     try {
       client.newCall(request).execute()
       fail("expected cleartext blocking")
     } catch (_: java.net.UnknownServiceException) {
+    }
+  }
+
+  private fun assumeNetwork() {
+    try {
+      InetAddress.getByName("www.google.com")
+    } catch (uhe: UnknownHostException) {
+      Assume.assumeNoException(uhe)
     }
   }
 }
