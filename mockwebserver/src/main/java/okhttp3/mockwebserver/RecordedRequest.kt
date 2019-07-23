3d1f0d441a17c3678259102deec713d37331ba7b
Yuri Schimke <yuri@schimke.ee>
2019-07-23 17:48:42 +0100

    Update RecordedRequest.kt

--- mockwebserver/src/main/java/okhttp3/mockwebserver/RecordedRequest.kt
+++ mockwebserver/src/main/java/okhttp3/mockwebserver/RecordedRequest.kt
@@ -102,7 +102,7 @@ class RecordedRequest(
 
       var hostname = inetAddress.hostName
       if (inetAddress is Inet6Address && hostname.contains(':')) {
-        // hostname is likely some form representing the ipv6 bytes
+        // hostname is likely some form representing the IPv6 bytes
         // 2001:0db8:85a3:0000:0000:8a2e:0370:7334
         // 2001:db8:85a3::8a2e:370:7334
         // ::1
