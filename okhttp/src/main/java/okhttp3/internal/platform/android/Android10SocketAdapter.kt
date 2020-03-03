21d71c71d19030ef26dcc88182d16a37d3441bf9
Yuri Schimke <yuri@schimke.ee>
2020-03-03 19:23:47 +0000

    Catch IllegalArgumentException from Android internals

--- okhttp/src/main/java/okhttp3/internal/platform/android/Android10SocketAdapter.kt
+++ okhttp/src/main/java/okhttp3/internal/platform/android/Android10SocketAdapter.kt
@@ -24,6 +24,8 @@ import javax.net.ssl.X509TrustManager
 import okhttp3.Protocol
 import okhttp3.internal.platform.AndroidPlatform.Companion.isAndroid
 import okhttp3.internal.platform.Platform
+import java.io.IOException
+import java.lang.IllegalArgumentException
 
 /**
  * Simple non-reflection SocketAdapter for Android Q.
@@ -54,14 +56,19 @@ class Android10SocketAdapter : SocketAdapter {
     hostname: String?,
     protocols: List<Protocol>
   ) {
-    socketFactory.setUseSessionTickets(sslSocket, true)
+    try {
+      socketFactory.setUseSessionTickets(sslSocket, true)
 
-    val sslParameters = sslSocket.sslParameters
+      val sslParameters = sslSocket.sslParameters
 
-    // Enable ALPN.
-    sslParameters.applicationProtocols = Platform.alpnProtocolNames(protocols).toTypedArray()
+      // Enable ALPN.
+      sslParameters.applicationProtocols = Platform.alpnProtocolNames(protocols).toTypedArray()
 
-    sslSocket.sslParameters = sslParameters
+      sslSocket.sslParameters = sslParameters
+    } catch (iae: IllegalArgumentException) {
+      // probably java.lang.IllegalArgumentException: Invalid input to toASCII from IDN.toASCII
+      throw IOException("Android internal error", iae)
+    }
   }
 
   companion object {
