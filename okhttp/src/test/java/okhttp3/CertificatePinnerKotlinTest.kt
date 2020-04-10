d0dd22fac8ea5e4116af3778767c0cbc51cf0207
Yuri Schimke <yuri@schimke.ee>
2020-04-10 17:50:55 +0100

    imports

--- okhttp/src/test/java/okhttp3/CertificatePinnerKotlinTest.kt
+++ okhttp/src/test/java/okhttp3/CertificatePinnerKotlinTest.kt
@@ -25,8 +25,6 @@ import org.junit.Assert.assertEquals
 import org.junit.Assert.assertFalse
 import org.junit.Assert.assertTrue
 import org.junit.Test
-import java.util.Arrays
-import java.util.HashSet
 
 class CertificatePinnerKotlinTest {
 
