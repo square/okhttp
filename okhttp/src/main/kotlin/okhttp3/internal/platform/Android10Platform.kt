7b092ae52f9b42284517b19b17fb1f21c64cddb8
Yuri Schimke <yuri@schimke.ee>
2020-04-18 14:14:58 +0100

    clean

--- okhttp/src/main/kotlin/okhttp3/internal/platform/Android10Platform.kt
+++ okhttp/src/main/kotlin/okhttp3/internal/platform/Android10Platform.kt
@@ -24,8 +24,8 @@ import javax.net.ssl.X509TrustManager
 import okhttp3.Protocol
 import okhttp3.internal.SuppressSignatureCheck
 import okhttp3.internal.platform.AndroidPlatform.Companion.isAndroid
-import okhttp3.internal.platform.android.AndroidCertificateChainCleaner
 import okhttp3.internal.platform.android.Android10SocketAdapter
+import okhttp3.internal.platform.android.AndroidCertificateChainCleaner
 import okhttp3.internal.platform.android.BouncyCastleSocketAdapter
 import okhttp3.internal.platform.android.ConscryptSocketAdapter
 import okhttp3.internal.platform.android.DeferredSocketAdapter
