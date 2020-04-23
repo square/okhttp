73a3c7513d65a6687984568ed370ad6822b12db7
Yuri Schimke <yuri@schimke.ee>
2020-04-23 19:45:14 +0100

    Add test

--- okhttp/src/main/kotlin/okhttp3/internal/authenticator/JavaNetAuthenticator.kt
+++ okhttp/src/main/kotlin/okhttp3/internal/authenticator/JavaNetAuthenticator.kt
@@ -16,6 +16,7 @@
 package okhttp3.internal.authenticator
 
 import okhttp3.Credentials
+import okhttp3.Dns
 import okhttp3.HttpUrl
 import okhttp3.Request
 import okhttp3.Response
@@ -30,7 +31,7 @@ import java.net.Proxy
  * Adapts [Authenticator] to [okhttp3.Authenticator]. Configure OkHttp to use [Authenticator] with
  * [OkHttpClient.Builder.authenticator] or [OkHttpClient.Builder.proxyAuthenticator].
  */
-class JavaNetAuthenticator : okhttp3.Authenticator {
+class JavaNetAuthenticator(private val defaultDns: Dns = Dns.SYSTEM) : okhttp3.Authenticator {
   @Throws(IOException::class)
   override fun authenticate(route: Route?, response: Response): Request? {
     val challenges = response.challenges()
@@ -44,11 +45,12 @@ class JavaNetAuthenticator : okhttp3.Authenticator {
         continue
       }
 
+      val dns = route?.address?.dns ?: defaultDns
       val auth = if (proxyAuthorization) {
         val proxyAddress = proxy.address() as InetSocketAddress
         Authenticator.requestPasswordAuthentication(
             proxyAddress.hostName,
-            proxy.connectToInetAddress(url),
+            proxy.connectToInetAddress(url, dns),
             proxyAddress.port,
             url.scheme,
             challenge.realm,
@@ -59,7 +61,7 @@ class JavaNetAuthenticator : okhttp3.Authenticator {
       } else {
         Authenticator.requestPasswordAuthentication(
             url.host,
-            proxy.connectToInetAddress(url),
+            proxy.connectToInetAddress(url, dns),
             url.port,
             url.scheme,
             challenge.realm,
@@ -83,9 +85,9 @@ class JavaNetAuthenticator : okhttp3.Authenticator {
   }
 
   @Throws(IOException::class)
-  private fun Proxy.connectToInetAddress(url: HttpUrl): InetAddress {
-    return when {
-      type() == Proxy.Type.DIRECT -> InetAddress.getByName(url.host)
+  private fun Proxy.connectToInetAddress(url: HttpUrl, dns: Dns): InetAddress {
+    return when(type()) {
+      Proxy.Type.DIRECT -> dns.lookup(url.host).first()
       else -> (address() as InetSocketAddress).address
     }
   }
---
+++ okhttp/src/test/java/okhttp3/internal/authenticator/JavaNetAuthenticatorTest.kt
@@ -0,0 +1,74 @@
+package okhttp3.internal.authenticator
+
+import okhttp3.Address
+import okhttp3.CertificatePinner
+import okhttp3.ConnectionSpec
+import okhttp3.FakeDns
+import okhttp3.Protocol.HTTP_1_1
+import okhttp3.Protocol.HTTP_2
+import okhttp3.Request
+import okhttp3.Response
+import okhttp3.Route
+import okhttp3.internal.RecordingAuthenticator
+import okhttp3.internal.proxy.NullProxySelector
+import okhttp3.internal.tls.OkHostnameVerifier
+import okhttp3.tls.internal.TlsUtil
+import org.junit.After
+import org.junit.Assert.assertEquals
+import org.junit.Assert.assertNotNull
+import org.junit.Before
+import org.junit.Test
+import java.net.Authenticator
+import java.net.InetAddress
+import java.net.InetSocketAddress
+import java.net.Proxy
+import javax.net.SocketFactory
+
+// Most tests from URLConnectionTest
+class JavaNetAuthenticatorTest {
+  var authenticator = JavaNetAuthenticator()
+
+  val fakeDns = FakeDns()
+
+  val recordingAuthenticator = RecordingAuthenticator()
+
+  @Before
+  fun setup() {
+    Authenticator.setDefault(recordingAuthenticator)
+  }
+
+  @After
+  fun tearDown() {
+    Authenticator.setDefault(null)
+  }
+
+  @Test
+  fun testBasicAuth() {
+    fakeDns["server"] = listOf(InetAddress.getLocalHost())
+
+    val address = Address(
+        "server", 443, fakeDns, SocketFactory.getDefault(), TlsUtil.localhost()
+        .sslSocketFactory(), OkHostnameVerifier, CertificatePinner.DEFAULT,
+        okhttp3.Authenticator.NONE, Proxy.NO_PROXY, listOf(HTTP_1_1),
+        listOf(ConnectionSpec.MODERN_TLS), NullProxySelector
+    )
+    val route = Route(address, Proxy.NO_PROXY, InetSocketAddress.createUnresolved("server", 443))
+
+    val request = Request.Builder()
+        .url("https://server/robots.txt")
+        .build()
+    val response = Response.Builder()
+        .request(request)
+        .code(401)
+        .header("WWW-Authenticate", "Basic realm=\"User Visible Realm\"")
+        .protocol(HTTP_2)
+        .message("Unauthorized")
+        .build()
+    val authRequest = authenticator.authenticate(route, response)
+
+    assertNotNull(authRequest)
+    assertEquals(
+        "Basic ${RecordingAuthenticator.BASE_64_CREDENTIALS}", authRequest!!.header("Authorization")
+    )
+  }
+}
