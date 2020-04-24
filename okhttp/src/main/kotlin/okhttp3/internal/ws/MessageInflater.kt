7d456d0e520a81f9413820157c0bb66e918b596d
Jesse Wilson <jwilson@squareup.com>
2020-04-23 22:26:31 -0400

    Don't crash processing fragmented web sockets messages
    Closes: https://github.com/square/okhttp/issues/5965

--- okhttp/src/main/kotlin/okhttp3/internal/ws/MessageInflater.kt
+++ okhttp/src/main/kotlin/okhttp3/internal/ws/MessageInflater.kt
@@ -35,14 +35,6 @@ class MessageInflater(
   fun inflate(buffer: Buffer) {
     require(deflatedBytes.size == 0L)
 
-    // Handle the empty message special case. The compressed empty message is one byte, '0x00'. We
-    // can't use the normal flow here because inflaterSource.read() throws EOFException if the
-    // deflated stream isn't complete but there's no bytes to return.
-    if (buffer.size == 1L && buffer[0L] == 0.toByte()) {
-      buffer.skip(1L)
-      return
-    }
-
     if (noContextTakeover) {
       inflater.reset()
     }
@@ -55,7 +47,7 @@ class MessageInflater(
     // We cannot read all, as the source does not close.
     // Instead, we ensure that all bytes from source have been processed by inflater.
     do {
-      inflaterSource.read(buffer, Long.MAX_VALUE)
+      inflaterSource.readOrInflate(buffer, Long.MAX_VALUE)
     } while (inflater.bytesRead < totalBytesToRead)
   }
 
