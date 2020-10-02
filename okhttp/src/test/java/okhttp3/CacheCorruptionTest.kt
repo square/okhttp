10e38e388a13066292d8dbb069510ff0c85872df
Yuri Schimke <yuri@schimke.ee>
2020-10-02 08:01:22 +0100

    Cleanup

--- okhttp/src/test/java/okhttp3/CacheCorruptionTest.kt
+++ okhttp/src/test/java/okhttp3/CacheCorruptionTest.kt
@@ -1,3 +1,18 @@
+/*
+ * Copyright (C) 2020 Square, Inc.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
 package okhttp3
 
 import okhttp3.internal.io.InMemoryFileSystem
@@ -63,6 +78,7 @@ class CacheCorruptionTest {
   @Test fun corruptedCipher() {
     val response = testCorruptingCache {
       corruptMetadata {
+        // mess with cipher suite
         it.replace("TLS_", "SLT_")
       }
     }
@@ -95,7 +111,6 @@ class CacheCorruptionTest {
 
     val contents = metadataBuffer!!.peek().readUtf8()
 
-    // mess with cipher suite
     metadataBuffer.clear()
     metadataBuffer.writeUtf8(corruptor(contents))
   }
