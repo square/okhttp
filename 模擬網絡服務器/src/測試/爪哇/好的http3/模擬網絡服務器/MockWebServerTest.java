236e301fc375d68ee62689221d0e0ee9df3ac022
Tang wiki <51043112+abc52090241@users.noreply.github.com>
2020-12-30 04:24:41 +0800

    更新許可

--- 模擬網絡服務器/src/測試/爪哇/好的http3/模擬網絡服務器/MockWebServerTest.java
+++ 模擬網絡服務器/src/測試/爪哇/好的http3/模擬網絡服務器/MockWebServerTest.java
@@ -1,5 +1,5 @@
 /*
- * Copyright (C) 2011 Google Inc.
+ * Copyright (C) 2020 Google Inc.
  *
  * Licensed under the Apache License, Version 2.0 (the "License");
  * you may not use this file except in compliance with the License.
