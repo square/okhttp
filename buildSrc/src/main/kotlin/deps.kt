a849b5aed576b2dadaa8d786a878c51fcc891aa7
Yuri Schimke <yuri@schimke.ee>
2021-02-06 14:29:16 +0000

    Copyrights

--- buildSrc/src/main/kotlin/deps.kt
+++ buildSrc/src/main/kotlin/deps.kt
@@ -1,5 +1,5 @@
 /*
- * Copyright (C) 2020 Square, Inc.
+ * Copyright (C) 2021 Square, Inc.
  *
  * Licensed under the Apache License, Version 2.0 (the "License");
  * you may not use this file except in compliance with the License.
@@ -26,7 +26,6 @@ object Versions {
   const val equinox = "3.16.0"
   const val findbugs = "3.0.2"
   const val guava = "28.2-jre"
-  const val java = "1.8"
   const val jnrUnixsocket = "0.28"
   const val jsoup = "1.13.1"
   const val junit = "4.13"
@@ -34,6 +33,7 @@ object Versions {
   const val kotlin = "1.4.21"
   const val ktlint = "0.38.0"
   const val moshi = "1.11.0"
+  const val okio = "3.0.0-alpha.1"
   const val openjsse = "1.1.0"
   const val picocli = "4.5.1"
 }
@@ -66,11 +66,12 @@ object Dependencies {
   const val kotlinStdlibOsgi = "org.jetbrains.kotlin:kotlin-osgi-bundle:${Versions.kotlin}"
   const val moshi = "com.squareup.moshi:moshi:${Versions.moshi}"
   const val moshiKotlin = "com.squareup.moshi:moshi-kotlin-codegen:${Versions.moshi}"
-  const val okio = "com.squareup.okio:okio:2.9.0"
+  const val okio = "com.squareup.okio:okio:${Versions.okio}"
+  const val okioFakeFileSystem = "com.squareup.okio:okio-fakefilesystem:${Versions.okio}"
   const val openjsse = "org.openjsse:openjsse:${Versions.openjsse}"
   const val picocli = "info.picocli:picocli:${Versions.picocli}"
 
   object Kotlin {
-    const val version = "1.4.20"
+    const val version = "1.4.30"
   }
 }
