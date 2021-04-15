e4f09320ff1e0a1a1422641fc52732746605875a
Goooler <wangzongler@gmail.com>
2021-04-16 02:13:00 +0800

    Bump versions

--- buildSrc/src/main/kotlin/deps.kt
+++ buildSrc/src/main/kotlin/deps.kt
@@ -15,24 +15,24 @@
  */
 
 object Versions {
-  const val animalSniffer = "1.19"
-  const val assertj = "3.15.0"
-  const val bnd = "5.1.2"
+  const val animalSniffer = "1.20"
+  const val assertj = "3.19.0"
+  const val bnd = "5.3.0"
   const val bouncycastle = "1.68"
   const val brotli = "0.1.2"
-  const val checkstyle = "8.28"
+  const val checkstyle = "8.41.1"
   const val conscrypt = "2.5.2"
-  const val corretto = "1.3.1"
-  const val equinox = "3.16.0"
+  const val corretto = "1.6.0"
+  const val equinox = "3.16.200"
   const val findbugs = "3.0.2"
   const val guava = "28.2-jre"
-  const val jnrUnixsocket = "0.28"
+  const val jnrUnixsocket = "0.38.6"
   const val jsoup = "1.13.1"
-  const val junit = "4.13"
-  const val junit5 = "5.7.0"
-  const val kotlin = "1.4.31"
+  const val junit = "4.13.2"
+  const val junit5 = "5.7.1"
+  const val kotlin = "1.4.32"
   const val ktlint = "0.38.0"
-  const val moshi = "1.11.0"
+  const val moshi = "1.12.0"
   const val okio = "3.0.0-alpha.2"
   const val openjsse = "1.1.0"
   const val picocli = "4.5.1"
@@ -52,7 +52,7 @@ object Dependencies {
   const val corretto = "software.amazon.cryptools:AmazonCorrettoCryptoProvider:${Versions.corretto}:linux-x86_64"
   const val equinox = "org.eclipse.platform:org.eclipse.osgi:${Versions.equinox}"
   const val guava = "com.google.guava:guava:${Versions.guava}"
-  const val hamcrest = "org.hamcrest:hamcrest-library:2.1"
+  const val hamcrest = "org.hamcrest:hamcrest-library:2.2"
   const val jnrUnixsocket = "com.github.jnr:jnr-unixsocket:${Versions.jnrUnixsocket}"
   const val jsoup = "org.jsoup:jsoup:${Versions.jsoup}"
   const val jsr305 = "com.google.code.findbugs:jsr305:${Versions.findbugs}"
@@ -72,6 +72,6 @@ object Dependencies {
   const val picocli = "info.picocli:picocli:${Versions.picocli}"
 
   object Kotlin {
-    const val version = "1.4.31"
+    const val version = "1.4.32"
   }
 }
