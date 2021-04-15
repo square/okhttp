23238fba79d345df948bf162b05c9a136f066637
Yuri Schimke <yuri@schimke.ee>
2021-04-15 20:46:17 +0100

    Make tests more tolerant of JDK versions

--- okhttp/src/test/java/okhttp3/JSSETest.kt
+++ okhttp/src/test/java/okhttp3/JSSETest.kt
@@ -88,15 +88,15 @@ class JSSETest(
     val s = factory.createSocket() as SSLSocket
 
     when {
-      PlatformVersion.majorVersion > 11 -> {
-        assertThat(s.enabledProtocols.toList()).containsExactly("TLSv1.3", "TLSv1.2")
-      }
-      PlatformVersion.majorVersion == 8 -> {
-        assertThat(s.enabledProtocols.toList()).contains("TLSv1.2", "TLSv1.1", "TLSv1")
-      }
-      else -> {
-        assertThat(s.enabledProtocols.toList()).containsExactly("TLSv1.3", "TLSv1.2", "TLSv1.1", "TLSv1")
-      }
+      PlatformVersion.majorVersion > 11 -> assertThat(s.enabledProtocols.toList()).containsExactly(
+        "TLSv1.3", "TLSv1.2"
+      )
+      PlatformVersion.majorVersion == 8 -> assertThat(s.enabledProtocols.toList()).contains(
+        "TLSv1.2", "TLSv1.1", "TLSv1"
+      )
+      else -> assertThat(s.enabledProtocols.toList()).containsExactly(
+        "TLSv1.3", "TLSv1.2", "TLSv1.1", "TLSv1"
+      )
     }
   }
 
