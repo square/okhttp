7eb759dfbd0f57b4898295d9d89a517263b7c4e3
Rob Moore <rmoore@gradle.com>
2021-06-04 13:28:34 +0800

    Undisable the locally-failing tests
    Signed-off-by: Rob Moore <rmoore@gradle.com>

--- okhttp/src/test/java/okhttp3/JSSETest.kt
+++ okhttp/src/test/java/okhttp3/JSSETest.kt
@@ -53,7 +53,6 @@ class JSSETest(
   }
 
   @Test
-  @Disabled
   fun testTlsv13Works() {
     // https://docs.oracle.com/en/java/javase/14/security/java-secure-socket-extension-jsse-reference-guide.html
     // TODO test jdk.tls.client.enableSessionTicketExtension
@@ -83,7 +82,6 @@ class JSSETest(
   }
 
   @Test
-  @Disabled
   fun testSupportedProtocols() {
     val factory = SSLSocketFactory.getDefault()
     assertThat(factory.javaClass.name).isEqualTo("sun.security.ssl.SSLSocketFactoryImpl")
