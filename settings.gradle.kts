3b547b2c41d9d37fc7937deea8e6c153dfda3c6f
Rob Moore <rmoore@gradle.com>
2021-06-04 14:01:14 +0800

    Remove GE & build cache config
    Signed-off-by: Rob Moore <rmoore@gradle.com>

--- settings.gradle.kts
+++ settings.gradle.kts
@@ -1,28 +1,5 @@
 rootProject.name = "okhttp-parent"
 
-plugins {
-  id("com.gradle.enterprise").version("3.6.2")
-  id("com.gradle.common-custom-user-data-gradle-plugin").version("1.3")
-}
-
-gradleEnterprise {
-  server = "https://ec2-18-205-192-234.compute-1.amazonaws.com"
-  allowUntrustedServer = true
-  buildScan {
-    publishAlways()
-    isCaptureTaskInputFiles = true
-  }
-}
-
-buildCache {
-  local {
-    removeUnusedEntriesAfterDays = 1
-  }
-  remote<HttpBuildCache> {
-    isEnabled = false
-  }
-}
-
 include(":mockwebserver")
 include(":mockwebserver-deprecated")
 include(":mockwebserver-junit4")
