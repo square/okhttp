3fbdf520e34692942cc716e95a244b2e011af625
Yuri Schimke <yuri@schimke.ee>
2021-06-13 14:29:48 +1000

    Get a passing CI build of JDK 11.

--- okhttp/src/test/java/okhttp3/CallHandshakeTest.kt
+++ okhttp/src/test/java/okhttp3/CallHandshakeTest.kt
@@ -212,19 +212,29 @@ class CallHandshakeTest {
       platform.newSslSocketFactory(platform.platformTrustManager()).defaultCipherSuites
     val cipherSuites =
       ConnectionSpec.RESTRICTED_TLS.effectiveCipherSuites(platformDefaultCipherSuites)
-    assertThat(cipherSuites).containsExactlyElementsOf(listOf(
-      TLS_AES_128_GCM_SHA256,
-      TLS_AES_256_GCM_SHA384,
-      TLS_ECDHE_ECDSA_WITH_AES_256_GCM_SHA384,
-      TLS_ECDHE_ECDSA_WITH_AES_128_GCM_SHA256,
-      TLS_ECDHE_RSA_WITH_AES_256_GCM_SHA384,
-      TLS_ECDHE_RSA_WITH_AES_128_GCM_SHA256,
 
-      // Disabled
-//      TLS_CHACHA20_POLY1305_SHA256,
-//      TLS_ECDHE_ECDSA_WITH_CHACHA20_POLY1305_SHA256,
-//      TLS_ECDHE_RSA_WITH_CHACHA20_POLY1305_SHA256
-    ).map { it.javaName })
+    if (cipherSuites.contains(TLS_CHACHA20_POLY1305_SHA256)) {
+      assertThat(cipherSuites).containsExactlyElementsOf(listOf(
+        TLS_AES_256_GCM_SHA384,
+        TLS_AES_128_GCM_SHA256,
+        TLS_CHACHA20_POLY1305_SHA256,
+        TLS_ECDHE_ECDSA_WITH_AES_256_GCM_SHA384,
+        TLS_ECDHE_ECDSA_WITH_AES_128_GCM_SHA256,
+        TLS_ECDHE_ECDSA_WITH_CHACHA20_POLY1305_SHA256,
+        TLS_ECDHE_RSA_WITH_AES_256_GCM_SHA384,
+        TLS_ECDHE_RSA_WITH_CHACHA20_POLY1305_SHA256,
+        TLS_ECDHE_RSA_WITH_AES_128_GCM_SHA256,
+      ).map { it.javaName })
+    } else {
+      assertThat(cipherSuites).containsExactlyElementsOf(listOf(
+        TLS_AES_128_GCM_SHA256,
+        TLS_AES_256_GCM_SHA384,
+        TLS_ECDHE_ECDSA_WITH_AES_256_GCM_SHA384,
+        TLS_ECDHE_ECDSA_WITH_AES_128_GCM_SHA256,
+        TLS_ECDHE_RSA_WITH_AES_256_GCM_SHA384,
+        TLS_ECDHE_RSA_WITH_AES_128_GCM_SHA256,
+      ).map { it.javaName })
+    }
   }
 
   private fun expectedConnectionCipherSuites(client: OkHttpClient): Set<String> {
