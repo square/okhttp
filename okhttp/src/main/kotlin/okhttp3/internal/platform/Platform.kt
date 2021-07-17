1b58902663772dd9c6599032d68914bdc6042786
Yuri Schimke <yuri@schimke.ee>
2021-07-17 12:19:56 +0100

    Fixes

--- okhttp/src/main/kotlin/okhttp3/internal/platform/Platform.kt
+++ okhttp/src/main/kotlin/okhttp3/internal/platform/Platform.kt
@@ -96,9 +96,13 @@ open class Platform {
       readFieldOrNull(context, X509TrustManager::class.java, "trustManager")
     } catch (e: ClassNotFoundException) {
       null
-    } catch (e: InaccessibleObjectException) {
-      // Throws InaccessibleObjectException on JDK 17 due to
+    } catch (e: RuntimeException) {
+      // Throws InaccessibleObjectException (added in JDK9) on JDK 17 due to
       // JEP 403 Strongly Encapsulate JDK Internals.
+      if (e.javaClass.name != "java.lang.reflect.InaccessibleObjectException") {
+        throw e
+      }
+
       null
     }
   }
