bee0bf82cef2738098f26029648fe4edc8cffddc
Yuri Schimke <yuri@schimke.ee>
2021-07-17 20:34:36 +0100

    Okio alpha 8 (#6753)

--- buildSrc/src/main/kotlin/deps.kt
+++ buildSrc/src/main/kotlin/deps.kt
@@ -33,7 +33,7 @@ object Versions {
   const val kotlin = "1.5.0"
   const val ktlint = "0.38.0"
   const val moshi = "1.12.0"
-  const val okio = "3.0.0-alpha.6"
+  const val okio = "3.0.0-alpha.8"
   const val openjsse = "1.1.6"
   const val picocli = "4.5.1"
 }
