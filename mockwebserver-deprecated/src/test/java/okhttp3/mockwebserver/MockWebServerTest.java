b357ad36737312e88fa89f21963970ac63c82aaf
Yuri Schimke <yuri@schimke.ee>
2021-08-02 08:07:15 +0300

    Case insensitive hostname check

--- mockwebserver-deprecated/src/test/java/okhttp3/mockwebserver/MockWebServerTest.java
+++ mockwebserver-deprecated/src/test/java/okhttp3/mockwebserver/MockWebServerTest.java
@@ -468,7 +468,7 @@ public final class MockWebServerTest {
 
     HttpUrl requestUrl = request.getRequestUrl();
     assertThat(requestUrl.scheme()).isEqualTo("http");
-    assertThat(requestUrl.host()).isEqualTo(server.getHostName());
+    assertThat(requestUrl.host()).isEqualToIgnoringCase(server.getHostName());
     assertThat(requestUrl.port()).isEqualTo(server.getPort());
     assertThat(requestUrl.encodedPath()).isEqualTo("/a/deep/path");
     assertThat(requestUrl.queryParameter("key")).isEqualTo("foo bar");
