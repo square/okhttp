25484e3c365ca13e85ca783a55a1a2b0ab56a3a4
Goooler <wangzongler@gmail.com>
2021-08-03 22:59:18 +0800

    Convert okhttp-dnsoverhttps module's gradle to kts

---
+++ okhttp-dnsoverhttps/build.gradle.kts
@@ -0,0 +1,26 @@
+Projects.applyOsgi(
+  project,
+  "Export-Package: okhttp3.dnsoverhttps",
+  "Automatic-Module-Name: okhttp3.dnsoverhttps",
+  "Bundle-SymbolicName: com.squareup.okhttp3.dnsoverhttps"
+)
+
+dependencies {
+  api(project(":okhttp"))
+  compileOnly(Dependencies.jsr305)
+
+  testImplementation(project(":okhttp-testing-support"))
+  testImplementation(project(":mockwebserver-deprecated"))
+  testImplementation(project(":mockwebserver-junit5"))
+  testImplementation(Dependencies.okioFakeFileSystem)
+  testImplementation(Dependencies.conscrypt)
+  testImplementation(Dependencies.junit)
+  testImplementation(Dependencies.assertj)
+}
+
+afterEvaluate {
+  tasks.dokka {
+    outputDirectory = "$rootDir/docs/4.x"
+    outputFormat = "gfm"
+  }
+}
