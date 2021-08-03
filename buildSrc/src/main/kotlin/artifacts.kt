a8bc6b6fe287739ee8efca601df90419fa744af1
Goooler <wangzongler@gmail.com>
2021-08-03 21:41:59 +0800

    Refactor baselineJar to kotlin

--- buildSrc/src/main/kotlin/artifacts.kt
+++ buildSrc/src/main/kotlin/artifacts.kt
@@ -15,10 +15,12 @@
  */
 
 import aQute.bnd.gradle.BundleTaskConvention
+import java.io.File
 import org.gradle.api.Project
 import org.gradle.api.plugins.ExtensionAware
 import org.gradle.api.tasks.SourceSetContainer
 import org.gradle.kotlin.dsl.apply
+import org.gradle.kotlin.dsl.extra
 import org.gradle.kotlin.dsl.get
 import org.gradle.kotlin.dsl.withConvention
 
@@ -56,7 +58,28 @@ object Projects {
       dependencies.add("osgiApi", Dependencies.kotlinStdlibOsgi)
     }
   }
+
+  /**
+   * Returns a .jar file for the golden version of this project.
+   * https://github.com/Visistema/Groovy1/blob/ba5eb9b2f19ca0cc8927359ce414c4e1974b7016/gradle/binarycompatibility.gradle#L48
+   */
+  @JvmStatic
+  @JvmOverloads
+  fun baselineJar(project: Project, version: String = "3.14.1"): File? {
+    val group = project.group
+    val artifactId = project.extra["artifactId"]
+    return try {
+      val jarFile = "$artifactId-${version}.jar"
+      project.group = "virtual_group_for_japicmp"
+      val dependency = project.dependencies.create("$group:$artifactId:$version@jar")
+      project.configurations.detachedConfiguration(dependency).files.find { (it.name == jarFile) }
+    } catch (e: Exception) {
+      null
+    } finally {
+      project.group = group
+    }
+  }
 }
 
-val org.gradle.api.Project.sourceSets: SourceSetContainer
+val Project.sourceSets: SourceSetContainer
   get() = (this as ExtensionAware).extensions.getByName("sourceSets") as SourceSetContainer
