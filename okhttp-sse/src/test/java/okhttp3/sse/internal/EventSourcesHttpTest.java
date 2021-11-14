4b8880aa29eb801d1610f73ea7f6a6479363be30
Yuri Schimke <yuri@schimke.ee>
2021-11-14 09:35:27 +0000

    Android 12 Build Dependency (#6892)
    * Android 12 Build Dependency
    * Add extension required for android junit5 runner

--- okhttp-sse/src/test/java/okhttp3/sse/internal/EventSourcesHttpTest.java
+++ okhttp-sse/src/test/java/okhttp3/sse/internal/EventSourcesHttpTest.java
@@ -17,6 +17,7 @@ package okhttp3.sse.internal;
 
 import mockwebserver3.MockResponse;
 import mockwebserver3.MockWebServer;
+import mockwebserver3.junit5.internal.MockWebServerExtension;
 import okhttp3.OkHttpClient;
 import okhttp3.OkHttpClientTestRule;
 import okhttp3.Request;
@@ -27,11 +28,13 @@ import org.junit.jupiter.api.AfterEach;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Tag;
 import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.extension.ExtendWith;
 import org.junit.jupiter.api.extension.RegisterExtension;
 
 import java.io.IOException;
 
 @Tag("Slowish")
+@ExtendWith(MockWebServerExtension.class)
 public final class EventSourcesHttpTest {
   @RegisterExtension public final PlatformRule platform = new PlatformRule();
 
