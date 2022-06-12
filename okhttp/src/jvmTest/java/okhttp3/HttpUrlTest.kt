0658b6756f1248eb9575baff6af773667a6cd408
Jesse Wilson <jwilson@squareup.com>
2022-06-12 16:29:20 -0400

    Enforce label length limits (#7324)
    * Enforce label length limits
    
    Closes: https://github.com/square/okhttp/issues/7301
    
    * Fixup other impacted tests

--- okhttp/src/jvmTest/java/okhttp3/HttpUrlTest.kt
+++ okhttp/src/jvmTest/java/okhttp3/HttpUrlTest.kt
@@ -2224,7 +2224,6 @@ open class HttpUrlTest {
 
     // https://github.com/square/okhttp/issues/6109
     assertThat(parse("http://a./").topPrivateDomain()).isNull()
-    assertThat(parse("http://./").topPrivateDomain()).isNull()
     assertThat(parse("http://squareup.com./").topPrivateDomain())
       .isEqualTo("squareup.com")
   }
@@ -2255,5 +2254,51 @@ open class HttpUrlTest {
     assertThat(parse("http://facebooK.com").host).isEqualTo("facebook.com")
     assertThat(parse("http://Facebook.com").host).isEqualTo("facebook.com")
     assertThat(parse("http://FacebooK.com").host).isEqualTo("facebook.com")
+  }
+
+  @Test
+  fun trailingDotIsOkay() {
+    val name251 = "a.".repeat(125) + "a"
+    assertThat(parse("http://a./").toString()).isEqualTo("http://a./")
+    assertThat(parse("http://${name251}a./").toString()).isEqualTo("http://${name251}a./")
+    assertThat(parse("http://${name251}aa/").toString()).isEqualTo("http://${name251}aa/")
+    assertInvalid("http://${name251}aa./", "Invalid URL host: \"${name251}aa.\"")
+  }
+
+  @Test
+  fun labelIsEmpty() {
+    assertInvalid("http:///", "Invalid URL host: \"\"")
+    assertInvalid("http://a..b/", "Invalid URL host: \"a..b\"")
+    assertInvalid("http://.a/", "Invalid URL host: \".a\"")
+    assertInvalid("http://./", "Invalid URL host: \".\"")
+    assertInvalid("http://../", "Invalid URL host: \"..\"")
+    assertInvalid("http://.../", "Invalid URL host: \"...\"")
+    assertInvalid("http://…/", "Invalid URL host: \"…\"")
+  }
+
+  @Test
+  fun labelTooLong() {
+    val a63 = "a".repeat(63)
+    assertThat(parse("http://$a63/").toString()).isEqualTo("http://$a63/")
+    assertThat(parse("http://a.$a63/").toString()).isEqualTo("http://a.$a63/")
+    assertThat(parse("http://$a63.a/").toString()).isEqualTo("http://$a63.a/")
+    assertInvalid("http://a$a63/", "Invalid URL host: \"a$a63\"")
+    assertInvalid("http://a.a$a63/", "Invalid URL host: \"a.a$a63\"")
+    assertInvalid("http://a$a63.a/", "Invalid URL host: \"a$a63.a\"")
+  }
+
+  @Test
+  fun labelTooLongDueToAsciiExpansion() {
+    val a60 = "a".repeat(60)
+    assertThat(parse("http://\u2121$a60/").toString()).isEqualTo("http://tel$a60/")
+    assertInvalid("http://a\u2121$a60/", "Invalid URL host: \"a\u2121$a60\"")
+  }
+
+  @Test
+  fun hostnameTooLong() {
+    val dotA126 = "a.".repeat(126)
+    assertThat(parse("http://a$dotA126/").toString())
+      .isEqualTo("http://a$dotA126/")
+    assertInvalid("http://aa$dotA126/", "Invalid URL host: \"aa$dotA126\"")
   }
 }
