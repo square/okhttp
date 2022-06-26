948d4f03b4ff38bf0acaede5e83b337b67d2de7c
Jesse Wilson <jwilson@squareup.com>
2022-06-26 14:15:19 -0400

    Make pom.xml depend on the JVM artifact
    This should prevent Maven users from needing to change
    their dependencies from okhttp to okhttp-jvm. More importantly,
    it should also prevent them from inadvertently getting two
    different OkHttp JVM artifacts on their classpaths at once.

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,5 +1,7 @@
 import com.vanniktech.maven.publish.MavenPublishBaseExtension
 import com.vanniktech.maven.publish.SonatypeHost
+import groovy.util.Node
+import groovy.util.NodeList
 import java.net.URL
 import kotlinx.validation.ApiValidationExtension
 import org.gradle.api.tasks.testing.logging.TestExceptionFormat
@@ -209,6 +211,7 @@ subprojects {
   }
 
   plugins.withId("com.vanniktech.maven.publish.base") {
+    val publishingExtension = extensions.getByType(PublishingExtension::class.java)
     configure<MavenPublishBaseExtension> {
       publishToMavenCentral(SonatypeHost.S01)
       signAllPublications()
@@ -231,6 +234,31 @@ subprojects {
         developers {
           developer {
             name.set("Square, Inc.")
+          }
+        }
+      }
+
+      // Configure the kotlinMultiplatform artifact to depend on the JVM artifact in pom.xml only.
+      // This hack allows Maven users to continue using our original OkHttp artifact names (like
+      // com.squareup.okhttp3:okhttp:5.x.y) even though we changed that artifact from JVM-only
+      // to Kotlin Multiplatform. Note that module.json doesn't need this hack.
+      val mavenPublications = publishingExtension.publications.withType<MavenPublication>()
+      mavenPublications.configureEach {
+        if (name != "jvm") return@configureEach
+        val jvmPublication = this
+        val kmpPublication = mavenPublications.getByName("kotlinMultiplatform")
+        kmpPublication.pom.withXml {
+          val root = asNode()
+          val dependencies = (root["dependencies"] as NodeList).firstOrNull() as Node?
+            ?: root.appendNode("dependencies")
+          for (child in dependencies.children().toList()) {
+            dependencies.remove(child as Node)
+          }
+          dependencies.appendNode("dependency").apply {
+            appendNode("groupId", jvmPublication.groupId)
+            appendNode("artifactId", jvmPublication.artifactId)
+            appendNode("version", jvmPublication.version)
+            appendNode("scope", "compile")
           }
         }
       }
