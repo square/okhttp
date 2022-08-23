b63c1d9d96973e438ce87d4b6ab24f3c3220a3ff
Yuri Schimke <yuri@schimke.ee>
2022-08-23 07:00:07 +0100

    Post process md links in package-list files. (#7425)

--- deploy_website.sh
+++ deploy_website.sh
@@ -34,6 +34,10 @@ cd $DIR
   :okhttp-urlconnection:dokkaGfm \
   :okhttp:dokkaGfm
 
+# mkdocs doesn't correctly handle the .md paths in package-list, so post-process.
+# See https://github.com/square/okhttp/issues/7338
+find docs/4.x -name package-list -exec sed -i '' -e 's/index.md$/index.html/' -e 's/\.md$/\.html/' -e '1 s/gfm/html/' -e '2 s/md/html/' {} \;
+
 # Dokka filenames like `-http-url/index.md` don't work well with MkDocs <title> tags.
 # Assign metadata to the file's first Markdown heading.
 # https://www.mkdocs.org/user-guide/writing-your-docs/#meta-data
