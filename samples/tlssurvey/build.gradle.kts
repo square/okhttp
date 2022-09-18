c56e8648ca7123625c20a93c4efc27d08c80d3cb
Yuri Schimke <yuri@schimke.ee>
2022-09-18 08:38:01 +0100

    Refresh the OkHttp cipher suite process (#7393)

---
+++ samples/tlssurvey/build.gradle.kts
@@ -0,0 +1,26 @@
+plugins {
+  kotlin("jvm")
+  application
+  id("com.google.devtools.ksp").version("1.6.21-1.0.6")
+}
+
+application {
+  mainClass.set("okhttp3.survey.RunSurveyKt")
+}
+
+dependencies {
+  implementation("com.squareup.okhttp3:okhttp:5.0.0-alpha.10")
+  implementation("com.squareup.okhttp3:okhttp-coroutines:5.0.0-alpha.10")
+  implementation(libs.conscrypt.openjdk)
+
+  implementation("com.squareup.retrofit2:retrofit:2.9.0")
+  implementation("com.squareup.retrofit2:converter-moshi:2.9.0")
+  implementation(libs.squareup.moshi)
+  implementation(libs.squareup.moshi.kotlin)
+
+  ksp("com.squareup.moshi:moshi-kotlin-codegen:1.13.0")
+}
+
+tasks.compileJava {
+  options.isWarnings = false
+}
---
+++ samples/tlssurvey/src/main/kotlin/okhttp3/survey/Iana.kt
@@ -0,0 +1,64 @@
+/*
+ * Copyright (C) 2016 Square, Inc.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+package okhttp3.survey
+
+import okhttp3.HttpUrl.Companion.toHttpUrl
+import okhttp3.OkHttpClient
+import okhttp3.Request
+import okhttp3.executeAsync
+import okhttp3.survey.types.SuiteId
+import okio.ByteString
+import okio.ByteString.Companion.decodeHex
+import okio.IOException
+
+/** Example: "0x00,0x08",TLS_RSA_EXPORT_WITH_DES40_CBC_SHA,Y,N,[RFC4346] */
+val IANA_CSV_PATTERN = "\"0x(\\w\\w),0x(\\w\\w)\",(\\w+).*".toRegex()
+
+fun parseIanaCsvRow(s: String): SuiteId? {
+  if (s.contains("Reserved") || s.contains("Unassigned")) return null
+  val matcher = IANA_CSV_PATTERN.matchEntire(s) ?: return null
+  val id: ByteString = (matcher.groupValues[1] + matcher.groupValues[2]).decodeHex()
+  return SuiteId(id, matcher.groupValues[3])
+}
+
+class IanaSuites(
+  val name: String, val suites: List<SuiteId>
+) {
+  fun fromJavaName(javaName: String): SuiteId {
+    for (suiteId in suites) {
+      val alternateName = "TLS_" + javaName.substring(4)
+      if (suiteId.name == javaName || suiteId.name == alternateName) {
+        return suiteId
+      }
+    }
+    throw IllegalArgumentException("No such suite: $javaName")
+  }
+}
+
+suspend fun fetchIanaSuites(okHttpClient: OkHttpClient): IanaSuites {
+  val url = "https://www.iana.org/assignments/tls-parameters/tls-parameters-4.csv"
+
+  val call = okHttpClient.newCall(Request(url.toHttpUrl()))
+
+  val suites = call.executeAsync().use {
+    if (!it.isSuccessful) {
+      throw IOException("Failed ${it.code} ${it.message}")
+    }
+    it.body.string().lines()
+  }.mapNotNull { parseIanaCsvRow(it) }
+
+  return IanaSuites("current", suites)
+}
