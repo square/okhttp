7c3980cdfdb51ee84b2b1c6ac04c81c599eb64c2
Yuri Schimke <yuri@schimke.ee>
2022-12-10 17:55:41 +0000

    Use HTML for docs.

--- test_docs.sh
+++ test_docs.sh
@@ -9,35 +9,9 @@
 set -ex
 
 # Generate the API docs
-./gradlew \
-  :mockwebserver:dokkaGfm \
-  :okhttp-brotli:dokkaGfm \
-  :okhttp-dnsoverhttps:dokkaGfm \
-  :logging-interceptor:dokkaGfm \
-  :okhttp-sse:dokkaGfm \
-  :okhttp-tls:dokkaGfm \
-  :okhttp-urlconnection:dokkaGfm \
-  :okhttp:dokkaGfm
+./gradlew dokkaHtmlMultiModule
 
-# Dokka filenames like `-http-url/index.md` don't work well with MkDocs <title> tags.
-# Assign metadata to the file's first Markdown heading.
-# https://www.mkdocs.org/user-guide/writing-your-docs/#meta-data
-title_markdown_file() {
-  TITLE_PATTERN="s/^[#]+ *(.*)/title: \1 - OkHttp/"
-  echo "---"                                                     > "$1.fixed"
-  cat $1 | sed -E "$TITLE_PATTERN" | grep "title: " | head -n 1 >> "$1.fixed"
-  echo "---"                                                    >> "$1.fixed"
-  echo                                                          >> "$1.fixed"
-  cat $1                                                        >> "$1.fixed"
-  mv "$1.fixed" "$1"
-}
-
-set +x
-for MARKDOWN_FILE in $(find docs/4.x -name '*.md'); do
-  echo $MARKDOWN_FILE
-  title_markdown_file $MARKDOWN_FILE
-done
-set -x
+mv ./build/dokka/htmlMultiModule docs/4.x
 
 # Copy in special files that GitHub wants in the project root.
 cat README.md | grep -v 'project website' > docs/index.md
