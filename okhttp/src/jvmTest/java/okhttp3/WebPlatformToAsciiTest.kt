8f9128ed2bdc7919beea23b20a9c295328fe8b1c
Jesse Wilson <jwilson@squareup.com>
2023-02-18 10:58:13 -0500

    Fix copyright holder

--- okhttp/src/jvmTest/java/okhttp3/WebPlatformToAsciiTest.kt
+++ okhttp/src/jvmTest/java/okhttp3/WebPlatformToAsciiTest.kt
@@ -1,5 +1,5 @@
 /*
- * Copyright (C) 2023 Square, Inc.
+ * Copyright (C) 2023 Block, Inc.
  *
  * Licensed under the Apache License, Version 2.0 (the "License");
  * you may not use this file except in compliance with the License.
