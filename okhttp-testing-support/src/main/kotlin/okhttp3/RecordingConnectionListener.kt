f705635ba5a322e9c59bedfc1647d02c84531501
Yuri Schimke <yuri@schimke.ee>
2023-04-23 07:52:04 +0100

    Review Comments

--- okhttp-testing-support/src/main/kotlin/okhttp3/RecordingConnectionListener.kt
+++ okhttp-testing-support/src/main/kotlin/okhttp3/RecordingConnectionListener.kt
@@ -20,6 +20,7 @@ import java.util.concurrent.ConcurrentLinkedDeque
 import java.util.concurrent.TimeUnit
 import okhttp3.ConnectionEvent.NoNewExchanges
 import okhttp3.internal.connection.RealConnection
+import okhttp3.internal.platform.Platform
 import okio.IOException
 import org.assertj.core.api.Assertions.assertThat
 import org.assertj.core.data.Offset
@@ -102,8 +103,6 @@ open class RecordingConnectionListener(
   }
 
   private fun logEvent(e: ConnectionEvent) {
-    println(e)
-
     if (e.connection != null) {
       assertThat(Thread.holdsLock(e.connection))
         .overridingErrorMessage("Called with lock $${e.connection}")
