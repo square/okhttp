316f2e22444bcfb894080a5fa2fc10b25a90e345
Yuri Schimke <yuri@schimke.ee>
2023-05-27 16:59:30 +0100

    Run okhttp-android unit tests

--- okhttp-android/src/test/kotlin/okhttp3/android/RobolectricOkHttpClientTest.kt
+++ okhttp-android/src/test/kotlin/okhttp3/android/RobolectricOkHttpClientTest.kt
@@ -23,7 +23,6 @@ import okhttp3.CacheControl
 import okhttp3.HttpUrl.Companion.toHttpUrl
 import okhttp3.OkHttpClient
 import okhttp3.Request
-import okhttp3.android.OkHttpClientContext.okHttpClient
 import org.assertj.core.api.Assertions.assertThat
 import org.junit.AssumptionViolatedException
 import org.junit.Before
@@ -31,17 +30,14 @@ import org.junit.Test
 import org.junit.runner.RunWith
 import org.robolectric.RobolectricTestRunner
 import org.robolectric.annotation.Config
-import org.robolectric.annotation.GraphicsMode
 import java.net.InetAddress
 import java.net.UnknownHostException
 
 @RunWith(RobolectricTestRunner::class)
 @Config(
   sdk = [30],
-  qualifiers = "w221dp-h221dp-small-notlong-round-watch-xhdpi-keyshidden-nonav"
 )
-@GraphicsMode(GraphicsMode.Mode.NATIVE)
-class RobolectricClientBuilderTest {
+class RobolectricOkHttpClientTest {
 
   private lateinit var context: Context
   private lateinit var client: OkHttpClient
@@ -49,7 +45,7 @@ class RobolectricClientBuilderTest {
   @Before
   fun setUp() {
     context = ApplicationProvider.getApplicationContext<Application>()
-    client = context.okHttpClient
+    client = OkHttpClient()
   }
 
   @Test
