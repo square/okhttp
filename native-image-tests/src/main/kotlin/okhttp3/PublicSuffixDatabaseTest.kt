d4ca39b0979c9c03a4782467439a9675502d6c02
Yuri Schimke <yuri@schimke.ee>
2023-12-24 09:33:12 +0000

    Avoid checking file metadata in graal tests (#8161)

--- native-image-tests/src/main/kotlin/okhttp3/PublicSuffixDatabaseTest.kt
+++ native-image-tests/src/main/kotlin/okhttp3/PublicSuffixDatabaseTest.kt
@@ -20,10 +20,16 @@ import assertk.assertions.isEqualTo
 import assertk.assertions.isGreaterThan
 import okhttp3.HttpUrl.Companion.toHttpUrl
 import okhttp3.internal.publicsuffix.PublicSuffixDatabase
+import okhttp3.testing.PlatformRule
 import okio.FileSystem
 import org.junit.jupiter.api.Test
+import org.junit.jupiter.api.extension.RegisterExtension
 
 class PublicSuffixDatabaseTest {
+  @RegisterExtension
+  @JvmField
+  val platform = PlatformRule()
+
   @Test
   fun testResourcesLoaded() {
     val url = "https://api.twitter.com".toHttpUrl()
@@ -33,6 +39,8 @@ class PublicSuffixDatabaseTest {
 
   @Test
   fun testPublicSuffixes() {
+    platform.assumeNotGraalVMImage()
+
     val metadata = FileSystem.RESOURCES.metadata(PublicSuffixDatabase.PUBLIC_SUFFIX_RESOURCE)
     assertThat(metadata.size!!).isGreaterThan(30000)
   }
