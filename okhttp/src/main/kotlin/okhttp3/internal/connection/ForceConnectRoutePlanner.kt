ec3fec210066429d6b44815edb79338c268a39ab
Evan Nelson <enelson@squareup.com>
2024-03-22 08:02:43 -0700

    Some easy fixes

--- okhttp/src/main/kotlin/okhttp3/internal/connection/ForceConnectRoutePlanner.kt
+++ okhttp/src/main/kotlin/okhttp3/internal/connection/ForceConnectRoutePlanner.kt
@@ -1,22 +1,24 @@
+/*
+ *  Licensed to the Apache Software Foundation (ASF) under one or more
+ *  contributor license agreements.  See the NOTICE file distributed with
+ *  this work for additional information regarding copyright ownership.
+ *  The ASF licenses this file to You under the Apache License, Version 2.0
+ *  (the "License"); you may not use this file except in compliance with
+ *  the License.  You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ *  Unless required by applicable law or agreed to in writing, software
+ *  distributed under the License is distributed on an "AS IS" BASIS,
+ *  WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ *  See the License for the specific language governing permissions and
+ *  limitations under the License.
+ */
 package okhttp3.internal.connection
 
-import okhttp3.Address
-import okhttp3.HttpUrl
-
 /**
- * A RoutePlanner that will always generate a ConnectPlan, ignoring any connection pooling
+ * A RoutePlanner that will always establish a new connection, ignoring any connection pooling
  */
-class ForceConnectRoutePlanner(private val delegate: RealRoutePlanner) : RoutePlanner {
-  override val address: Address
-    get() = delegate.address
-  override val deferredPlans: ArrayDeque<RoutePlanner.Plan>
-    get() = delegate.deferredPlans
-
-  override fun isCanceled(): Boolean = delegate.isCanceled()
-
-  override fun plan(): RoutePlanner.Plan = delegate.planConnect()
-
-  override fun hasNext(failedConnection: RealConnection?): Boolean = delegate.hasNext(failedConnection)
-
-  override fun sameHostAndPort(url: HttpUrl): Boolean = delegate.sameHostAndPort(url)
+class ForceConnectRoutePlanner(private val delegate: RealRoutePlanner) : RoutePlanner by delegate {
+  override fun plan(): RoutePlanner.Plan = delegate.planConnect() // not delegate.plan()
 }
