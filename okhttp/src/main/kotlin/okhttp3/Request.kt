8382d0d6db335dee85b1d2a3c6eac0b91427a356
Jesse Wilson <jwilson@squareup.com>
2024-04-05 23:45:01 -0400

    Put Request.tag(KClass) into the public API

--- okhttp/src/main/kotlin/okhttp3/Request.kt
+++ okhttp/src/main/kotlin/okhttp3/Request.kt
@@ -101,7 +101,6 @@ class Request internal constructor(builder: Builder) {
   inline fun <reified T : Any> tag(): T? = tag(T::class)
 
   /** Returns the tag attached with [type] as a key, or null if no tag is attached with that key. */
-  @ExperimentalOkHttpApi
   fun <T : Any> tag(type: KClass<T>): T? = type.java.cast(tags[type])
 
   /**
@@ -300,7 +299,6 @@ class Request internal constructor(builder: Builder) {
      * Use this API to attach timing, debugging, or other application data to a request so that
      * you may read it in interceptors, event listeners, or callbacks.
      */
-    @ExperimentalOkHttpApi
     fun <T : Any> tag(
       type: KClass<T>,
       tag: T?,
