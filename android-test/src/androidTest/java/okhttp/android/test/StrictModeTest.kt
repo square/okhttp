f4805b00179502d911640408fae2ab9c191a3688
Yuri Schimke <yuri@schimke.ee>
2024-04-06 12:42:50 +0100

    dont fail

--- android-test/src/androidTest/java/okhttp/android/test/StrictModeTest.kt
+++ android-test/src/androidTest/java/okhttp/android/test/StrictModeTest.kt
@@ -2,8 +2,12 @@ package okhttp.android.test
 
 import android.os.StrictMode
 import android.os.StrictMode.ThreadPolicy
+import android.os.strictmode.Violation
 import assertk.assertThat
 import assertk.assertions.hasMessage
+import assertk.assertions.hasSize
+import assertk.assertions.isEmpty
+import assertk.assertions.isEqualTo
 import okhttp3.HttpUrl.Companion.toHttpUrl
 import okhttp3.OkHttpClient
 import okhttp3.Request
@@ -17,6 +21,8 @@ import org.opentest4j.AssertionFailedError
 
 @Isolated
 class StrictModeTest {
+  private val violations = mutableListOf<Violation>()
+
   @AfterEach
   fun cleanup() {
     StrictMode.setThreadPolicy(
@@ -32,13 +38,12 @@ class StrictModeTest {
 
     applyStrictMode()
 
-    val e =
-      assertThrows<AssertionFailedError> {
-        // Not currently safe
-        // See https://github.com/square/okhttp/pull/8248
-        OkHttpClient()
-      }
-    assertThat(e).hasMessage("Slow call on main")
+    // Not currently safe
+    // See https://github.com/square/okhttp/pull/8248
+    OkHttpClient()
+
+    assertThat(violations).hasSize(1)
+    assertThat(violations[0].message).isEqualTo("newSSLContext")
   }
 
   @Test
@@ -51,6 +56,8 @@ class StrictModeTest {
 
     // Safe on main
     client.newCall(Request("https://google.com/robots.txt".toHttpUrl()))
+
+    assertThat(violations).isEmpty()
   }
 
   private fun applyStrictMode() {
@@ -58,7 +65,7 @@ class StrictModeTest {
       ThreadPolicy.Builder()
         .detectCustomSlowCalls()
         .penaltyListener({ it.run() }) {
-          fail("Slow call on main")
+          violations.add(it)
         }
         .build(),
     )
