577d621585f7525d3e98a9161bc26d2965686538
Jesse Wilson <jwilson@squareup.com>
2024-04-17 21:32:42 -0400

    Prepare next development version.

--- build.gradle.kts
+++ build.gradle.kts
@@ -47,7 +47,7 @@ configure<SpotlessExtension> {
 
 allprojects {
   group = "com.squareup.okhttp3"
-  version = "5.0.0-alpha.14"
+  version = "5.0.0-SNAPSHOT"
 
   repositories {
     mavenCentral()
