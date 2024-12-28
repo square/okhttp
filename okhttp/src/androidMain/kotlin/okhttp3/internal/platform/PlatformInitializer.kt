74baf3883db084be570fc3fdc3fc932416aa496c
Yuri Schimke <yuri@schimke.ee>
2024-12-28 11:06:49 +0000

    reformat

--- okhttp/src/androidMain/kotlin/okhttp3/internal/platform/PlatformInitializer.kt
+++ okhttp/src/androidMain/kotlin/okhttp3/internal/platform/PlatformInitializer.kt
@@ -34,23 +34,26 @@ class PlatformInitializer : ContentProvider() {
     projection: Array<out String?>?,
     selection: String?,
     selectionArgs: Array<out String?>?,
-    sortOrder: String?
+    sortOrder: String?,
   ): Cursor? = null
 
   override fun getType(uri: Uri): String? = null
 
-  override fun insert(uri: Uri, values: ContentValues?): Uri? = null
+  override fun insert(
+    uri: Uri,
+    values: ContentValues?,
+  ): Uri? = null
 
   override fun delete(
     uri: Uri,
     selection: String?,
-    selectionArgs: Array<out String?>?
+    selectionArgs: Array<out String?>?,
   ): Int = 0
 
   override fun update(
     uri: Uri,
     values: ContentValues?,
     selection: String?,
-    selectionArgs: Array<out String?>?
+    selectionArgs: Array<out String?>?,
   ): Int = 0
 }
