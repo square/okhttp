3a04d6855b75f2ee9901b97d17d66e312da5db86
Yuri Schimke <yuri@schimke.ee>
2024-12-31 16:48:46 +0000

    merge conflicts

--- okhttp/build.gradle.kts
+++ okhttp/build.gradle.kts
@@ -97,6 +97,7 @@ kotlin {
         compileOnly(libs.conscrypt.openjdk)
         implementation(libs.androidx.annotation)
         implementation(libs.androidx.startup.runtime)
+        implementation(libs.androidx.tracing.ktx)
       }
     }
 
