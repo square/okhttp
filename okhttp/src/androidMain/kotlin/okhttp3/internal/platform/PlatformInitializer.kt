5d4863174f55a4cff734bf682baf1a46dad29ee2
Yuri Schimke <yuri@schimke.ee>
2024-12-31 10:50:35 +0000

    Revert "switch to ContentProvider"
    This reverts commit 07a6f748

--- okhttp/src/androidMain/kotlin/okhttp3/internal/platform/PlatformInitializer.kt
+++ okhttp/src/androidMain/kotlin/okhttp3/internal/platform/PlatformInitializer.kt
@@ -15,42 +15,18 @@
  */
 package okhttp3.internal.platform
 
-import android.content.ContentProvider
-import android.content.ContentValues
-import android.database.Cursor
-import android.net.Uri
+import android.content.Context
+import androidx.startup.Initializer
 
 /**
- * ContentProvider initializer to ensure that the AndroidPlatform has access to the application context.
+ * Androidx Startup initializer to ensure that the AndroidPlatform has access to the application context.
  */
-class PlatformInitializer : ContentProvider() {
-  override fun onCreate(): Boolean {
+class PlatformInitializer : Initializer<Platform> {
+  override fun create(context: Context): Platform {
     PlatformRegistry.applicationContext = context
-    return true
+
+    return Platform.get()
   }
 
-  override fun query(
-    uri: Uri,
-    projection: Array<out String?>?,
-    selection: String?,
-    selectionArgs: Array<out String?>?,
-    sortOrder: String?
-  ): Cursor? = null
-
-  override fun getType(uri: Uri): String? = null
-
-  override fun insert(uri: Uri, values: ContentValues?): Uri? = null
-
-  override fun delete(
-    uri: Uri,
-    selection: String?,
-    selectionArgs: Array<out String?>?
-  ): Int = 0
-
-  override fun update(
-    uri: Uri,
-    values: ContentValues?,
-    selection: String?,
-    selectionArgs: Array<out String?>?
-  ): Int = 0
+  override fun dependencies(): List<Class<Initializer<*>>> = listOf()
 }
