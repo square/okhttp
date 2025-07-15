852567d7bde16baf2a1ac85d015b42351f6cc8a5
Jesse Wilson <jwilson@squareup.com>
2025-07-15 18:10:34 -0400

    New zstd interceptor

---
+++ okhttp-zstd/README.md
@@ -0,0 +1,20 @@
+OkHttp ZStandard (zstd) Integration
+===================================
+
+This module enables [ZStandard (zstd)][1] response compression in addition to Gzip, as long as
+the `Accept-Encoding` header is not otherwise set. Web servers must be configured to return zstd
+responses.
+
+Note that zstd is not used for sending requests.
+
+```java
+OkHttpClient client = new OkHttpClient.Builder()
+  .addInterceptor(ZstdInterceptor.INSTANCE)
+  .build();
+```
+
+```kotlin
+implementation("com.squareup.okhttp3:okhttp-zstd:0.0.0")
+```
+
+ [1]: https://github.com/facebook/zstd
---
+++ okhttp-zstd/build.gradle.kts
@@ -0,0 +1,31 @@
+import com.vanniktech.maven.publish.JavadocJar
+import com.vanniktech.maven.publish.KotlinJvm
+
+plugins {
+  kotlin("jvm")
+  id("org.jetbrains.dokka")
+  id("com.vanniktech.maven.publish.base")
+  id("binary-compatibility-validator")
+}
+
+project.applyOsgi(
+  "Export-Package: okhttp3.zstd",
+  "Automatic-Module-Name: okhttp3.zstd",
+  "Bundle-SymbolicName: com.squareup.okhttp3.zstd"
+)
+
+dependencies {
+  api(projects.okhttp)
+  implementation(libs.zstd.kmp.okio)
+
+  testImplementation(projects.okhttpTestingSupport)
+  testImplementation(projects.mockwebserver3Junit5)
+  testImplementation(libs.junit)
+  testImplementation(libs.kotlin.test.common)
+  testImplementation(libs.kotlin.test.junit)
+  testImplementation(libs.assertk)
+}
+
+mavenPublishing {
+  configure(KotlinJvm(javadocJar = JavadocJar.Empty()))
+}
