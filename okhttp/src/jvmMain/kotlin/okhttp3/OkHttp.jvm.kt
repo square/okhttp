09ca52f1dde29cffbfd68207bbccc333134f1146
Yuri Schimke <yuri@schimke.ee>
2025-07-26 13:17:53 +0100

    keep suppress

--- okhttp/src/jvmMain/kotlin/okhttp3/OkHttp.jvm.kt
+++ okhttp/src/jvmMain/kotlin/okhttp3/OkHttp.jvm.kt
@@ -21,6 +21,7 @@ actual object OkHttp {
   @JvmStatic
   actual val Version: String = CONST_VERSION
 
+  @Suppress("MayBeConstant") // Non-const so external callers get the runtime version.
   @JvmField
   @Deprecated(level = DeprecationLevel.WARNING, message = "Replaced with Version", replaceWith = ReplaceWith("Version"))
   val VERSION: String = CONST_VERSION
