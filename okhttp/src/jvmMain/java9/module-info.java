bc4fffe1f31fed1a797c97a06d947c988f9c3cf2
Yuri Schimke <yuri@schimke.ee>
2025-09-21 09:30:26 +0100

    Fix merge issue with modules and logging (#9078)

--- okhttp/src/jvmMain/java9/module-info.java
+++ okhttp/src/jvmMain/java9/module-info.java
@@ -6,7 +6,7 @@ module okhttp3 {
   exports okhttp3;
   exports okhttp3.internal to okhttp3.logging, okhttp3.sse, okhttp3.java.net.cookiejar, okhttp3.dnsoverhttps, mockwebserver3, okhttp3.mockwebserver, okhttp3.coroutines, okhttp3.tls;
   exports okhttp3.internal.concurrent to mockwebserver3, okhttp3.mockwebserver;
-  exports okhttp3.internal.connection to mockwebserver3, okhttp3.mockwebserver;
+  exports okhttp3.internal.connection to mockwebserver3, okhttp3.mockwebserver, okhttp3.logging;
   exports okhttp3.internal.http to okhttp3.logging, okhttp3.brotli, mockwebserver3;
   exports okhttp3.internal.http2 to mockwebserver3, okhttp3.mockwebserver;
   exports okhttp3.internal.platform to okhttp3.logging, okhttp3.java.net.cookiejar, okhttp3.dnsoverhttps, mockwebserver3, okhttp3.mockwebserver, okhttp3.tls;
