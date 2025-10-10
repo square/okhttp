d5b3f2c7621a89a4491cd4854f773bab5c91cbd1
Yuri Schimke <yuri@schimke.ee>
2025-10-10 06:19:46 +0100

    Fix SSL Handshake issue affecting MockWebserver on Android 24 (#9129)
    * Fix SSL Handshake issue affecting MockWebserver on Android 24
    
    Only called in MWS, but fails on API 24 when called. Adds a single Junit 4 test to run on Android 21-24.
    
    Add a test since Junit 5 wasn't running on older devices.
    
    * fix version number
    
    * reformat
    
    * reformat

--- okhttp/src/androidMain/kotlin/okhttp3/internal/platform/AndroidPlatform.kt
+++ okhttp/src/androidMain/kotlin/okhttp3/internal/platform/AndroidPlatform.kt
@@ -132,8 +132,8 @@ class AndroidPlatform :
     }
 
   override fun getHandshakeServerNames(sslSocket: SSLSocket): List<String> {
-    // The superclass implementation requires APIs not available until API 24+.
-    if (Build.VERSION.SDK_INT < 24) return listOf()
+    // The superclass implementation requires APIs not available until API 25+.
+    if (Build.VERSION.SDK_INT <= 24) return listOf()
     return super.getHandshakeServerNames(sslSocket)
   }
 
