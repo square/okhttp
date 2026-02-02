940dcb0a9cfb6f2c8ebebdd61dd4f372c7557f36
Yuri Schimke <yuri@schimke.ee>
2026-02-02 21:18:19 +0000

    fix warnings

--- okhttp-testing-support/src/main/kotlin/okhttp3/FakeSSLSession.kt
+++ okhttp-testing-support/src/main/kotlin/okhttp3/FakeSSLSession.kt
@@ -14,6 +14,8 @@
  * License for the specific language governing permissions and limitations under
  * the License.
  */
+@file:Suppress("DEPRECATION")
+
 package okhttp3
 
 import java.security.Principal
