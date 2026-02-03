20e52968dc37c037280d593c3b0d10575066886a
AmirMohammad Yazdanmanesh <amir.yazdanmanesh@gmail.com>
2026-02-03 11:42:25 +0330

    Fix flaky platformTrustedCertificates test for non-Entrust platforms (#9295)

--- okhttp-tls/src/test/java/okhttp3/tls/HandshakeCertificatesTest.kt
+++ okhttp-tls/src/test/java/okhttp3/tls/HandshakeCertificatesTest.kt
@@ -163,8 +163,18 @@ class HandshakeCertificatesTest {
         .toSet()
 
     // It's safe to assume all platforms will have a major Internet certificate issuer.
+    val majorIssuers = listOf(
+      "DigiCert",
+      "Let's Encrypt",
+      "ISRG",  // Internet Security Research Group (Let's Encrypt parent)
+      "GlobalSign",
+      "Comodo",
+      "Sectigo",
+      "GeoTrust",
+      "Entrust",
+    )
     assertThat(names).matchesPredicate { strings ->
-      strings.any { it.matches(Regex("[A-Z]+=Entrust.*")) }
+      strings.any { name -> majorIssuers.any { issuer -> name.contains(issuer, ignoreCase = true) } }
     }
   }
 
