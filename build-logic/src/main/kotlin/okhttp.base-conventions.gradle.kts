1cfe02f6bc428e6a5f4f4e1699fc34d1cb16dc62
Jesse Wilson <jesse@swank.ca>
2026-06-08 15:34:56 -0400

    Prepare next development version.

--- build-logic/src/main/kotlin/okhttp.base-conventions.gradle.kts
+++ build-logic/src/main/kotlin/okhttp.base-conventions.gradle.kts
@@ -7,7 +7,7 @@ import org.jetbrains.kotlin.gradle.tasks.KotlinCompile
 val libs = extensions.getByType<VersionCatalogsExtension>().named("libs")
 
 group = "com.squareup.okhttp3"
-version = "5.4.0"
+version = "5.5.0-SNAPSHOT"
 
 val platform = project.platform
 val testJavaVersion = project.testJavaVersion
