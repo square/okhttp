61b16cc6b0f1ac1269165d29319431ab2ef37665
Jesse Wilson <jesse@swank.ca>
2026-07-10 23:07:00 -0400

    Fixup docs

--- okhttp/src/commonJvmAndroid/kotlin/okhttp3/dns/Dns2.kt
+++ okhttp/src/commonJvmAndroid/kotlin/okhttp3/dns/Dns2.kt
@@ -33,14 +33,34 @@ interface DnsCall {
 
 interface DnsCallback {
   /**
-   * @param last true if no further calls to [onRecords] will be made for this call.
+   * @param last true if this is the last list of records for this address. That is a terminal event
+   *   and no further calls to this callback will be made.
+   * @param records a possibly-empty set of records received from the name server.
    */
   fun onRecords(call: DnsCall, last: Boolean, records: List<DnsRecord>)
+
+  /**
+   * This is a terminal event, and no further calls to this callback will be made.
+   */
   fun onFailure(call: DnsCall, e: IOException)
 }
 
 // TODO: no data classes in public APIs
 data class DnsRequest(
+  /**
+   * The host from a URL like `www.publicobject.com` or `ietf.org`. It may also be a
+   * Punycode-encoded name like `xn--n3h.net`.
+   *
+   * Hostnames must satisfy the following constraints:
+   *
+   *  * All characters must be printable ASCII characters.
+   *  * These characters are forbidden: `#`, `%`, `/`, `:`, `?`, `@`, `[`, `\`, and `]`.
+   *  * The string length must be in [1..253].
+   *  * After splitting the string into `.`-separated labels, each label must be in [1..63]. A
+   *    single trailing `.` is permitted.
+   *
+   * Hostnames that don't satisfy these requirements will fail with [DnsCallback.onFailure].
+   */
   val hostname: String,
 
   /**
@@ -56,17 +76,22 @@ data class DnsRequest(
    *
    * In an SVCB DNS query, the protocol, port, and hostname are combined like
    * `_8020._ftp.api.example.com`. The protocol and port segments are omitted when they hold
-   * default values.
+   * default values. This scheme is called [AttrLeaf](https://www.rfc-editor.org/info/rfc8552/).
    */
   val port: Int = -1,
 )
 
 sealed class DnsRecord private constructor() {
   /**
-   * The hostname this record applies to.
+   * The hostname that this record applies to.
    *
-   * This will be different from the query's original hostname if it is served by alternate
-   * hostnames. This will be the original hostname if no alternate hostnames are in use, or if the
+   * A single hostname (`api.example.com`) may be served by multiple supporting servers. For
+   * example, this is useful for geographic distribution (`us-east.api.example.com` and
+   * `eu-central.api.example.com`), and for change management (`blue.api.example.com`,
+   * `green.api.example.com`).
+   *
+   * This field will be different from [DnsRequest.hostname] if it is served by alternate hostname,
+   * as above. This will be that original hostname if no alternate hostnames are in use, or if the
    * underlying DNS resolver doesn't expose that data.
    */
   abstract val hostname: String
@@ -84,7 +109,13 @@ sealed class DnsRecord private constructor() {
   ) : DnsRecord()
 
   /**
-   * Service metadata for this host.
+   * Service metadata.
+   *
+   * This may apply to all supporting servers, or to a subset of them. If multiple hostnames are
+   * in use (as in the `us-east.api.example.com` example above), those hostnames must be themselves
+   * resolved to get the IP addresses to connect to. If [Svcb.ipAddressHints] is non-empty, those
+   * addresses are available without to an additional DNS round trip. (The additional round trip
+   * is still useful for completeness.)
    *
    * If this record is present on the DNS results for an `http://` request, the client should
    * simulate a 307 redirect to the equivalent `https://` URL. From RFC 9460, part 9.5:
@@ -120,15 +151,19 @@ sealed class DnsRecord private constructor() {
     val port: Int = -1,
 
     /**
-     * The IP addresses of hosts known to support this configuration. If empty, assume all records
-     * with the same [DnsRecord.hostname] also support this configuration.
+     * The IP of hostnames addresses known to support this [Svcb] record. If empty, assume all
+     * records with the same [DnsRecord.hostname] also support this configuration.
      *
      * (`ipv4hint`, 4)
      * (`ipv6hint`, 6)
      */
     val ipAddressHints: List<InetAddress> = listOf(),
 
-    /** This data is encoded according to RFC 9848 and RFC 9849. (`ech`, 5) */
+    /**
+     * The Encrypted Client Hello (ECH) data. This is encoded according to RFC 9848 and RFC 9849.
+     *
+     * (`ech`, 5)
+     */
     val echConfigList: ByteString? = null,
   ) : DnsRecord()
 }
